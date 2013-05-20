fce966dbcbcf8d497869d8dea1cae777d2603471
Lee Richardson <github@leerichardson.com>
2013-05-20 15:57:03 -0400

    Fixing the build?

--- ucd/src/main/java/com/altamiracorp/reddawn/ucd/models/Source.java
+++ ucd/src/main/java/com/altamiracorp/reddawn/ucd/models/Source.java
@@ -1,19 +1,6 @@
 package com.altamiracorp.reddawn.ucd.models;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
-import com.sun.xml.internal.bind.v2.runtime.JAXBContextImpl;
-import org.apache.accumulo.core.client.RowIterator;
-import org.apache.accumulo.core.client.Scanner;
-import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Mutation;
-import org.apache.accumulo.core.data.Value;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 public class Source {
   public static final String TABLE_NAME = "Source";
