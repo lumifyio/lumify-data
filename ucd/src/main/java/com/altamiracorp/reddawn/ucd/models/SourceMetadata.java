4103916e7ef19d57adcf133cfac0e038e52169ad
Lee Richardson <github@leerichardson.com>
2013-05-20 16:34:24 -0400

    Add all remaining source fields

--- ucd/src/main/java/com/altamiracorp/reddawn/ucd/models/SourceMetadata.java
+++ ucd/src/main/java/com/altamiracorp/reddawn/ucd/models/SourceMetadata.java
@@ -4,7 +4,23 @@ public class SourceMetadata {
   public static final String COLUMN_FAMILY_NAME = "Source";
   private static final String COLUMN_ACRONYM = "acronym";
   private String acronym;
-
+  private String additionalDetails;
+  private String country;
+  private String dataType;
+  private String description;
+  private long ingestDate;
+  private String ingestStatus;
+  private String intelTypes;
+  private String location;
+  private String mediaTypes;
+  private String name;
+  private String orgAcronym;
+  private String orgName;
+  private String orgParentAcronym;
+  private String orgParentName;
+  private String type;
+          
+          
   private SourceMetadata() {
 
   }
@@ -15,6 +31,66 @@ public class SourceMetadata {
 
   public String getAcronym() {
     return acronym;
+  }
+
+  public String getAdditionalDetails() {
+    return additionalDetails;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public String getDataType() {
+    return dataType;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public long getIngestDate() {
+    return ingestDate;
+  }
+
+  public String getIngestStatus() {
+    return ingestStatus;
+  }
+
+  public String getIntelTypes() {
+    return intelTypes;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public String getMediaTypes() {
+    return mediaTypes;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getOrgAcronym() {
+    return orgAcronym;
+  }
+
+  public String getOrgName() {
+    return orgName;
+  }
+
+  public String getOrgParentAcronym() {
+    return orgParentAcronym;
+  }
+
+  public String getOrgParentName() {
+    return orgParentName;
+  }
+
+  public String getType() {
+    return type;
   }
 
   public static class Builder {
@@ -30,5 +106,66 @@ public class SourceMetadata {
     public void acronym(String acronym) {
       metadata.acronym = acronym;
     }
+
+    public void additionalDetails(String additionalDetails) {
+      metadata.additionalDetails = additionalDetails;
+    }
+
+    public void country(String country) {
+      metadata.country = country;
+    }
+
+    public void dataType(String dataType) {
+      metadata.dataType = dataType;
+    }
+
+    public void description(String description) {
+      metadata.description = description;
+    }
+
+    public void ingestDate(long ingestDate) {
+      metadata.ingestDate = ingestDate;
+    }
+
+    public void ingestStatus(String ingestStatus) {
+      metadata.ingestStatus = ingestStatus;
+    }
+
+    public void intelTypes(String intelTypes) {
+      metadata.intelTypes = intelTypes;
+    }
+
+    public void location(String location) {
+      metadata.location = location;
+    }
+
+    public void mediaTypes(String mediaTypes) {
+      metadata.mediaTypes = mediaTypes;
+    }
+
+    public void name(String name) {
+      metadata.name = name;
+    }
+
+    public void orgAcronym(String orgAcronym) {
+      metadata.orgAcronym = orgAcronym;
+    }
+
+    public void orgName(String orgName) {
+      metadata.orgName = orgName;
+    }
+
+    public void orgParentAcronym(String orgParentAcronym) {
+      metadata.orgParentAcronym = orgParentAcronym;
+    }
+
+    public void orgParentName(String orgParentName) {
+      metadata.orgParentName = orgParentName;
+    }
+
+    public void type(String type) {
+      metadata.type = type;
+    }
+
   }
 }
--- ucd/src/test/java/com/altamiracorp/reddawn/ucd/models/SourceTest.java
+++ ucd/src/test/java/com/altamiracorp/reddawn/ucd/models/SourceTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JUnit4.class)
@@ -14,6 +17,21 @@ public class SourceTest {
 
     SourceMetadata.Builder sm = SourceMetadata.newBuilder();
     sm.acronym("DOD");
+    sm.additionalDetails("additionalDetails");
+    sm.country("country");
+    sm.dataType("dataType");
+    sm.description("description");
+    sm.ingestDate(new GregorianCalendar(2013, 1, 1, 1, 1).getTime().getTime());
+    sm.ingestStatus("ingestStatus");
+    sm.intelTypes("intelTypes");
+    sm.location("location");
+    sm.mediaTypes("mediaTypes");
+    sm.name("name");
+    sm.orgAcronym("orgAcronym");
+    sm.orgName("orgName");
+    sm.orgParentAcronym("orgParentAcronym");
+    sm.orgParentName("orgParentName");
+    sm.type("type");
 
     Source source = sb
             .uuid("uuid")
@@ -22,5 +40,20 @@ public class SourceTest {
 
     assertEquals("uuid", source.getKey().toString());
     assertEquals("DOD", source.getMetadata().getAcronym());
+    assertEquals("additionalDetails", source.getMetadata().getAdditionalDetails());
+    assertEquals("country", source.getMetadata().getCountry());
+    assertEquals("dataType", source.getMetadata().getDataType());
+    assertEquals("description", source.getMetadata().getDescription());
+    assertEquals(new GregorianCalendar(2013, 1, 1, 1, 1).getTime().getTime(), source.getMetadata().getIngestDate());
+    assertEquals("ingestStatus", source.getMetadata().getIngestStatus());
+    assertEquals("intelTypes", source.getMetadata().getIntelTypes());
+    assertEquals("location", source.getMetadata().getLocation());
+    assertEquals("mediaTypes", source.getMetadata().getMediaTypes());
+    assertEquals("name", source.getMetadata().getName());
+    assertEquals("orgAcronym", source.getMetadata().getOrgAcronym());
+    assertEquals("orgName", source.getMetadata().getOrgName());
+    assertEquals("orgParentAcronym", source.getMetadata().getOrgParentAcronym());
+    assertEquals("orgParentName", source.getMetadata().getOrgParentName());
+    assertEquals("type", source.getMetadata().getType());
   }
 }
