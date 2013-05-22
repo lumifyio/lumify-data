81489accc1dec7926589458732b682867ca5b7ed
Joe Ferner <joe@fernsroth.com>
2013-05-22 10:43:41 -0400

    refactored map job into base class

---
+++ core/src/main/java/com/altamiracorp/reddawn/ConfigurableMapJobBase.java
@@ -0,0 +1,89 @@
+package com.altamiracorp.reddawn;
+
+import com.altamiracorp.reddawn.cmdline.UcdCommandLineBase;
+import com.altamiracorp.reddawn.ucd.inputFormats.UCDArtifactInputFormat;
+import com.altamiracorp.reddawn.ucd.models.Term;
+import com.altamiracorp.reddawn.ucd.outputFormats.UCDOutputFormat;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Value;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+
+public abstract class ConfigurableMapJobBase extends UcdCommandLineBase implements Tool {
+
+    private Class clazz;
+    private String[] config;
+
+    @Override
+    protected Options getOptions() {
+        Options options = super.getOptions();
+
+        options.addOption(
+                OptionBuilder
+                        .withArgName("c")
+                        .withLongOpt("classname")
+                        .withDescription("The class that implements EntityExtractor")
+                        .withArgName("name")
+                        .isRequired()
+                        .hasArg()
+                        .create()
+        );
+
+        options.addOption(
+                OptionBuilder
+                        .withArgName("D")
+                        .withLongOpt("config")
+                        .withDescription("Configuration for the extractor")
+                        .withArgName("name=value")
+                        .hasArg()
+                        .create()
+        );
+
+        return options;
+    }
+
+    @Override
+    protected void processOptions(CommandLine cmd) {
+        super.processOptions(cmd);
+
+        String textExtractorClassName = cmd.getOptionValue("classname");
+        if (textExtractorClassName == null) {
+            throw new RuntimeException("'class' parameter is required");
+        }
+        clazz = loadClass(textExtractorClassName);
+        config = cmd.getOptionValues("config");
+    }
+
+    @Override
+    protected int run(CommandLine cmd) throws Exception {
+        Job job = new Job(getConf(), this.getClass().getSimpleName());
+        job.setJarByClass(this.getClass());
+
+        job.setInputFormatClass(UCDArtifactInputFormat.class);
+        UCDArtifactInputFormat.init(job, getUsername(), getPassword(), getAuthorizations(), getZookeeperInstanceName(), getZookeeperServerNames());
+
+        if (this.config != null) {
+            for (String config : this.config) {
+                String[] parts = config.split("=", 2);
+                job.getConfiguration().set(parts[0], parts[1]);
+            }
+        }
+
+        job.setMapOutputKeyClass(Key.class);
+        job.setMapOutputValueClass(Value.class);
+        job.setMapperClass(getMapperClass(job, clazz));
+
+        job.setNumReduceTasks(0);
+
+        job.setOutputFormatClass(UCDOutputFormat.class);
+        UCDOutputFormat.init(job, getUsername(), getPassword(), getZookeeperInstanceName(), getZookeeperServerNames(), Term.TABLE_NAME);
+
+        job.waitForCompletion(true);
+        return job.isSuccessful() ? 0 : 1;
+    }
+
+    protected abstract Class getMapperClass(Job job, Class clazz);
+}
