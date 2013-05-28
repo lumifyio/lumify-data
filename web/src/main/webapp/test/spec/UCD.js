2cb19ad082af739286c623a00bba644f4903e4eb
Ryan Lanman <rmlanman@gmail.com>
2013-05-28 14:06:07 -0400

    Updating UCD JS unit tests

--- web/src/main/webapp/test/spec/UCD.js
+++ web/src/main/webapp/test/spec/UCD.js
@@ -6,6 +6,21 @@ define(['ucd/ucd'], function(UCD) {
             expect(UCD).to.be.a('function');
         });
 
+		it('should have some defaults', function () {
+			var ucd = new UCD({});
+			assert.isFalse(ucd.options.jsonp);
+			assert.equal(ucd.options.serviceBaseUrl,"/");
+		});
+		
+		it('should have some functions', function () {
+			var ucd = new UCD({});
+			expect(ucd.artifactSearch).to.be.a("function");
+			expect(ucd.entitySearch).to.be.a("function");
+			expect(ucd.getArtifactById).to.be.a("function");
+			expect(ucd.getEntityById).to.be.a("function");
+			assert.equal(ucd._resolveUrl("entity/search"),"/red-dawn-web/entity/search");
+		})
+
     });
 
 });
