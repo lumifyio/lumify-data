3bbdf236bac0571787372dffa16dbd9bec94ce03
Ryan Lanman <rmlanman@gmail.com>
2013-06-05 10:56:34 -0400

    Cleaning up Maven dependencies

--- web/src/main/java/com/altamiracorp/reddawn/web/Server.java
+++ web/src/main/java/com/altamiracorp/reddawn/web/Server.java
@@ -13,69 +13,6 @@ import com.altamiracorp.reddawn.cmdline.UcdCommandLineBase;
 
 public class Server extends UcdCommandLineBase {
 
-<<<<<<< Updated upstream
-    private int port;
-
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(CachedConfiguration.getInstance(), new Server(), args);
-        if (res != 0) {
-            System.exit(res);
-        }
-    }
-
-    @Override
-    protected Options getOptions() {
-        Options options = super.getOptions();
-
-        options.addOption(
-                OptionBuilder
-                        .withArgName("p")
-                        .withLongOpt("port")
-                        .withDescription("The port to run the server on")
-                        .withArgName("port")
-                        .hasArg()
-                        .create()
-        );
-
-        return options;
-    }
-
-    @Override
-    protected void processOptions(CommandLine cmd) {
-        super.processOptions(cmd);
-
-        String port = cmd.getOptionValue("port");
-        if (port == null) {
-            this.port = 8080;
-        } else {
-            this.port = Integer.parseInt(port);
-        }
-    }
-
-    @Override
-    protected int run(CommandLine cmd) throws Exception {
-        // TODO refactor this
-        WebUcdClientFactory.setUcdCommandLineBase(this);
-        WebUcdClientFactory.createUcdClient().initializeTables();
-        WebUcdClientFactory.createRedDawnClient().initializeTables();
-
-        org.mortbay.jetty.Server server = new org.mortbay.jetty.Server();
-        SelectChannelConnector connector = new SelectChannelConnector();
-        connector.setHost("127.0.0.1");
-        connector.setPort(this.port);
-        server.addConnector(connector);
-
-        WebAppContext webAppContext = new WebAppContext();
-        webAppContext.setContextPath("/");
-        webAppContext.setWar("./web/src/main/webapp/");
-
-        server.addHandler(webAppContext);
-        server.start();
-        server.join();
-
-        return 0;
-    }
-=======
 	private int port;
 
 	public static void main(String[] args) throws Exception {
@@ -90,9 +27,15 @@ public class Server extends UcdCommandLineBase {
 	protected Options getOptions() {
 		Options options = super.getOptions();
 
-		options.addOption(OptionBuilder.withArgName("p").withLongOpt("port")
-				.withDescription("The port to run the server on")
-				.withArgName("port").hasArg().create());
+        options.addOption(
+                OptionBuilder
+                        .withArgName("p")
+                        .withLongOpt("port")
+                        .withDescription("The port to run the server on")
+                        .withArgName("port")
+                        .hasArg()
+                        .create()
+        );
 
 		return options;
 	}
@@ -109,11 +52,12 @@ public class Server extends UcdCommandLineBase {
 		}
 	}
 
-	@Override
-	protected int run(CommandLine cmd) throws Exception {
-		// TODO refactor this
-		WebUcdClientFactory.setUcdCommandLineBase(this);
-		WebUcdClientFactory.createUcdClient().initializeTables();
+    @Override
+    protected int run(CommandLine cmd) throws Exception {
+        // TODO refactor this
+        WebUcdClientFactory.setUcdCommandLineBase(this);
+        WebUcdClientFactory.createUcdClient().initializeTables();
+        WebUcdClientFactory.createRedDawnClient().initializeTables();
 
 		InetSocketAddress addr = new InetSocketAddress("127.0.0.1", this.port);
 		org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server(
@@ -129,6 +73,5 @@ public class Server extends UcdCommandLineBase {
 
 		return 0;
 	}
->>>>>>> Stashed changes
 
 }
