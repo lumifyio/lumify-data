a71ff79c8ef9bd0aec0949d9fd759ca884286009
Samantha Woloszynski <swoloszy@nearinfinity.com>
2013-06-05 13:22:15 -0400

    test commit / comment

--- crawler/src/main/java/com/altamiracorp/reddawn/Query.java
+++ crawler/src/main/java/com/altamiracorp/reddawn/Query.java
@@ -9,5 +9,6 @@ public class Query
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        // Testing to make sure we're using git correctly.
     }
 }
