7ee20c2e81f392de9ff7b5dee4f7d022f67b3244
Jeff Principe <jprincip@nearinfinity.com>
2013-06-05 13:27:55 -0400

    Change hello world string

--- crawler/src/main/java/com/altamiracorp/reddawn/Query.java
+++ crawler/src/main/java/com/altamiracorp/reddawn/Query.java
@@ -8,7 +8,7 @@ public class Query
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Sam!" );
         // Testing to make sure we're using git correctly.
     }
 }
