9eb34ca172a252036436df06d7e99293746d9936
Samantha Woloszynski <swoloszy@nearinfinity.com>
2013-06-05 12:44:09 -0400

    Creation of crawler branch

---
+++ crawler/src/main/java/com/altamiracorp/reddawn/Query.java
@@ -0,0 +1,13 @@
+package com.altamiracorp.reddawn;
+
+/**
+ * Hello world!
+ *
+ */
+public class Query
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
