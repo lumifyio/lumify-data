930a89c9d34fc86b186b46aae9e5cc6a242e9555
Samantha Woloszynski <swoloszy@nearinfinity.com>
2013-06-05 17:31:54 -0400

    Query implemented, needs more testing

--- crawler/src/main/java/com/altamiracorp/reddawn/Query.java
+++ crawler/src/main/java/com/altamiracorp/reddawn/Query.java
@@ -1,40 +1,79 @@
 package com.altamiracorp.reddawn;
-import java.net.URL;
-import java.net.URLConnection;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
- * Hello world!
- *
+ * This class wraps the data to be used to construct the query with the user's input.
  */
+
 public class Query
 {
+    private HashMap<String, String> searchItems;
+    private ArrayList<String> excludedTerms;
+    private ArrayList<String> requiredTerms;
+    private ArrayList<String> optionalTerms;
+
+    public Query(String country_, String startDate_, String endDate_, String geoLoc_,
+                 String lowRange_, String highRange_)
+    {
+        //jeff, the ranges are taken in as Strings, so engine will need to parse
+        searchItems = new HashMap<String, String>();
+        searchItems.put("country", country_);
+        searchItems.put("startDate", startDate_);
+        searchItems.put("endDate", endDate_);
+        searchItems.put("geoLoc", geoLoc_);
+        searchItems.put("lowRange", lowRange_);
+        searchItems.put("highRange", highRange_);
+
+        excludedTerms = new ArrayList<String>();
+        requiredTerms = new ArrayList<String>();
+        optionalTerms = new ArrayList<String>();
+    }
+
+    public void addExcludedTerm(String term)
+    {
+        excludedTerms.add(term);
+    }
+
+    public void addRequiredTerm(String term)
+    {
+        requiredTerms.add(term);
+    }
+
+    public void addOptionalTerm(String term)
+    {
+        optionalTerms.add(term);
+    }
+
+
+    public HashMap<String, String> getSearchItems() {
+        return searchItems;
+    }
+
+    public ArrayList<String> getExcludedTerms() {
+        return excludedTerms;
+    }
+
+    public ArrayList<String> getRequiredTerms() {
+        return requiredTerms;
+    }
+
+    public ArrayList<String> getOptionalTerms() {
+        return optionalTerms;
+    }
+
+    //PRELIMINARY TESTING ONLY! REMOVE MAIN
+    //Try to convert this to JUnit testing tomorrow, or whichever kind of testing the team uses.
     public static void main( String[] args )   throws Exception
     {
-        System.out.println( "Hello Sam!" );
-        // Testing to make sure we're using git correctly.
-
-        // The request also includes the userip parameter which provides the end
-// user's IP address. Doing so will help distinguish this legitimate
-// server-side traffic from traffic which doesn't come from an end-user.
-        URL url = new URL(
-                "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
-                        + "q=Paris%20Hilton&userip=USERS-IP-ADDRESS");
-        URLConnection connection = url.openConnection();
-        connection.addRequestProperty("Referer", "nearinfinity.com");
-
-        String line;
-        StringBuilder builder = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        while((line = reader.readLine()) != null) {
-            builder.append(line);
-        }
-
-        JSONObject json = new JSONObject(builder.toString());
-        System.out.println(json.toString());
-// now have some fun with the results...
-
+        System.out.println( "Starting..." );
+        Query q1 = new Query("US", "Monday", "Friday", "as;ldkfj", "100", "400");
+        System.out.println(q1.getSearchItems().toString());
+        q1.addExcludedTerm("excludedTerm1");
+        System.out.println("Excluded: " + q1.getExcludedTerms().toString());
+        q1.addOptionalTerm("optionalTerm1");
+        System.out.println("Optional: " + q1.getOptionalTerms().toString());
+        q1.addRequiredTerm("requiredTerm1");
+        System.out.println("Required:" + q1.getRequiredTerms().toString());
     }
 }
