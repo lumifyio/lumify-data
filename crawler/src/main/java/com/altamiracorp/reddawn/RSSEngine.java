b09aee716681f02d081ae1e83ed23bc2a1d7ece6
Samantha Woloszynski <swoloszy@nearinfinity.com>
2013-06-10 12:30:14 -0400

    Initial RSSEngine commit

---
+++ crawler/src/main/java/com/altamiracorp/reddawn/RSSEngine.java
@@ -0,0 +1,11 @@
+package com.altamiracorp.reddawn;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: swoloszy
+ * Date: 6/10/13
+ * Time: 12:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RSSEngine {
+}
---
+++ crawler/src/test/java/com/altamiracorp/reddawn/RSSEngineTest.java
@@ -0,0 +1,17 @@
+package com.altamiracorp.reddawn;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: swoloszy
+ * Date: 6/10/13
+ * Time: 12:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RSSEngineTest {
+	@Test
+	public void testSearch() throws Exception {
+
+	}
+}
