f8be15ef0d898f1f9eeab4d3b6c3ada6688e245b
Jeff Principe <jprincip@nearinfinity.com>
2013-06-12 17:35:14 -0400

    Refactored WebCrawl and WebCrawlTest

--- crawler/src/main/java/com/altamiracorp/reddawn/WebCrawl.java
+++ crawler/src/main/java/com/altamiracorp/reddawn/WebCrawl.java
@@ -16,9 +16,6 @@ public class WebCrawl {
     private int results;
 	public static final int DEFAULT_RESULT_COUNT = 20;
 
-	/**
-	 * Empty constructor which only initializes the fields of the object. Mainly here for testing
-	 */
 	public WebCrawl() {
 		parser = new GnuParser();
 
@@ -30,36 +27,32 @@ public class WebCrawl {
 		results = -1;
 	}
 
-	/**
-	 * Constructs a driver and loads in all of the necessary information. After this it is ready to crawl
-	 *
-	 * @param args command line arguments passed into the main method
-	 */
-    public WebCrawl(String[] args) {
-		this();
+	protected void setCommandLine(CommandLine cl) {
+		this.cl = cl;
+	}
 
-        loadCommandLine(args);
+	protected void setCrawler(Crawler crawler) {
+		this.crawler = crawler;
+	}
 
-        if(getQueryParam() != null) {
-            for(String s : getQueryParam().split(",")) {
-                Query search = addSearchQuery(s);
-                addRedditQueries(search);
-            }
-        } else {
-            addRedditQueries(new Query());
-        }
+	public void prepare(String[] args) {
+		loadCommandLine(args);
 
-        addRSSLinks();
-        setResultCount();
-        addEngines();
-    }
+		if(getQueryParam() != null) {
+			for(String s : getQueryParam().split(",")) {
+				Query search = addSearchQuery(s);
+				addRedditQueries(search);
+			}
+		} else {
+			addRedditQueries(new Query());
+		}
 
-	/**
-	 * Loads the command line parameters and options into the CommndLine field of the object
-	 *
-	 * @param args
-	 */
-    public void loadCommandLine(String[] args) {
+		addRSSLinks();
+		setResultCount();
+		addEngines();
+	}
+
+    protected void loadCommandLine(String[] args) {
         try {
             cl = parser.parse(createOptions(), args);
         } catch(ParseException e) {
@@ -70,13 +63,7 @@ public class WebCrawl {
         crawler = new Crawler(cl.getOptionValue("directory"));
     }
 
-	/**
-	 * Adds and returns a search query, given a string to search
-	 *
-	 * @param queryString The string for a single search query in the format entered by the user
-	 * @return Query built from the string given
-	 */
-    public Query addSearchQuery(String queryString) {
+    protected Query addSearchQuery(String queryString) {
         Map<String, ArrayList<String>> queryTerms = parseQuery(queryString.trim());
         Query q = new Query();
 
@@ -92,13 +79,7 @@ public class WebCrawl {
         return q;
     }
 
-	/**
-	 * Adds queries to search on reddit for a search query passed in. To poll reddit with no search, pass an empty Query
-	 *
-	 * @param q The search query to process into reddit queries
-	 */
-    public void addRedditQueries(Query q) {
-        // Checks for subreddits and sets information accordingly
+    protected void addRedditQueries(Query q) {
         String subreddits = cl.getOptionValue("subreddit");
 
         if(subreddits != null && subreddits.trim().length() > 0) {
@@ -114,10 +95,7 @@ public class WebCrawl {
         }
     }
 
-	/**
-	 * Adds all of the rss links passed in to the list of queries to be performed on the RSS engine
-	 */
-    public void addRSSLinks() {
+    protected void addRSSLinks() {
         if(cl.getOptionValue("rss") != null) {
             for(String feed : cl.getOptionValue("rss").split(",")) {
 				if(feed.trim().length() == 0) continue;
@@ -128,11 +106,7 @@ public class WebCrawl {
         }
     }
 
-	/**
-	 * Adds an engine for each provider given and adds the appropriate set of queries to it
-	 */
-    public void addEngines() {
-        // Adds engines based on the provider option
+    protected void addEngines() {
         ArrayList<String> enginesAdded = new ArrayList<String>();
         for(String s : cl.getOptionValue("provider").split(",")) {
             String trimmed = s.trim();
@@ -162,10 +136,7 @@ public class WebCrawl {
         }
     }
 
-	/**
-	 * Sets the number of results to return based on command line parameters
-	 */
-    public void setResultCount() {
+    protected void setResultCount() {
         try {
             String countParam = cl.getOptionValue("result-count");
             if(countParam != null) {
@@ -177,28 +148,15 @@ public class WebCrawl {
         }
     }
 
-	/**
-	 * Runs the driver on the set of engines and queries that were set up
-	 */
     public void run() {
         // Runs queries on the search engine
         for(SearchEngine engine : engines) engine.runQueue();
     }
 
-	/**
-	 * Gets the "query" command line parameter
-	 *
-	 * @return String for the "query" command line parameter passed to the CommandLine
-	 */
     public String getQueryParam() {
         return cl.getOptionValue("query");
     }
 
-    /**
-     * Generates the list of command line options for the program
-     *
-     * @return The options for the command line program
-     */
     public static Options createOptions() {
         Options options = new Options();
 
@@ -261,12 +219,6 @@ public class WebCrawl {
         return options;
     }
 
-    /**
-     * Parses a single query for required, excluded and optional terms (mark required terms with +<term> and excluded with -<term>)
-     *
-     * @param query The query string to parse
-     * @return Map of type to an ArrayList of terms that match it.  Entries are "optional", "excluded", and "required"
-     */
     public static Map<String, ArrayList<String>> parseQuery(String query) {
 		Map<String, ArrayList<String>> params = new TreeMap<String, ArrayList<String>>();
         params.put("optional", new ArrayList<String>());
@@ -283,18 +235,6 @@ public class WebCrawl {
 
         return params;
     }
-
-	/**
-	 * Reinitializes fields to their initial empty state. Used mostly for testing
-	 */
-	public void clear() {
-		engines = new ArrayList<SearchEngine>();
-		queries = new ArrayList<Query>();
-		rssLinks = new ArrayList<Query>();
-		redditQueries = new ArrayList<Query>();
-
-		results = -1;
-	}
 
 	public ArrayList<SearchEngine> getEngines() {
 		return engines;
@@ -316,14 +256,9 @@ public class WebCrawl {
 		return results;
 	}
 
-	/**
-	 * Main method. creates and runs the driver
-	 *
-	 * @param args Command line arguments
-	 *
-	 */
 	public static void main(String[] args) {
-		WebCrawl driver = new WebCrawl(args);
+		WebCrawl driver = new WebCrawl();
+		driver.prepare(args);
 		driver.run();
 	}
 }
--- crawler/src/test/java/com/altamiracorp/reddawn/WebCrawlTest.java
+++ crawler/src/test/java/com/altamiracorp/reddawn/WebCrawlTest.java
@@ -1,159 +1,186 @@
 package com.altamiracorp.reddawn;
 
-import junit.framework.TestCase;
+import org.apache.commons.cli.CommandLine;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jprincip
- * Date: 6/11/13
- * Time: 8:56 AM
- * To change this template use File | Settings | File Templates.
- */
-public class WebCrawlTest extends TestCase {
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
-	WebCrawl driver;
-	String[] argsAll = { "--query=boston marathon,boston +bombing -mit", "--provider=google,news,rss,reddit", "--directory=fakepath", "--rss=http://rss.cnn.com/rss/cnn_topstories.rss",
-					"--result-count=20", "--subreddit=all,inthenews" },
-			argsJustSubreddit = { "--query=boston marathon,boston +bombing -mit", "--provider=reddit", "--directory=fakepath", "--result-count=20", "--subreddit=inthenews" },
-			argsNoSubreddit = { "--query=boston marathon,boston +bombing -mit", "--provider=reddit", "--directory=fakepath", "--result-count=20" },
-			argsEmptySubreddit = { "--query=boston marathon,boston +bombing -mit", "--provider=google,news,rss,reddit", "--directory=fakepath", "--rss=http://rss.cnn.com/rss/cnn_topstories.rss",
-					"--result-count=20", "--subreddit=" },
-			argsNoRssLink = { "--query=boston marathon,boston +bombing -mit", "--provider=google,news,rss,reddit", "--directory=fakepath", "--result-count=20" },
-			argsEmptyRssLink = { "--query=boston marathon,boston +bombing -mit", "--provider=google,news,rss,reddit", "--directory=fakepath", "--result-count=20", "--rss=" },
-			argsMultipleRssLinks = { "--query=boston marathon", "--provider=rss", "--directory=fakepath", "--rss=http://rss.cnn.com/rss/cnn_topstories.rss,http://rss.cnn.com/rss/cnn_topstories.rss"},
-			argsNegResultCount = { "--query=boston marathon,boston +bombing -mit", "--provider=google,news,rss,reddit", "--directory=fakepath", "--rss=http://rss.cnn.com/rss/cnn_topstories.rss",
-					"--result-count=-20", "--subreddit=all,inthenews" },
-			argsZeroResultCount = { "--query=boston marathon,boston +bombing -mit", "--provider=google,news,rss,reddit", "--directory=fakepath", "--rss=http://rss.cnn.com/rss/cnn_topstories.rss",
-					"--result-count=-20", "--subreddit=all,inthenews" },
-			argsNaNResultCount = { "--query=boston marathon,boston +bombing -mit", "--provider=google,news,rss,reddit", "--directory=fakepath", "--rss=http://rss.cnn.com/rss/cnn_topstories.rss",
-					"--result-count=hello", "--subreddit=all,inthenews" };
+@RunWith(JUnit4.class)
+public class WebCrawlTest {
+
+	WebCrawl driverSpy;
+	CommandLine mockCL;
+	Query mockQuery;
 
 	@Before
 	public void setUp() {
-		driver = new WebCrawl();
+		driverSpy = spy(new WebCrawl());
+		mockCL = mock(CommandLine.class);
+		driverSpy.setCrawler(mock(Crawler.class));
+		driverSpy.setCommandLine(mockCL);
+		mockQuery = spy(new Query());
+		when(mockQuery.getOptionalTerms()).thenReturn(new ArrayList<String>(Arrays.asList("boston", "bombing")));
 	}
 
 	@Test
 	public void testAddSearchQuery() {
-		assertEquals("A standard search query was not properly created", "boston bombing", driver.addSearchQuery("boston bombing").getQueryString());
-		assertEquals("A standard search query was not properly added to the queries list", 1, driver.getQueries().size());
-		driver.clear();
-		assertEquals("A search query with required and excluded terms was not properly created", "boston +bombing -marathon", driver.addSearchQuery("boston +bombing -marathon").getQueryString());
-		assertEquals("A search query with required and excluded terms was not properly added", 1, driver.getQueries().size());
-		driver.clear();
-		assertEquals("An empty search query was not properly created", "", driver.addSearchQuery("").getQueryString());
-		assertEquals("An empty search query was erroneously added to the queries list", 0, driver.getQueries().size());
-
+		assertEquals("A standard search query was not properly created", "boston bombing", driverSpy.addSearchQuery("boston bombing").getQueryString());
+		assertEquals("A standard search query was not properly added to the queries list", 1, driverSpy.getQueries().size());
 	}
 
 	@Test
-	public void testAddRedditQueries() {
-		driver.loadCommandLine(argsNoSubreddit);
-		driver.addRedditQueries(driver.addSearchQuery("boston bombing"));
-		assertEquals("A standard search query was not added to the reddit queries properly", 1, driver.getRedditQueries().size());
-		assertEquals("A standard search query was not integrated into reddit properly", "", driver.getRedditQueries().get(0).getSubreddit());
+	public void testAddSearchQueryComplex() {
+		assertEquals("A search query with required and excluded terms was not properly created", "boston +bombing -marathon", driverSpy.addSearchQuery("boston +bombing -marathon").getQueryString());
+		assertEquals("A search query with required and excluded terms was not properly added", 1, driverSpy.getQueries().size());
+	}
 
-		driver.clear();
-		driver.loadCommandLine(argsAll);
-		driver.addRedditQueries(driver.addSearchQuery("boston bombing"));
-		assertEquals("A query for both the main reddit and a subreddit were not added properly", 2, driver.getRedditQueries().size());
+	@Test
+	public void testAddSearchQueryEmpty() {
+		assertEquals("An empty search query was not properly created", "", driverSpy.addSearchQuery("").getQueryString());
+		assertEquals("An empty search query was erroneously added to the queries list", 0, driverSpy.getQueries().size());
+	}
+
+	@Test
+	public void testAddRedditQueriesMain() {
+		when(mockCL.getOptionValue("subreddit")).thenReturn(null);
+		driverSpy.addRedditQueries(mockQuery);
+		assertEquals("A standard search query was not added to the reddit queries properly", 1, driverSpy.getRedditQueries().size());
+		assertEquals("A standard search query was not integrated into reddit properly", "", driverSpy.getRedditQueries().get(0).getSubreddit());
+	}
+
+	@Test
+	public void testAddRedditQueriesMainAndSubreddit() {
+		when(mockCL.getOptionValue("subreddit")).thenReturn("all,inthenews");
+		driverSpy.addRedditQueries(mockQuery);
+		assertEquals("A query for both the main reddit and a subreddit were not added properly", 2, driverSpy.getRedditQueries().size());
 
 		String subreddit = "";
 		boolean hasMainReddit = false;
-		for(Query q : driver.getRedditQueries()) {
-			if(q.getSubreddit().length() > 0) subreddit = q.getSubreddit();
-			else hasMainReddit = true;
+		for(Query q : driverSpy.getRedditQueries()) {
+			if(q.getSubreddit().length() > 0) {
+				subreddit = q.getSubreddit();
+			} else {
+				hasMainReddit = true;
+			}
 		}
 
 		assertTrue("A query for both the main reddit and a subreddit did not add the main reddit query properly", hasMainReddit);
 		assertEquals("A query for both the main reddit and a subreddit did not add the proper subreddit query", "inthenews", subreddit);
+	}
 
-		driver.clear();
-		driver.loadCommandLine(argsJustSubreddit);
-		driver.addRedditQueries(driver.addSearchQuery("boston bombing"));
-		assertEquals("A query for just a subreddit did not add the subreddit properly", 1, driver.getRedditQueries().size());
-		assertEquals("A query for just a subreddit did not add right subreddit name", "inthenews", driver.getRedditQueries().get(0).getSubreddit());
+	@Test
+	public void testAddRedditQueriesSubreddit() {
+		when(mockCL.getOptionValue("subreddit")).thenReturn("inthenews");
+		driverSpy.addRedditQueries(mockQuery);
+		assertEquals("A query for just a subreddit did not add the subreddit properly", 1, driverSpy.getRedditQueries().size());
+		assertEquals("A query for just a subreddit did not add right subreddit name", "inthenews", driverSpy.getRedditQueries().get(0).getSubreddit());
+	}
 
-		driver.clear();
-		driver.loadCommandLine(argsEmptySubreddit);
-		driver.addRedditQueries(driver.addSearchQuery("boston bombing"));
-		assertEquals("A query with an empty subreddit value did not add the right number of subreddit queries", 1, driver.getRedditQueries().size());
-		assertEquals("A query with an empty subreddit value gave the added query a subreddit", "", driver.getRedditQueries().get(0).getSubreddit());
+	@Test
+	public void testAddRedditQueriesEmptySubreddit() {
+		when(mockCL.getOptionValue("subreddit")).thenReturn("");
+		driverSpy.addRedditQueries(mockQuery);
+		assertEquals("A query with an empty subreddit value did not add the right number of subreddit queries", 1, driverSpy.getRedditQueries().size());
+		assertEquals("A query with an empty subreddit value gave the added query a subreddit", "", driverSpy.getRedditQueries().get(0).getSubreddit());
 	}
 
 	@Test
 	public void testAddRSSLinks() {
-		driver.loadCommandLine(argsAll);
-		driver.addRSSLinks();
-		assertEquals("A single RSS feed link was not properly added to the list of RSS queries", 1, driver.getRssLinks().size());
+		when(mockCL.getOptionValue("rss")).thenReturn("http://rss.cnn.com/rss/cnn_topstories.rss");
+		driverSpy.addRSSLinks();
+		assertEquals("A single RSS feed link was not properly added to the list of RSS queries", 1, driverSpy.getRssLinks().size());
+	}
 
-		driver.clear();
-		driver.loadCommandLine(argsNoRssLink);
-		driver.addRSSLinks();
-		assertEquals("A query with no rss option provided erroneously added element(s) to the list of RSS links", 0, driver.getRssLinks().size());
+	@Test
+	public void testAddRSSLinksNone() {
+		when(mockCL.getOptionValue("rss")).thenReturn(null);
+		driverSpy.addRSSLinks();
+		assertEquals("A query with no rss option provided erroneously added element(s) to the list of RSS links", 0, driverSpy.getRssLinks().size());
+	}
 
-		driver.clear();
-		driver.loadCommandLine(argsEmptyRssLink);
-		driver.addRSSLinks();
-		assertEquals("A query with an empty rss option provided erroneously added element(s) to the list of RSS links", 0, driver.getRssLinks().size());
+	@Test
+	public void testAddRSSLinksEmpty() {
+		when(mockCL.getOptionValue("rss")).thenReturn("");
+		driverSpy.addRSSLinks();
+		assertEquals("A query with an empty rss option provided erroneously added element(s) to the list of RSS links", 0, driverSpy.getRssLinks().size());
+	}
 
-		driver.clear();
-		driver.loadCommandLine(argsMultipleRssLinks);
-		driver.addRSSLinks();
-		assertEquals("A query with multiple rss links provided added the incorrect number to the list of RSS links", 2, driver.getRssLinks().size());
+	@Test
+	public void testAddRSSLinksMultiple() {
+		when(mockCL.getOptionValue("rss")).thenReturn("http://rss.cnn.com/rss/cnn_topstories.rss,http://rss.cnn.com/rss/cnn_topstories.rss");
+		driverSpy.addRSSLinks();
+		assertEquals("A query with multiple rss links provided added the incorrect number to the list of RSS links", 2, driverSpy.getRssLinks().size());
 	}
 
 	@Test
 	public void testAddEngines() {
-		WebCrawl crawlAll = new WebCrawl(argsAll);
-		assertEquals("Query for all providers created an incorrect number of search engines", 4, crawlAll.getEngines().size());
-		for(SearchEngine engine : crawlAll.getEngines()) {
-			if(engine instanceof GoogleSearchEngine) assertEquals("Google search engine received the incorrect query list", 2, engine.getQueryQueue().size());
-			else if(engine instanceof GoogleNewsSearchEngine) assertEquals("Google news search engine received the incorrect query list", 2, engine.getQueryQueue().size());
-			else if(engine instanceof RedditSearchEngine) assertEquals("Reddit search engine received the incorrect query list", 4, engine.getQueryQueue().size());
-			else if(engine instanceof RSSEngine) assertEquals("RSS search engine received the incorrect query list", 1, engine.getQueryQueue().size());
-		}
+		when(mockCL.getOptionValue("provider")).thenReturn("google,news,reddit,rss");
+		when(driverSpy.getQueries()).thenReturn(new ArrayList<Query>(Arrays.asList(new Query(), new Query())));
+		when(driverSpy.getRedditQueries()).thenReturn(new ArrayList<Query>(Arrays.asList(new Query(), new Query(),
+				new Query(), new Query())));
+		when(driverSpy.getRssLinks()).thenReturn(new ArrayList<Query>(Arrays.asList(new Query())));
+
+		driverSpy.addEngines();
+		assertEquals("Query for all providers created an incorrect number of search engines", 4, driverSpy.getEngines().size());
 	}
 
 	@Test
 	public void testSetResultCount() {
-		driver.loadCommandLine(argsAll);
-		driver.setResultCount();
-		assertEquals("Setting standard result count returned the incorrect result count", 20, driver.getResults());
-
-		driver.clear();
-		driver.loadCommandLine(argsNegResultCount);
-		driver.setResultCount();
-		assertEquals("Setting negative result count did not return default result count", WebCrawl.DEFAULT_RESULT_COUNT, driver.getResults());
-
-		driver.clear();
-		driver.loadCommandLine(argsZeroResultCount);
-		driver.setResultCount();
-		assertEquals("Setting zero result count did not return default result count", WebCrawl.DEFAULT_RESULT_COUNT, driver.getResults());
-
-		driver.clear();
-		driver.loadCommandLine(argsNaNResultCount);
-		driver.setResultCount();
-		assertEquals("Setting non-number result count did not return default result count", WebCrawl.DEFAULT_RESULT_COUNT, driver.getResults());
+		when(mockCL.getOptionValue("result-count")).thenReturn("20");
+		driverSpy.setResultCount();
+		assertEquals("Setting standard result count returned the incorrect result count", 20, driverSpy.getResults());
 	}
 
 	@Test
-	public void testParseQuery() {
+	public void testSetResultCountNegative() {
+		when(mockCL.getOptionValue("result-count")).thenReturn("-20");
+		driverSpy.setResultCount();
+		assertEquals("Setting negative result count did not return default result count", WebCrawl.DEFAULT_RESULT_COUNT, driverSpy.getResults());
+	}
+
+	@Test
+	public void testSetResultCountZero() {
+		when(mockCL.getOptionValue("result-count")).thenReturn("0");
+		driverSpy.setResultCount();
+		assertEquals("Setting zero result count did not return default result count", WebCrawl.DEFAULT_RESULT_COUNT, driverSpy.getResults());
+	}
+
+	@Test
+	public void testSetResultCountNaN() {
+		when(mockCL.getOptionValue("result-count")).thenReturn("hello");
+		driverSpy.setResultCount();
+		assertEquals("Setting non-number result count did not return default result count", WebCrawl.DEFAULT_RESULT_COUNT, driverSpy.getResults());
+	}
+
+	@Test
+	public void testParseQueryOptional() {
 		Map<String, ArrayList<String>> params = WebCrawl.parseQuery("boston bombing");
 		assertEquals("A standard query did not return the right number of terms", 2, params.get("optional").size());
 		assertEquals("A standard query added erroneous required and/or excluded terms", 0, params.get("excluded").size() + params.get("required").size());
+	}
 
-		params = WebCrawl.parseQuery("boston +bombing -mit");
+	@Test
+	public void testParseQueryOptionalRequiredExcluded() {
+		Map<String, ArrayList<String>> params = WebCrawl.parseQuery("boston +bombing -mit");
 		assertEquals("A query with required, optional, and excluded terms did not add optional terms to the map properly", 1, params.get("optional").size());
 		assertEquals("A query with required, optional, and excluded terms did not add required terms to the map properly", 1, params.get("required").size());
 		assertEquals("A query with required, optional, and excluded terms did not add excluded terms to the map properly", 1, params.get("excluded").size());
+	}
 
-		params = WebCrawl.parseQuery("");
+	@Test
+	public void testParseQueryEmpty() {
+		Map<String, ArrayList<String>> params = WebCrawl.parseQuery("");
 		assertEquals("An empty query did not add anyterms to the query", 0, params.get("optional").size() + params.get("required").size() + params.get("excluded").size());
 	}
 }
