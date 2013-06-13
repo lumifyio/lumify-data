c6d4bd76df72c9423cba11c1299b3508c13f538f
Ryan Lanman <rmlanman@gmail.com>
2013-06-13 17:37:44 -0400

    Switching to adapters rather than no-op interfaces

--- messaging/src/main/java/com/altamiracorp/messaging/synchronization/SynchronizationSessionListener.java
+++ messaging/src/main/java/com/altamiracorp/messaging/synchronization/SynchronizationSessionListener.java
@@ -2,44 +2,23 @@ package com.altamiracorp.messaging.synchronization;
 
 import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.Broadcaster;
-import org.atmosphere.cpr.BroadcasterListener;
+import org.atmosphere.cpr.BroadcasterListenerAdapter;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class SynchronizationSessionListener implements BroadcasterListener {
+public class SynchronizationSessionListener extends BroadcasterListenerAdapter {
 
-	public void onAddAtmosphereResource(Broadcaster arg0,
-			AtmosphereResource arg1) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void onComplete(Broadcaster arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void onPostCreate(Broadcaster arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void onPreDestroy(Broadcaster arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
+	@Override
 	public void onRemoveAtmosphereResource(Broadcaster b,
 			AtmosphereResource resource) {
 		if (SyncUtil.isSyncTopic(b.getID())) {
-			System.out.println("Resource removed from a sync topic: " + b.getID());
 			String sessionId = SyncUtil.topicToSyncSessionId(b.getID());
 			SynchronizationBus bus = SynchronizationBus.getSyncBus(resource
 					.getAtmosphereConfig().getServletContext());
 			SynchronizationRequest session = bus.getSession(sessionId);
 			if (session != null && session.getUserIds().size() == 1) {
 				bus.endSession(sessionId);
-				JSONObject syncEndMsg = new JSONObject ();
+				JSONObject syncEndMsg = new JSONObject();
 				try {
 					JSONObject sync = new JSONObject();
 					sync.put("initiator", false);
--- messaging/src/main/java/com/altamiracorp/messaging/users/UserChangeListener.java
+++ messaging/src/main/java/com/altamiracorp/messaging/users/UserChangeListener.java
@@ -1,27 +1,19 @@
 package com.altamiracorp.messaging.users;
 
-import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.Broadcaster;
 import org.atmosphere.cpr.BroadcasterFactory;
-import org.atmosphere.cpr.BroadcasterListener;
+import org.atmosphere.cpr.BroadcasterListenerAdapter;
 import org.json.JSONException;
 
-public class UserChangeListener implements BroadcasterListener {
+public class UserChangeListener extends BroadcasterListenerAdapter {
 
 	private static final String USER_CHANGE_TOPIC = "userChanges";
 
-	public void onAddAtmosphereResource(Broadcaster b,
-			AtmosphereResource resource) {
-		// no-op
-	}
-
-	public void onComplete(Broadcaster b) {
-		// no-op
-	}
-
+	@Override
 	public void onPostCreate(Broadcaster b) {
 		if (UserUtil.isUserTopic(b.getID())) {
-			UserUtil.changeUserStatus(UserUtil.createUserFromTopic(b.getID()), "online");
+			UserUtil.changeUserStatus(UserUtil.createUserFromTopic(b.getID()),
+					"online");
 			try {
 				BroadcasterFactory.getDefault().lookup(USER_CHANGE_TOPIC)
 						.broadcast(UserUtil.getSessionUsers().toJson());
@@ -31,21 +23,18 @@ public class UserChangeListener implements BroadcasterListener {
 		}
 	}
 
+	@Override
 	public void onPreDestroy(Broadcaster b) {
 		if (UserUtil.isUserTopic(b.getID())) {
 			try {
-				UserUtil.changeUserStatus(UserUtil.createUserFromTopic(b.getID()), "offline");
+				UserUtil.changeUserStatus(
+						UserUtil.createUserFromTopic(b.getID()), "offline");
 				BroadcasterFactory.getDefault().lookup(USER_CHANGE_TOPIC, true)
 						.broadcast(UserUtil.getSessionUsers().toJson());
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
 		}
-	}
-
-	public void onRemoveAtmosphereResource(Broadcaster b,
-			AtmosphereResource resource) {
-		// no-op
 	}
 
 }
