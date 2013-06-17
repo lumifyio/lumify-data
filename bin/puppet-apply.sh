5eca647b76cf42eff37f0ada73a401094c599f76
David Singley <dsingley@nearinfinity.com>
2013-06-17 18:04:50 -0400

    new script to run puppet within the VM

---
+++ bin/puppet-apply.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eu
+
+module_dirs=$(mount | awk '/modules.*vboxsf/ {print $1}')
+module_path=$(echo ${module_dirs} | sed -e 's/ /:/g')
+manifests_dir=$(mount | awk '/manifests.*vboxsf/ {print $1}')
+manifest=red-dawn.pp
+
+sudo puppet apply --modulepath ${module_path} ${manifests_dir}/${manifest}
