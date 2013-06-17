dac37fd5aa4241a4faa9561b9842e13e798ec418
Jeff Principe <jprincip@nearinfinity.com>
2013-06-17 17:28:32 -0400

    Created test class for searcher

---
+++ dictionary-seed/src/test/java/com/altamiracorp/reddawn/DictionarySearcherTest.java
@@ -0,0 +1,19 @@
+package com.altamiracorp.reddawn;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnit4ClassRunner.class)
+public class DictionarySearcherTest {
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testSearch() throws Exception {
+
+	}
+}
