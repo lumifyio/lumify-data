4370fe9f0cf6d2e3fff199fee970470d951eb2b7
Jeff Principe <jprincip@nearinfinity.com>
2013-06-17 17:23:49 -0400

    Added basic SPARQL search

--- dictionary-seed/src/main/java/com/altamiracorp/reddawn/DictionarySearcher.java
+++ dictionary-seed/src/main/java/com/altamiracorp/reddawn/DictionarySearcher.java
@@ -1,4 +1,63 @@
 package com.altamiracorp.reddawn;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class DictionarySearcher {
+	public static final String ALL = "Resource",
+			PLACE = "Place",
+			PERSON = "Person",
+			WORK = "Work",
+			SPECIES = "Species",
+			ORGANIZATION = "Organisation";
+
+	public String baseURL;
+
+	public DictionarySearcher() {
+		baseURL = "http://dbpedia.org/sparql/?format=json&query=";
+	}
+
+	public String search(String type) throws IOException, JSONException {
+		String response = httpRequest(getUrl(type));
+
+		JSONObject json = new JSONObject(response.toString());
+		JSONArray results = json.getJSONObject("results").getJSONArray("bindings");
+
+		StringBuilder output = new StringBuilder();
+
+		for (int i = 0; i < results.length(); i++) {
+			String name = results.getJSONObject(i).getJSONObject("name").getString("value");
+			output.append(name);
+			if (i != results.length() - 1) {
+				output.append("\n");
+			}
+		}
+
+		return output.toString();
+	}
+
+	private String httpRequest(String url) throws IOException {
+		URL query = new URL(url);
+		URLConnection connection = query.openConnection();
+		BufferedReader inputStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		StringBuilder response = new StringBuilder();
+		String line;
+		while ((line = inputStream.readLine()) != null) {
+			response.append(line);
+		}
+
+		return response.toString();
+	}
+
+	private String getUrl(String type) {
+		return baseURL + "PREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0A%0D%0ASELECT+DISTINCT+%3Fname+" +
+				"WHERE+%7B%0D%0A+++++%3Fperson+a+dbo%3A" + type + "+%3B+foaf%3Aname+%3Fname+.%0D%0A%7D%0D%0A";
+	}
 }
--- dictionary-seed/src/main/java/com/altamiracorp/reddawn/DictionarySeederDriver.java
+++ dictionary-seed/src/main/java/com/altamiracorp/reddawn/DictionarySeederDriver.java
@@ -1,13 +1,12 @@
 package com.altamiracorp.reddawn;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class DictionarySeederDriver {
+	public static void main(String[] args) throws IOException, JSONException {
+		DictionarySearcher searcher = new DictionarySearcher();
+		System.out.println(searcher.search(DictionarySearcher.ORGANIZATION));
+	}
 }
