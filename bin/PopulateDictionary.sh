40c538163ef44144bff9f13803bc3ed242549d39
Jeff Principe <jprincip@nearinfinity.com>
2013-06-18 16:29:20 -0400

    Added shell script

---
+++ bin/PopulateDictionary.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+classpath=$(${DIR}/classpath.sh core)
+if [ $? -ne 0 ]; then
+  echo "${classpath}"
+  exit
+fi
+
+DIRECTORY=${DIR}/../dictionary-files/
+
+while [ $# -ne 0 ]
+do
+    case "$1" in
+        --types=*)
+            TYPES=`cut -d "=" -f 2 <<< "$1"` ;;
+        --directory=*)
+            DIRECTORY=${DIR}/`cut -d "=" -f 2 <<< "$1"` ;;
+    shift
+done
+
+declare -a PARAMS=("--directory=${DIRECTORY}")
+if [ -n "$TYPES" ]
+then
+    PARAMS=("${PARAMS[@]}" "--types=${TYPES}")
+fi
+
+java \
+-Dfile.encoding=UTF-8 \
+-classpath ${classpath} \
+com.altamiracorp.reddawn.cmdline.FileImport \
+PARAMS
