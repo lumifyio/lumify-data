3c461140692756fbcb06cfc0b6d946e8c472be62
Jeff Principe <jprincip@nearinfinity.com>
2013-06-18 12:07:03 -0400

    Improved logging and error reporting for searches

--- dictionary-seed/src/main/java/com/altamiracorp/reddawn/DictionarySearcher.java
+++ dictionary-seed/src/main/java/com/altamiracorp/reddawn/DictionarySearcher.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -26,58 +27,76 @@ public class DictionarySearcher {
 		baseURL = "http://dbpedia.org/sparql/?format=json&query=";
 	}
 
-	public String search(String type) throws IOException, JSONException {
+	public String search(String type) {
 		StringBuilder output = new StringBuilder();
 		int totalResultCount = 0;
 		int resultOffset = 0;
+        final int MAX_RESULTS_PER_SEARCH = 50000;
 
-		do {
-			System.out.print("Querying... ");
+        System.out.println("Querying... ");
+
+        do {
+            System.out.print("Fetching results " + (resultOffset + 1) + "-" +
+                    (resultOffset + MAX_RESULTS_PER_SEARCH) + "... ");
 			String response = httpRequest(getUrl(type, resultOffset));
-			System.out.println("DONE");
+            System.out.println("DONE");
+            try {
+                JSONObject json = new JSONObject(response.toString());
+                JSONArray results = json.getJSONObject("results").getJSONArray("bindings");
 
-			JSONObject json = new JSONObject(response.toString());
-			JSONArray results = json.getJSONObject("results").getJSONArray("bindings");
+                totalResultCount += results.length();
 
-			totalResultCount += results.length();
+                for (int i = 0; i < results.length(); i++) {
+                    String name = results.getJSONObject(i).getJSONObject("name").getString("value");
+                    output.append(name);
+                    if (i != results.length() - 1) {
+                        output.append("\n");
+                    }
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException("Could not parse the result set for the search of " + type +
+                        ", offset: " + resultOffset);
+            }
 
-			for (int i = 0; i < results.length(); i++) {
-				String name = results.getJSONObject(i).getJSONObject("name").getString("value");
-				output.append(name);
-				if (i != results.length() - 1) {
-					output.append("\n");
-				}
-			}
-
-			resultOffset += 50000;
+			resultOffset += MAX_RESULTS_PER_SEARCH;
 
 		} while(totalResultCount - resultOffset == 0);
 
-		System.out.println("Found " + totalResultCount + " matches for " + type);
+        System.out.println("Found " + totalResultCount + " matches for \"" + type + "\"");
 		return output.toString();
 	}
 
-	private String httpRequest(String url) throws IOException {
-		URL query = new URL(url);
-		URLConnection connection = query.openConnection();
-		BufferedReader inputStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		StringBuilder response = new StringBuilder();
-		String line;
-		while ((line = inputStream.readLine()) != null) {
-			response.append(line);
-		}
+	private String httpRequest(String url) {
+        StringBuilder response = new StringBuilder();
 
-		return response.toString();
+        try {
+            URL query = new URL(url);
+            URLConnection connection = query.openConnection();
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = inputStream.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("The search URL was malformed");
+        } catch (IOException e) {
+            throw new RuntimeException("Could not fetch search results from server");
+        }
+
+        return response.toString();
 	}
 
-	private String getUrl(String type, int offset) throws UnsupportedEncodingException {
+	private String getUrl(String type, int offset) {
 		String query = "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
 				"SELECT ?name WHERE{?place a dbo:" + type + ";foaf:name ?name.}\n" +
 				"LIMIT 50000\nOFFSET " + offset;
 
-		return baseURL + URLEncoder.encode(query, "UTF-8");
-
-//		return baseURL + "PREFIX+dbo%3A+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2F%3E%0D%0A%0D%0ASELECT+DISTINCT+%3Fname+" +
-//				"WHERE+%7B%0D%0A+++++%3Fperson+a+dbo%3A" + type + "+%3B+foaf%3Aname+%3Fname+.%0D%0A%7D%0D%0A";
+		String url;
+        try {
+            url = baseURL + URLEncoder.encode(query, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("The search URL " + query + " could not be encoded properly");
+        }
+        return url;
 	}
 }
