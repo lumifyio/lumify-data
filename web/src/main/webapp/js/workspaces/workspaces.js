0633556ed9996e46554826affe832ee94e7c03f1
Jason Harwig <jharwig@nearinfinity.com>
2013-06-18 12:02:19 -0400

    add buttons for add/delete workspace

--- web/src/main/webapp/js/workspaces/workspaces.js
+++ web/src/main/webapp/js/workspaces/workspaces.js
@@ -12,12 +12,23 @@ define([
 
         this.defaultAttrs({
             listSelector: 'ul.nav-list',
-            workspaceListItemSelector: 'ul.nav-list li'
+            workspaceListItemSelector: 'ul.nav-list li',
+            addNewSelector: 'button.new',
+            deleteSelector: 'button.delete'
         });
 
         this.onWorkspaceItemClick = function( event ) {
             var rowKey = $(event.target).parents('li').data('rowkey');
             this.trigger( document, 'switchWorkspace', { rowKey: rowKey });
+        };
+
+        this.onAddNew = function() {
+            // TODO
+        };
+
+        this.onDelete = function( event ) {
+            var rowKey = $(event.target).parents('li').data('rowkey');
+            // TODO: delete workspace
         };
 
         this.onWorkspaceSwitch = function( event, data ) {
@@ -30,6 +41,7 @@ define([
                     }
                 });
         };
+
 
         this.after( 'initialize', function() {
 
@@ -53,6 +65,8 @@ define([
             this.on( document, 'switchWorkspace', this.onWorkspaceSwitch );
             this.on( 'click', {
                 workspaceListItemSelector: this.onWorkspaceItemClick,
+                addNewSelector: this.onAddNew,
+                deleteSelector: this.onDelete
             });
         });
     }
