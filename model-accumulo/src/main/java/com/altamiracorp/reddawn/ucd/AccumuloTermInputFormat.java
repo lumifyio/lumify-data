843c486c1967aef90fac945f67ab9068d0d661bc
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2013-06-18 10:32:37 -0400

    Refactor similar code from each AccumuloInputFormat implementation

--- model-accumulo/src/main/java/com/altamiracorp/reddawn/ucd/AccumuloTermInputFormat.java
+++ model-accumulo/src/main/java/com/altamiracorp/reddawn/ucd/AccumuloTermInputFormat.java
@@ -1,27 +1,12 @@
 package com.altamiracorp.reddawn.ucd;
 
-import com.altamiracorp.reddawn.model.AccumuloHelper;
-import com.altamiracorp.reddawn.ucd.sentence.Sentence;
-import com.altamiracorp.reddawn.ucd.sentence.SentenceRepository;
 import com.altamiracorp.reddawn.ucd.term.Term;
 import com.altamiracorp.reddawn.ucd.term.TermRepository;
-import org.apache.accumulo.core.client.RowIterator;
 import org.apache.accumulo.core.client.mapreduce.AccumuloInputFormat;
-import org.apache.accumulo.core.client.mapreduce.InputFormatBase;
-import org.apache.accumulo.core.data.Key;
-import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
-import org.apache.accumulo.core.util.PeekingIterator;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.RecordReader;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
-import java.io.IOException;
-import java.util.Map;
-
-public class AccumuloTermInputFormat extends InputFormatBase<Text, Term> {
+public class AccumuloTermInputFormat extends AccumuloBaseInputFormat<Term, TermRepository> {
     private TermRepository termRepository = new TermRepository();
 
     public static void init(Job job, String username, byte[] password, Authorizations authorizations, String zookeeperInstanceName, String zookeeperServerNames) {
@@ -30,30 +15,7 @@ public class AccumuloTermInputFormat extends InputFormatBase<Text, Term> {
     }
 
     @Override
-    public RecordReader<Text, Term> createRecordReader(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
-        return new RecordReaderBase<Text, Term>() {
-            RowIterator rowIterator;
-
-            @Override
-            public void initialize(InputSplit inSplit, TaskAttemptContext attempt) throws IOException {
-                super.initialize(inSplit, attempt);
-                this.rowIterator = new RowIterator(scannerIterator);
-                this.currentK = new Text();
-                this.currentV = null;
-            }
-
-            @Override
-            public boolean nextKeyValue() throws IOException, InterruptedException {
-                if (!rowIterator.hasNext()) {
-                    return false;
-                }
-                PeekingIterator<Map.Entry<Key, Value>> it = new PeekingIterator<Map.Entry<Key, Value>>(rowIterator.next());
-                this.currentV = termRepository.fromRow(AccumuloHelper.accumuloRowToRow(termRepository.getTableName(), it));
-                this.numKeysRead = this.rowIterator.getKVCount();
-                this.currentKey = new Key(this.currentV.getRowKey().toString());
-                this.currentK = new Text(this.currentKey.getRow());
-                return true;
-            }
-        };
+    public TermRepository getRepository() {
+        return termRepository;
     }
 }
