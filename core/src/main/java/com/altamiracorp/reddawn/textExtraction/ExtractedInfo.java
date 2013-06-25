722658a354e12f7b944ff63d5b6d422ca1c6a07a
Jeff Principe <reddawn@localhost.localdomain>
2013-06-25 12:00:17 -0400

    Added more detailed metadata to artifacts

--- core/src/main/java/com/altamiracorp/reddawn/textExtraction/ExtractedInfo.java
+++ core/src/main/java/com/altamiracorp/reddawn/textExtraction/ExtractedInfo.java
@@ -7,6 +7,11 @@ public class ExtractedInfo {
 	private String subject = "";
 	private String text = "";
 	private Date date;
+    private String url = "";
+    private String type = "";
+    private String extUrl = "";
+    private String srcType = "";
+    private Long retrievalTime = 0l;
 
 	public void setText(String text) {
 		this.text = text;
@@ -39,4 +44,44 @@ public class ExtractedInfo {
 	public void setMediaType(String mediaType) {
 		this.mediaType = mediaType;
 	}
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getExtUrl() {
+        return extUrl;
+    }
+
+    public void setExtUrl(String extUrl) {
+        this.extUrl = extUrl;
+    }
+
+    public String getSrcType() {
+        return srcType;
+    }
+
+    public void setSrcType(String srcType) {
+        this.srcType = srcType;
+    }
+
+    public Long getRetrievalTime() {
+        return retrievalTime;
+    }
+
+    public void setRetrievalTime(Long retrievalTime) {
+        this.retrievalTime = retrievalTime;
+    }
 }
