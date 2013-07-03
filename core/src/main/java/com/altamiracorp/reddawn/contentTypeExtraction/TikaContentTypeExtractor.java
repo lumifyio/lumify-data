ef973ad244ea4068231b0a3edcaa8bcf450a3f9a
Susan Feng <sfeng88@gmail.com>
2013-07-03 11:21:58 -0400

    Used Tika to extract content type for all artifacts and tested it using m4v, m2v, mp4, jpg, pdf, txt

---
+++ core/src/main/java/com/altamiracorp/reddawn/contentTypeExtraction/TikaContentTypeExtractor.java
@@ -0,0 +1,30 @@
+package com.altamiracorp.reddawn.contentTypeExtraction;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+
+import java.io.InputStream;
+
+public class TikaContentTypeExtractor implements ContentTypeExtractor{
+
+    private static final String MIME_TYPE_KEY = "Content-Type";
+
+    @Override
+    public void setup (Mapper.Context context){
+    }
+
+    @Override
+    public String extract (InputStream in) throws Exception{
+        Parser parser = new AutoDetectParser();
+        BodyContentHandler handler = new BodyContentHandler (10000000);
+        Metadata metadata = new Metadata();
+        ParseContext ctx = new ParseContext ();
+        parser.parse (in, handler, metadata, ctx);
+
+        return metadata.get(MIME_TYPE_KEY);
+    }
+}
---
+++ core/src/test/java/com/altamiracorp/reddawn/contentTypeExtraction/TikaContentTypeExtractorTest.java
@@ -0,0 +1,44 @@
+package com.altamiracorp.reddawn.contentTypeExtraction;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class TikaContentTypeExtractorTest{
+
+    @Test
+    public void testTikaExtractContentType () throws Exception{
+        TikaContentTypeExtractor tikaContentTypeExtractor = new TikaContentTypeExtractor();
+
+        // Testing .m4v files
+        InputStream h264 = TikaContentTypeExtractor.class.getResourceAsStream("/H_264.m4v");
+        assertEquals("video/x-m4v", tikaContentTypeExtractor.extract(h264));
+
+        // Testing .m2v files
+        InputStream mpeg2 = TikaContentTypeExtractor.class.getResourceAsStream("/MPEG-2.m2v");
+        assertEquals("video/mpeg", tikaContentTypeExtractor.extract(mpeg2));
+
+        // Testing .mp4 files
+        InputStream mpeg4 = TikaContentTypeExtractor.class.getResourceAsStream("/MPEG-4.mp4");
+        assertEquals("video/mp4", tikaContentTypeExtractor.extract(mpeg4));
+
+        // Testing .txt files
+        InputStream txt = TikaContentTypeExtractor.class.getResourceAsStream("/hello.txt");
+        assertEquals("text/plain; charset=ISO-8859-1", tikaContentTypeExtractor.extract(txt));
+
+        // Testing .pdf files
+        InputStream pdf = TikaContentTypeExtractor.class.getResourceAsStream("/hello.pdf");
+        assertEquals("application/pdf", tikaContentTypeExtractor.extract(pdf));
+
+        // Testing .jpg files
+        InputStream jpg = TikaContentTypeExtractor.class.getResourceAsStream("/cat.jpg");
+        assertEquals("image/jpeg", tikaContentTypeExtractor.extract(jpg));
+
+    }
+
+}
