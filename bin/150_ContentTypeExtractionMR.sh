d83a32dfb242187b699535049d3d901d3ce097f8
Susan Feng <sfeng88@gmail.com>
2013-07-03 13:24:52 -0400

    Shell scrip to run ContentTypeMR prior to TextExtractionMR

---
+++ bin/150_ContentTypeExtractionMR.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+classpath=$(${DIR}/classpath.sh core)
+if [ $? -ne 0 ]; then
+  echo "${classpath}"
+  exit
+fi
+
+if [ "${VIRTUALIZATION_DISABLED}" = 'true' ]; then
+  ip=localhost
+else
+  ip=192.168.33.10
+fi
+
+java \
+-Dfile.encoding=UTF-8 \
+-classpath ${classpath} \
+com.altamiracorp.reddawn.contentTypeExtraction.ContentTypeMR \
+--zookeeperInstanceName=reddawn \
+--zookeeperServerNames=${ip} \
+--blurControllerLocation=${ip}:40010 \
+--blurPath=hdfs://${ip}/blur \
+--username=root \
+--password=password \
+--classname=com.altamiracorp.reddawn.contentTypeExtraction.TikaContentType
