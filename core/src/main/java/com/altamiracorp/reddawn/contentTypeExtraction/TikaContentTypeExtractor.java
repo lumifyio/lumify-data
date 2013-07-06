0364962c30a18a180f0a980ff4bedb69b7283dce
Susan Feng <susan.feng@altamiracorp.com>
2013-07-06 15:17:59 -0400

    Tika parser for content type

--- core/src/main/java/com/altamiracorp/reddawn/contentTypeExtraction/TikaContentTypeExtractor.java
+++ core/src/main/java/com/altamiracorp/reddawn/contentTypeExtraction/TikaContentTypeExtractor.java
@@ -7,29 +7,41 @@ import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.BodyContentHandler;
 
+import java.util.Properties;
+
+import java.io.IOException;
 import java.io.InputStream;
 
 public class TikaContentTypeExtractor implements ContentTypeExtractor{
-
     private static final String MIME_TYPE_KEY = "Content-Type";
+    private static final String PROPS_FILE = "tika-extractor.properties";
 
     @Override
-    public void setup (Mapper.Context context){
+    public void setup (Mapper.Context context) {
+        Properties tikaProperties = new Properties ();
+        try {
+            InputStream propsIn = Thread.currentThread().getContextClassLoader().getResourceAsStream(PROPS_FILE);
+            if (propsIn != null) {
+                tikaProperties.load(propsIn);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public String extract (InputStream in) throws Exception{
+    public String extract (InputStream in) throws Exception {
         Parser parser = new AutoDetectParser();
-        BodyContentHandler handler = new BodyContentHandler (10000000);
+        BodyContentHandler handler = new BodyContentHandler();
         Metadata metadata = new Metadata();
-        ParseContext ctx = new ParseContext ();
-        parser.parse (in, handler, metadata, ctx);
+        ParseContext ctx = new ParseContext();
+        parser.parse(in,  handler, metadata, ctx);
 
         String contentType = metadata.get(MIME_TYPE_KEY);
-        if (contentType == null) {
+        if (contentType == null){
             contentType = "";
         }
-
         return contentType;
     }
 }
