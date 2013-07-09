c79c2359bc9dbf84034dc4f632dd8f416c1bdb94
Jeff Principe <princjef@gmail.com>
2013-07-09 17:05:26 -0400

    Fixed merge conflict in StatementRowKey.java

--- model/src/main/java/com/altamiracorp/reddawn/ucd/statement/StatementRowKey.java
+++ model/src/main/java/com/altamiracorp/reddawn/ucd/statement/StatementRowKey.java
@@ -4,8 +4,6 @@ import com.altamiracorp.reddawn.model.RowKey;
 import com.altamiracorp.reddawn.model.RowKeyHelper;
 import com.altamiracorp.reddawn.ucd.predicate.PredicateRowKey;
 import com.altamiracorp.reddawn.ucd.term.TermRowKey;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class StatementRowKey extends RowKey {
     public StatementRowKey(String rowKey) {
@@ -18,30 +16,5 @@ public class StatementRowKey extends RowKey {
 
     public StatementRowKey(String subjectRowKey, String predicateLabel, String objectRowKey) {
         super(RowKeyHelper.buildMajor(subjectRowKey, predicateLabel, objectRowKey).toString());
-    }
-
-    public String getSubjectRowKey() {
-        return RowKeyHelper.splitOnMajorFieldSeperator(this.toString())[0];
-    }
-
-    public String getPredicateLabel() {
-        return RowKeyHelper.splitOnMajorFieldSeperator(this.toString())[1];
-    }
-
-    public String getObjectRowKey() {
-        return RowKeyHelper.splitOnMajorFieldSeperator(this.toString())[2];
-    }
-
-    @Override
-    public JSONObject toJson() {
-        try {
-            JSONObject json = super.toJson();
-            json.put("subjectRowKey", new TermRowKey(getSubjectRowKey()).toJson());
-            json.put("predicateLabel", new PredicateRowKey(getPredicateLabel()).toJson());
-            json.put("objectRowKey", new TermRowKey(getObjectRowKey()).toJson());
-            return json;
-        } catch (JSONException e) {
-            throw new RuntimeException("cannot get rowkey JSON", e);
-        }
     }
 }
