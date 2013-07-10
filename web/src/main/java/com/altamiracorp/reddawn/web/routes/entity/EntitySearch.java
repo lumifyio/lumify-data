0978e4b48132b7bb294cebe6f64340d01f29168b
Susan Feng <susan.feng@altamiracorp.com>
2013-07-10 12:00:10 -0400

    Changed to use blur term search

--- web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntitySearch.java
+++ web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntitySearch.java
@@ -1,6 +1,8 @@
 package com.altamiracorp.reddawn.web.routes.entity;
 
 import com.altamiracorp.reddawn.RedDawnSession;
+import com.altamiracorp.reddawn.search.SearchProvider;
+import com.altamiracorp.reddawn.search.TermSearchResult;
 import com.altamiracorp.reddawn.ucd.term.Term;
 import com.altamiracorp.reddawn.ucd.term.TermRepository;
 import com.altamiracorp.reddawn.web.Responder;
@@ -17,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+import java.util.Collection;
+
 // TODO: change this over to an Entity search once entities work
 public class EntitySearch implements Handler, AppAware {
     private TermRepository termRepository = new TermRepository();
@@ -31,9 +35,33 @@ public class EntitySearch implements Handler, AppAware {
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
         String query = request.getParameter("q");
         RedDawnSession session = app.getRedDawnSession(request);
-        List<Term> terms = termRepository.findByRowStartsWith(session.getModelSession(), query.toLowerCase());
-        JSONObject termsJson = termsToSearchResults(terms, request);
+        SearchProvider searchProvider = session.getSearchProvider();
+        Collection <TermSearchResult> termSearchResults = queryTerm (searchProvider, query);
+        JSONObject termsJson = termsToSearchResults(termSearchResults, request);
+        //List<Term> terms = termRepository.findByRowStartsWith(session.getModelSession(), query.toLowerCase());
+        //JSONObject termsJson = termsToSearchResults(terms, request);
         new Responder(response).respondWith(termsJson);
+    }
+
+    private Collection<TermSearchResult> queryTerm (SearchProvider searchProvider, String query) throws Exception{
+        return searchProvider.searchTerms(query);
+    }
+
+    private JSONObject termsToSearchResults (Collection <TermSearchResult> terms, HttpServletRequest request) throws JSONException{
+        JSONObject termsJson = new JSONObject();
+        for (TermSearchResult termSearchResult : terms){
+            JSONObject termObject = termToSearchResult (request, termSearchResult);
+        }
+        return termsJson;
+    }
+
+    private JSONObject termToSearchResult (HttpServletRequest request, TermSearchResult termSearchResult) throws JSONException{
+        JSONObject termJson = new JSONObject();
+        termJson.put("url", EntityByRowKey.getUrl(request, termSearchResult.getRowKey()));
+        termJson.put("rowKey", termSearchResult.getRowKey().toString());
+        termJson.put("sign", termSearchResult.getSign());
+        termJson.put("model", termSearchResult.getRowKey().getModelKey());
+        return termJson;
     }
 
     private JSONObject termsToSearchResults(List<Term> terms, HttpServletRequest request) throws JSONException {
