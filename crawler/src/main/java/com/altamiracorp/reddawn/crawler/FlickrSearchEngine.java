bde253c33dc461c82441a02983e2df28cedad194
Sam Woloszynski <sdw5fs@virginia.edu>
2013-07-11 13:48:07 -0400

    Wrote tests for creation of search query to use with Flickr API

--- crawler/src/main/java/com/altamiracorp/reddawn/crawler/FlickrSearchEngine.java
+++ crawler/src/main/java/com/altamiracorp/reddawn/crawler/FlickrSearchEngine.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class FlickrSearchEngine extends SearchEngine {
 
-    private final String BASE_URL = "http://api.flickr.photos.search/?";
+    private final String BASE_URL = "http://api.flickr.com/services/rest?method=flickr.photos.search";
     private final String API_KEY = "06e4190d750d2386f81d1afde77d7b38"; // for Sam's account (sam wolo)
     private final String NO_KNOWN_COPYRIGHT_RESTRICTIONS_LICENSE = "7";
     private final String USGOVT_WORK_LICENSE = "8";
@@ -13,7 +13,7 @@ public class FlickrSearchEngine extends SearchEngine {
     private final String GEO_ACCURACY = "7"; // scale is 1 (world) to 16 (city / street)
     private final String CONTENT_TYPE_ALL = "7"; // includes photos, screenshots, and other
     private final String CONTENT_TYPE_PHOTOS_AND_SCREENSHOTS = "4";
-    private final String EXTRAS = "description,license,date_upload,date_taken,owner_name,icon_server, " +
+    private final String EXTRAS = "description,license,date_upload,date_taken,owner_name,icon_server," +
             "original_format,last_update,geo,tags,machine_tags,o_dims,views,media";
     private final String PER_PAGE = "500";
 
@@ -35,7 +35,6 @@ public class FlickrSearchEngine extends SearchEngine {
         queryParams.put("privacy_filter", PRIVACY_FILTER);
         queryParams.put("accuracy", GEO_ACCURACY);
         queryParams.put("content_type", CONTENT_TYPE_ALL);
-        queryParams.put("content_type", CONTENT_TYPE_ALL);
         queryParams.put("extras", EXTRAS);
         queryParams.put("per_page", "" + perPage);
         queryParams.put("page", "" + page);
@@ -47,8 +46,17 @@ public class FlickrSearchEngine extends SearchEngine {
 
     protected TreeMap<String, String> processQuery(Query q) {
         TreeMap<String, String> querySearchTerms = new TreeMap<String, String>();
-        querySearchTerms.put("text", Utils.concatenate(q.getOptionalTerms(), "+") + "+" +
-                Utils.concatenate(q.getRequiredTerms(), "+"));
+        String queryString = "";
+        if (q.getOptionalTerms().size() > 0) {
+            queryString += Utils.concatenate(q.getOptionalTerms(), "+");
+        }
+        if (q.getRequiredTerms().size() > 0) {
+            queryString += "+" + Utils.concatenate(q.getRequiredTerms(), "+");
+        }
+        if (q.getExcludedTerms().size() > 0) {
+            queryString += "-" + Utils.concatenate(q.getExcludedTerms(), "-");
+        }
+        querySearchTerms.put("text", queryString);
         return querySearchTerms;
     }
 
