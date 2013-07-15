c78018a9ff93f9247a52419107735ce881c9c0ff
Susan Feng <susan.feng@altamiracorp.com>
2013-07-14 20:59:06 -0400

    Fixed bug where object key wasn't an Json Object

--- web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntityByRelatedEntities.java
+++ web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntityByRelatedEntities.java
@@ -3,6 +3,7 @@ package com.altamiracorp.reddawn.web.routes.entity;
 import com.altamiracorp.reddawn.model.Session;
 import com.altamiracorp.reddawn.ucd.statement.Statement;
 import com.altamiracorp.reddawn.ucd.statement.StatementRepository;
+import com.altamiracorp.reddawn.ucd.term.TermRowKey;
 import com.altamiracorp.reddawn.web.WebApp;
 import com.altamiracorp.reddawn.web.utils.UrlUtils;
 import com.altamiracorp.reddawn.web.Responder;
@@ -33,7 +34,7 @@ public class EntityByRelatedEntities implements Handler, AppAware{
         JSONObject json = new JSONObject();
         JSONArray statementsJson = new JSONArray();
         for (Statement statement : statements){
-            statementsJson.put(statement.getRowKey().getObjectRowKey());
+            statementsJson.put(new TermRowKey(statement.getRowKey().getObjectRowKey()).toJson());
         }
         json.put("statements", statementsJson);
 
