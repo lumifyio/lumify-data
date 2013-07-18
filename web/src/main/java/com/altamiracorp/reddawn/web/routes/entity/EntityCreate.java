b82c6e24b13cd4b368e52af45ffcf822f6aa9dea
Joe Ferner <joe@fernsroth.com>
2013-07-18 15:36:30 -0400

    parameter validation

--- web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntityCreate.java
+++ web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntityCreate.java
@@ -42,16 +42,65 @@ public class EntityCreate implements Handler, AppAware {
         RedDawnSession session = app.getRedDawnSession(request);
         JSONObject resultsJson = new JSONObject();
 
+        // validate parameters
         String artifactKey = request.getParameter("artifactKey");
+        if (artifactKey == null) {
+            throw new RuntimeException("'artifactKey' is required.");
+        }
+
+        if (request.getParameter("mentionStart") == null) {
+            throw new RuntimeException("'mentionStart' is required.");
+        }
         long mentionStart = Long.parseLong(request.getParameter("mentionStart"));
+
+        if (request.getParameter("mentionEnd") == null) {
+            throw new RuntimeException("'mentionEnd' is required.");
+        }
         long mentionEnd = Long.parseLong(request.getParameter("mentionEnd"));
+
         String sign = request.getParameter("sign");
+        if (sign == null) {
+            throw new RuntimeException("'sign' is required.");
+        }
+
         String conceptLabel = request.getParameter("conceptLabel");
-        String sentenceRowKey = request.getParameter("sentenceRowKey");
-        String objectRowKey = request.getParameter("objectRowKey");
+        if (conceptLabel == null) {
+            throw new RuntimeException("'conceptLabel' is required.");
+        }
+
         String newObjectSign = request.getParameter("newObjectSign");
         String newObjectConceptLabel = request.getParameter("newObjectConceptLabel");
+        if ((newObjectSign == null && newObjectConceptLabel != null) || (newObjectSign != null && newObjectConceptLabel == null)) {
+            throw new RuntimeException("When creating a new object both 'newObjectSign' and 'newObjectConceptLabel' need to be specified.");
+        }
 
+        String sentenceRowKey = request.getParameter("sentenceRowKey");
+        String objectRowKey = request.getParameter("objectRowKey");
+        if (sentenceRowKey != null && (objectRowKey == null || (newObjectSign == null && newObjectConceptLabel == null))) {
+            throw new RuntimeException("When associating a Term to and Object 'sentenceRowKey' and ('objectRowKey' or ('newObjectSign' and 'newObjectConceptLabel')) are required.");
+        }
+
+        // do the work
+        Term term = createTerm(currentUser, session, artifactKey, mentionStart, mentionEnd, sign, conceptLabel);
+        resultsJson.put("termRowKey", term.getRowKey().toJson());
+
+        if (newObjectSign != null && newObjectConceptLabel != null) {
+            objectRowKey = createObjectTerm(currentUser, session, artifactKey, newObjectSign, newObjectConceptLabel, mentionStart, mentionEnd);
+            resultsJson.put("objectTermRowKey", objectRowKey);
+        }
+
+        if (sentenceRowKey != null && objectRowKey != null) {
+            Statement statement = createIsAnObjectStatement(currentUser, session, artifactKey, sentenceRowKey, objectRowKey, term);
+            resultsJson.put("isAnObjectStatementRowKey", statement.getRowKey().toJson());
+
+            UcdObject ucdObject = createObject(session, objectRowKey, statement);
+            resultsJson.put("ucdObjectRowKey", ucdObject.getRowKey().toJson());
+        }
+
+        new Responder(response).respondWith(resultsJson);
+    }
+
+    private Term createTerm(User currentUser, RedDawnSession session, String artifactKey, long mentionStart, long mentionEnd, String sign, String conceptLabel) {
         Term term = new Term(sign, MODEL_KEY, conceptLabel);
         TermMention termMention = new TermMention()
                 .setArtifactKey(artifactKey)
@@ -61,44 +110,44 @@ public class EntityCreate implements Handler, AppAware {
                 .setDate(new Date());
         term.addTermMention(termMention);
         termRepository.save(session.getModelSession(), term);
-        resultsJson.put("termRowKey", term.getRowKey().toJson());
+        return term;
+    }
 
-        if (newObjectSign != null && newObjectConceptLabel != null) {
-            Term objectTerm = new Term(newObjectSign, "object", newObjectConceptLabel);
-            TermMention objectTermMention = new TermMention()
-                    .setArtifactKey(artifactKey)
-                    .setMentionStart(mentionStart)
-                    .setMentionEnd(mentionEnd)
-                    .setAuthor(currentUser.getUsername())
-                    .setDate(new Date());
-            objectTerm.addTermMention(objectTermMention);
-            termRepository.save(session.getModelSession(), objectTerm);
-            resultsJson.put("objectTermRowKey", objectTerm.getRowKey().toJson());
+    private UcdObject createObject(RedDawnSession session, String objectRowKey, Statement statement) {
+        UcdObject ucdObject = new UcdObject(objectRowKey);
+        UcdObjectObjectStatement ucdObjectObjectStatement = new UcdObjectObjectStatement();
+        ucdObjectObjectStatement.set(statement.getRowKey().toString(), statement.getRowKey().toString());
+        ucdObject.addObjectStatement(ucdObjectObjectStatement);
+        ucdObjectRepository.save(session.getModelSession(), ucdObject);
+        return ucdObject;
+    }
 
-            objectRowKey = objectTerm.getRowKey().toString();
-        }
+    private Statement createIsAnObjectStatement(User currentUser, RedDawnSession session, String artifactKey, String sentenceRowKey, String objectRowKey, Term term) {
+        StatementRowKey statementRowKey = new StatementRowKey(term.getRowKey(), PredicateRowKey.IS_AN_OBJECT, new TermRowKey(objectRowKey));
+        Statement statement = new Statement(statementRowKey);
+        StatementArtifact statementArtifact = new StatementArtifact()
+                .setArtifactKey(artifactKey)
+                .setAuthor(currentUser.getUsername())
+                .setDate(new Date())
+                .setSentence(sentenceRowKey);
+        statement.addStatementArtifact(statementArtifact);
+        statementRepository.save(session.getModelSession(), statement);
+        return statement;
+    }
 
-        if (sentenceRowKey != null && objectRowKey != null) {
-            StatementRowKey statementRowKey = new StatementRowKey(term.getRowKey(), PredicateRowKey.IS_AN_OBJECT, new TermRowKey(objectRowKey));
-            Statement statement = new Statement(statementRowKey);
-            StatementArtifact statementArtifact = new StatementArtifact()
-                    .setArtifactKey(artifactKey)
-                    .setAuthor(currentUser.getUsername())
-                    .setDate(new Date())
-                    .setSentence(sentenceRowKey);
-            statement.addStatementArtifact(statementArtifact);
-            statementRepository.save(session.getModelSession(), statement);
-            resultsJson.put("isAnObjectStatementRowKey", statement.getRowKey().toJson());
-
-            UcdObject ucdObject = new UcdObject(objectRowKey);
-            UcdObjectObjectStatement ucdObjectObjectStatement = new UcdObjectObjectStatement();
-            ucdObjectObjectStatement.set(statement.getRowKey().toString(), statement.getRowKey().toString());
-            ucdObject.addObjectStatement(ucdObjectObjectStatement);
-            ucdObjectRepository.save(session.getModelSession(), ucdObject);
-            resultsJson.put("ucdObjectRowKey", ucdObject.getRowKey().toJson());
-        }
-
-        new Responder(response).respondWith(resultsJson);
+    private String createObjectTerm(User currentUser, RedDawnSession session, String artifactKey, String objectSign, String objectConceptLabel, long mentionStart, long mentionEnd) {
+        String objectRowKey;
+        Term objectTerm = new Term(objectSign, "object", objectConceptLabel);
+        TermMention objectTermMention = new TermMention()
+                .setArtifactKey(artifactKey)
+                .setMentionStart(mentionStart)
+                .setMentionEnd(mentionEnd)
+                .setAuthor(currentUser.getUsername())
+                .setDate(new Date());
+        objectTerm.addTermMention(objectTermMention);
+        termRepository.save(session.getModelSession(), objectTerm);
+        objectRowKey = objectTerm.getRowKey().toString();
+        return objectRowKey;
     }
 
     public void setApp(App app) {
