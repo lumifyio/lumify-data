8efa65dba57895b466af2bcfb791ccdf5210c64e
Joe Ferner <joe@fernsroth.com>
2013-07-18 13:55:40 -0400

    fix compare

--- core/src/main/java/com/altamiracorp/reddawn/entityExtraction/EntityHighlightMR.java
+++ core/src/main/java/com/altamiracorp/reddawn/entityExtraction/EntityHighlightMR.java
@@ -261,9 +261,12 @@ public class EntityHighlightMR extends ConfigurableMapJobBase {
         @Override
         public int compare(OffsetItem o1, OffsetItem o2) {
             if (o1.getStart() == o2.getStart()) {
-                return Long.compare(o1.getEnd(), o2.getEnd());
+                if (o1.getEnd().longValue() == o2.getEnd().longValue()) {
+                    return 0;
+                }
+                return o1.getEnd() < o2.getEnd() ? -1 : 1;
             }
-            return Long.compare(o1.getStart(), o2.getStart());
+            return o1.getStart() < o2.getStart() ? -1 : 1;
         }
     }
 }
