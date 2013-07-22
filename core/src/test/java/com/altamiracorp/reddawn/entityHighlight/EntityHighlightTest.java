1509f52afe0e58152bc37c208c59d71be956ac47
Jeff Principe <princjef@gmail.com>
2013-07-22 13:57:31 -0400

    Removed nested and overlapping highlighted entities from artifact highlight text

--- core/src/test/java/com/altamiracorp/reddawn/entityHighlight/EntityHighlightTest.java
+++ core/src/test/java/com/altamiracorp/reddawn/entityHighlight/EntityHighlightTest.java
@@ -3,6 +3,9 @@ package com.altamiracorp.reddawn.entityHighlight;
 import com.altamiracorp.reddawn.ucd.artifact.ArtifactRowKey;
 import com.altamiracorp.reddawn.ucd.term.Term;
 import com.altamiracorp.reddawn.ucd.term.TermMention;
+import edu.emory.mathcs.backport.java.util.Arrays;
+import junit.framework.Assert;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -11,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public class EntityHighlightTest {
@@ -61,5 +66,56 @@ public class EntityHighlightTest {
         List<OffsetItem> termAndTermMetadata = EntityHighlighter.getTermAndTermMetadataForArtifact(artifactKey, terms);
         String highlightText = EntityHighlighter.getHighlightedText("Test highlight of Joe Ferner.", 0, termAndTermMetadata);
         assertEquals("Test highlight of <span class=\"entity person\" term-key=\"joe ferner\\x1Fee\\x1Fperson\"><span class=\"entity person\" term-key=\"joe\\x1Fee\\x1Fperson\">Joe</span> Ferner</span>.", highlightText);
+    }
+
+    @Test
+    public void testGetHighlightedTextNestedEntity() throws Exception {
+        String text = "This is a test sentence";
+        List<OffsetItem> offsetItems = new ArrayList<OffsetItem>();
+
+        OffsetItem mockEntity1 = mock(OffsetItem.class);
+        when(mockEntity1.getStart()).thenReturn(0l);
+        when(mockEntity1.getEnd()).thenReturn(4l);
+        when(mockEntity1.getCssClasses()).thenReturn(new ArrayList<String>(Arrays.asList(new String[]{"first"})));
+        when(mockEntity1.shouldHighlight()).thenReturn(true);
+        when(mockEntity1.getInfoJson()).thenReturn(new JSONObject("{\"data\":\"attribute\"}"));
+        offsetItems.add(mockEntity1);
+
+        OffsetItem mockEntity2 = mock(OffsetItem.class);
+        when(mockEntity2.getStart()).thenReturn(0l);
+        when(mockEntity2.getEnd()).thenReturn(4l);
+        when(mockEntity2.getCssClasses()).thenReturn(new ArrayList<String>(Arrays.asList(new String[]{"second"})));
+        when(mockEntity2.shouldHighlight()).thenReturn(true);
+        when(mockEntity2.getInfoJson()).thenReturn(new JSONObject("{\"data\":\"attribute\"}"));
+        offsetItems.add(mockEntity2);
+
+        OffsetItem mockEntity3 = mock(OffsetItem.class);
+        when(mockEntity3.getStart()).thenReturn(0l);
+        when(mockEntity3.getEnd()).thenReturn(7l);
+        when(mockEntity3.getCssClasses()).thenReturn(new ArrayList<String>(Arrays.asList(new String[]{"third"})));
+        when(mockEntity3.shouldHighlight()).thenReturn(true);
+        when(mockEntity3.getInfoJson()).thenReturn(new JSONObject("{\"data\":\"attribute\"}"));
+        offsetItems.add(mockEntity3);
+
+        OffsetItem mockEntity4 = mock(OffsetItem.class);
+        when(mockEntity4.getStart()).thenReturn(5l);
+        when(mockEntity4.getEnd()).thenReturn(9l);
+        when(mockEntity4.getCssClasses()).thenReturn(new ArrayList<String>(Arrays.asList(new String[]{"fourth"})));
+        when(mockEntity4.shouldHighlight()).thenReturn(true);
+        when(mockEntity4.getInfoJson()).thenReturn(new JSONObject("{\"data\":\"attribute\"}"));
+        offsetItems.add(mockEntity4);
+
+        OffsetItem mockEntity5 = mock(OffsetItem.class);
+        when(mockEntity5.getStart()).thenReturn(15l);
+        when(mockEntity5.getEnd()).thenReturn(23l);
+        when(mockEntity5.getCssClasses()).thenReturn(new ArrayList<String>(Arrays.asList(new String[]{"fifth"})));
+        when(mockEntity5.shouldHighlight()).thenReturn(true);
+        when(mockEntity5.getInfoJson()).thenReturn(new JSONObject("{\"data\":\"attribute\"}"));
+        offsetItems.add(mockEntity5);
+
+        Assert.assertEquals("<span class=\"first\" data-info=\"{&quot;data&quot;:&quot;attribute&quot;}\">This</span> " +
+                "<span class=\"fourth\" data-info=\"{&quot;data&quot;:&quot;attribute&quot;}\">is a</span> test <span " +
+                "class=\"fifth\" data-info=\"{&quot;data&quot;:&quot;attribute&quot;}\">sentence",
+                EntityHighlighter.getHighlightedText(text, 0, offsetItems));
     }
 }
