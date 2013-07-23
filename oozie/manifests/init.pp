d397884ef395165ccf8d79a1e72b28cbb707926b
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2013-07-23 13:50:52 -0400

    download extjs for oozie web console

--- oozie/manifests/init.pp
+++ oozie/manifests/init.pp
@@ -1,6 +1,25 @@
-class oozie {
-  package { 'oozie':
+class oozie($prefix = "/usr/lib/oozie") {
+  require hadoop
+  include macro
+
+  $downloadpath = "/usr/local/src/ext-2.2.zip"
+
+  package { "oozie":
     ensure  => installed,
-    require => Package['hadoop-0.20'],
+    require => Package["hadoop-0.20"],
+  }
+
+  file { "oozie-extjs-dir":
+    path    => "${prefix}/libext",
+    ensure  => directory,
+    require => Package["oozie"],
+  }
+
+  macro::download { "http://extjs.com/deploy/ext-2.2.zip":
+    path    => $downloadpath,
+  } -> macro::extract { $downloadpath:
+    type    => "zip",
+    path    => "${prefix}/libext",
+    require => File["oozie-extjs-dir"],
   }
 }
