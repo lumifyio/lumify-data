bb5699f55115c454b10fd569949e12b1aeae4dff
Jeff Principe <princjef@gmail.com>
2013-07-25 09:13:01 -0400

    Spline rendering changed to polyline from orthogonal

--- docs/makeErd.sh
+++ docs/makeErd.sh
@@ -3,5 +3,5 @@
 mkdir -p build
 # TODO build erd.gv from all *.desc
 rm -f build/erd.gv && node js/gvCreator.js
-rm -f build/erd.png && dot -Gsplines=none build/erd.gv | neato -n -Gsplines=ortho -Tpng -obuild/erd.png
+rm -f build/erd.png && dot build/erd.gv | neato -n -Tpng -obuild/erd.png
 
