5ea2f0f2a0eaee52eed276dd635de2627c1f2ed4
Ryan Lanman <rmlanman@gmail.com>
2013-08-01 16:42:47 -0400

    Changing visibility of initializeTable

--- model/src/main/java/com/altamiracorp/reddawn/model/Session.java
+++ model/src/main/java/com/altamiracorp/reddawn/model/Session.java
@@ -55,7 +55,7 @@ public abstract class Session {
     abstract List<ColumnFamily> findByRowKeyWithColumnFamilyRegexOffsetAndLimit(String tableName, String rowKey, QueryUser queryUser,
                                                                                 long colFamOffset, long colFamLimit, String colFamRegex);
 
-    abstract void initializeTable(String tableName);
+    public abstract void initializeTable(String tableName);
 
     public abstract void deleteTable(String tableName);
 
