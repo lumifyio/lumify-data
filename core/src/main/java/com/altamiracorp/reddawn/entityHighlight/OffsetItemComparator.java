e62c743749f03afe0ff974f36ef56ba98eb5f610
Jeff Principe <princjef@gmail.com>
2013-08-05 15:00:23 -0400

    Fixed excessive highlighting of artifacts

--- core/src/main/java/com/altamiracorp/reddawn/entityHighlight/OffsetItemComparator.java
+++ core/src/main/java/com/altamiracorp/reddawn/entityHighlight/OffsetItemComparator.java
@@ -5,7 +5,13 @@ import java.util.Comparator;
 public class OffsetItemComparator implements Comparator<OffsetItem> {
     @Override
     public int compare(OffsetItem o1, OffsetItem o2) {
-        if (o1.getStart() == o2.getStart()) {
+        if (o1.getStart().equals(o2.getStart())) {
+            if (o1 instanceof SentenceOffsetItem && o2 instanceof TermAndTermMentionOffsetItem) {
+                return -1;
+            }
+            if (o1 instanceof TermAndTermMentionOffsetItem && o2 instanceof SentenceOffsetItem) {
+                return 1;
+            }
             if (o1.getEnd().longValue() == o2.getEnd().longValue()) {
                 return 0;
             }
