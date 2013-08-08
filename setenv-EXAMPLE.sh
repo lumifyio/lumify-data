5a94d6444119ab2a93258eafc3f0f2a621908177
David Singley <dsingley@nearinfinity.com>
2013-08-08 15:08:09 -0400

    add example files

---
+++ setenv-EXAMPLE.sh
@@ -0,0 +1,13 @@
+export AWS_ACCESS_KEY=
+export AWS_SECRET_KEY=
+export EC2_URL=https://ec2.us-east-1.amazonaws.com
+# see http://docs.aws.amazon.com/general/latest/gr/rande.html#ec2_region
+
+export EC2_HOME=/path/to/ec2-api-tools-1.6.7.4
+export EC2_AMITOOL_HOME=/path/to/ec2-ami-tools-1.4.0.9
+# Amazon EC2 API Tools - http://aws.amazon.com/developertools/351
+# Amazon EC2 AMI Tools - http://aws.amazon.com/developertools/368
+
+export JAVA_HOME=$(readlink -f $(which java) | sed -E -e 's|/(jre)?/bin/java||')
+
+export PATH=$EC2_HOME/bin:$EC2_AMITOOL_HOME/bin:$JAVA_HOME/bin:$PATH
