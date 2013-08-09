ba9ee358d46bab9034bafc1bfa3cf119e52e8bca
Jason Harwig <jharwig@nearinfinity.com>
2013-08-09 16:23:44 -0500

    Fix dragging artifacts from detail pane to graph/map

--- web/src/main/webapp/js/detail/detail.js
+++ web/src/main/webapp/js/detail/detail.js
@@ -31,7 +31,7 @@ define([
 
         // Ignore drop events so they don't propagate to the graph/map
         this.preventDropEventsFromPropagating = function() {
-            this.$node.droppable({ accept: '.entity' });
+            this.$node.droppable({ accept: '.entity,.artifact' });
         };
 
 
