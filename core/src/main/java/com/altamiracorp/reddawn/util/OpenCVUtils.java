df42ddabac443c9a88222561c09eb1d28259af4e
Jason Harwig <jharwig@nearinfinity.com>
2013-08-13 10:39:17 -0500

    [#55089004] Convert images to 3 channel Mats if not already

--- core/src/main/java/com/altamiracorp/reddawn/util/OpenCVUtils.java
+++ core/src/main/java/com/altamiracorp/reddawn/util/OpenCVUtils.java
@@ -8,16 +8,43 @@ import java.awt.image.DataBufferByte;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import static org.opencv.core.CvType.CV_8UC3;
+import static org.opencv.core.CvType.*;
 
 public class OpenCVUtils {
 
     public static Mat bufferedImageToMat (BufferedImage image) {
         if (image != null) {
-            Mat mat = new Mat(image.getHeight(),image.getWidth(),CV_8UC3);
+            Mat mat;
+            int numComponents = image.getColorModel().getNumComponents();
+            switch (numComponents) {
+                case 1:
+                    mat = new Mat(image.getHeight(), image.getWidth(), CV_8UC1);
+                    break;
+                case 2:
+                    mat = new Mat(image.getHeight(), image.getWidth(), CV_8UC2);
+                    break;
+                case 3:
+                    mat = new Mat(image.getHeight(), image.getWidth(), CV_8UC3);
+                    break;
+                case 4:
+                    mat = new Mat(image.getHeight(), image.getWidth(), CV_8UC4);
+                    break;
+                default:
+                    throw new RuntimeException("Image has an unsupportable number of channels: " + numComponents);
+            }
+
             byte[] pixelData = ((DataBufferByte)image.getRaster().getDataBuffer()).getData();
             mat.put(0,0,pixelData);
-            return mat;
+
+            Mat mat3;
+            if (numComponents == 3) {
+                mat3 = mat;
+            } else {
+                mat3 = new Mat(image.getHeight(), image.getWidth(), CV_8UC3);
+                mat.convertTo(mat3, CV_8UC3);
+            }
+
+            return mat3;
         }
         return null;
     }
