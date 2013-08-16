caa242dd5ec251f18c41d82045011f9e50007411
David Singley <dsingley@nearinfinity.com>
2013-08-16 14:00:38 -0400

    extract puppet config updates to a seperate script

---
+++ aws-deployment/update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+latest_modules=$(ls ~/modules-*.tgz | tail -1)
+latest_puppet_modules=$(ls ~/puppet-modules-*.tgz | tail -1)
+
+( cd /etc/puppet
+  tar xzf ${latest_modules}
+  unlink reddawn-modules || true
+  ln -s $(basename ${latest_modules} .tgz)/puppet/modules reddawn-modules
+
+  unlink hiera || true
+  ln -s $(basename ${latest_modules} .tgz)/puppet/hiera hiera
+
+  unlink hiera.yaml || true
+  ln -s $(basename ${latest_modules} .tgz)/puppet/hiera-reddawn_demo.yaml hiera.yaml
+
+  tar xzf ${latest_puppet_modules}
+  unlink puppet-modules || true
+  ln -s $(basename ${latest_puppet_modules} .tgz) puppet-modules
+)
+( cd /etc/puppet/manifests
+  unlink site.pp || true
+  ln -s ../$(basename ${latest_modules} .tgz)/puppet/manifests/reddawn_demo.pp site.pp
+)
+
+# (re)start the puppetmaster service
+set +u
+[ "$1" = 'start' ] && action=start || action=restart
+set -u
+service puppetmaster ${action}
