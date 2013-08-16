d92bb20788d7b1d8acf6c58b487f95546bba9330
David Singley <dsingley@nearinfinity.com>
2013-08-16 14:28:22 -0400

    configure hadoop master and slaves

--- hadoop/manifests/init.pp
+++ hadoop/manifests/init.pp
@@ -2,6 +2,10 @@ class hadoop {
   include repo::cloudera::cdh3
   require java
 
+  $namenode_ipaddress = hiera("namenode_ipaddress")
+  $hadoop_masters = hiera_array('hadoop_masters')
+  $hadoop_slaves = hiera_array('hadoop_slaves')
+
   package { 'hadoop-0.20':
     ensure  => installed,
     require => Class['java', 'repo::cloudera::cdh3'],
@@ -12,8 +16,6 @@ class hadoop {
     require => Package['hadoop-0.20'],
   }
 
-  $namenode_ipaddress = hiera("namenode_ipaddress")
-
   file { "/etc/hadoop/conf/core-site.xml":
     ensure   => file,
     content  => template("hadoop/core-site.xml.erb"),
@@ -21,5 +23,19 @@ class hadoop {
     group    => "root",
     force    => true,
     require  => Package['hadoop-0.20'],
+  }
+
+  file { "hadoop-masters-config":
+    path    => "/etc/hadoop/conf/masters",
+    ensure  => file,
+    content => template("hadoop/masters.erb"),
+    require => Package['hadoop-0.20'],
+  }
+
+  file { "hadoop-slaves-config":
+    path    => "/etc/hadoop/conf/slaves",
+    ensure  => file,
+    content => template("hadoop/slaves.erb"),
+    require => Package['hadoop-0.20'],
   }
 }
