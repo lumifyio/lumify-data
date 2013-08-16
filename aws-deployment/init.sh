f783b60e4a3d648a3462e6c81d68af6a2907f320
David Singley <dsingley@nearinfinity.com>
2013-08-16 17:23:31 -0400

    configure and use the proxy server

--- aws-deployment/init.sh
+++ aws-deployment/init.sh
@@ -9,7 +9,7 @@ hosts_file=$1
 # configure enviornment name resolution
 grep -q $(hostname) /etc/hosts || cat ${hosts_file} >> /etc/hosts
 
-# add the PuppetLabs YUM repo, install and enable puppet
+# add the PuppetLabs Yum repo, install and enable puppet
 rpm -ivh ${PUPPETLABS_RPM_URL}
 yum -y install puppet-server
 chkconfig puppetmaster on
@@ -19,9 +19,11 @@ chkconfig puppet on
 puppet cert generate $(hostname) --dns_alt_names=puppet
 awk -v localhost=$(hostname) '$2!=localhost {print $2}' ${hosts_file} > /etc/puppet/autosign.conf
 
-# insert a firewall rule for the puppermaster service
+# insert a firewall rule for the puppermaster and tinyproxy services
 rule_number=$(iptables -L -n --line-numbers | awk '/tcp dpt:22/ {print $1}')
 iptables -I INPUT ${rule_number} -p tcp -m state --state NEW -m tcp --dport 8140 -j ACCEPT
+rule_number=$(iptables -L -n --line-numbers | awk '/tcp dpt:22/ {print $1}')
+iptables -I INPUT ${rule_number} -p tcp -m state --state NEW -m tcp --dport 8080 -j ACCEPT
 service iptables save
 
 cat >> /etc/puppet/puppet.conf <<EO_PUPPET_CONF
@@ -44,8 +46,14 @@ for other_host in $(awk -v localhost=$(hostname) '$2!=localhost {print $1}' ${ho
   scp ${SSH_OPTS} ${hosts_file} ${other_host}:
   ssh ${SSH_OPTS} ${other_host} "grep -q ${other_host} /etc/hosts || cat ${hosts_file} >> /etc/hosts"
 
-  # add the PuppetLabs YUM repo, install and enable puppet
-  ssh ${SSH_OPTS} ${other_host} rpm -ivh ${PUPPETLABS_RPM_URL}
+  # configure Yum to use the proxy
+  cat <<EO_YUM_CONF | ssh ${SSH_OPTS} ${other_host} 'cat >> /etc/yum.conf'
+
+  proxy=http://$(hostname):8080
+EO_YUM_CONF
+
+  # add the PuppetLabs Yum repo, install and enable puppet
+  ssh ${SSH_OPTS} ${other_host} http_proxy=http://$(hostname):8080 rpm -ivh ${PUPPETLABS_RPM_URL}
   ssh ${SSH_OPTS} ${other_host} yum -y install puppet
   ssh ${SSH_OPTS} ${other_host} chkconfig puppet on
 
