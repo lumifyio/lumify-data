8c9d66f8553695ffb0bbfc72e792c30083ec4345
David Singley <dsingley@nearinfinity.com>
2013-08-20 18:47:59 -0400

    firewall and init fixes

--- jetty/manifests/fw.pp
+++ jetty/manifests/fw.pp
@@ -1,25 +1,25 @@
 class jetty::fw::server {
-  firewall { '101 allow http and https for jetty' :
+  firewall { '101 allow 8080 and 8443 for jetty' :
     proto  => tcp,
-    port   => [80, 443],
-    action => allow,
+    port   => [8080, 8443],
+    action => accept,
   }
 
   firewall { '102 map 80 to 8080 for jetty' :
-    table  => 'nat',
-    chail  => 'PREROUTING',
-    proto  => tcp,
-    port   => 80,
-    jump   => 'REDIRECT',
-    toport => 8080,
+    table   => 'nat',
+    chain   => 'PREROUTING',
+    proto   => tcp,
+    port    => 80,
+    jump    => 'REDIRECT',
+    toports => 8080,
   }
 
   firewall { '103 map 443 to 8443 for jetty' :
-    table  => 'nat',
-    chail  => 'PREROUTING',
-    proto  => tcp,
-    port   => 443,
-    jump   => 'REDIRECT',
-    toport => 8443,
+    table   => 'nat',
+    chain   => 'PREROUTING',
+    proto   => tcp,
+    port    => 443,
+    jump    => 'REDIRECT',
+    toports => 8443,
   }
 }
--- jetty/manifests/init.pp
+++ jetty/manifests/init.pp
@@ -1,7 +1,19 @@
 class jetty(
-  $major_version="9",
-  $version="9.0.5.v20130815"
+  $major_version='8',
+  $version='8.1.12.v20130726'
 ){
+  require java
+
+  group { 'jetty' :
+    ensure => present,
+  }
+
+  user { 'jetty' :
+   ensure  => present,
+   gid     => 'jetty',
+   home    => '/opt/jetty',
+   require => Group['jetty'],
+  }
 
   macro::download { 'jetty-download':
     url  => "http://eclipse.org/downloads/download.php?file=/jetty/stable-${major_version}/dist/jetty-distribution-${version}.tar.gz&r=1",
@@ -11,10 +23,28 @@ class jetty(
     path => "/opt",
   }
 
-  file { "/opt/jetty" :
-    ensure  => link,
-    target  => "/opt/jetty-distribution-${version}",
-    require => Macro::Extract['jetty-extract'],
+  file { "/opt/jetty-distribution-${version}" :
+    ensure  => directory,
+    owner   => 'jetty',
+    group   => 'jetty',
+    require => [ User['jetty'], Macro::Extract['jetty-extract'] ],
   }
 
+  file { '/opt/jetty' :
+    ensure  => link,
+    target  => "/opt/jetty-distribution-${version}",
+    require => File["/opt/jetty-distribution-${version}"],
+  }
+
+  file { '/etc/init.d/jetty' :
+    ensure  => link,
+    target  => '/opt/jetty/bin/jetty.sh',
+    require => File['/opt/jetty'],
+  }
+
+  service { 'jetty' :
+    enable  => true,
+    ensure  => running,
+    require => File['/etc/init.d/jetty'],
+  }
 }
