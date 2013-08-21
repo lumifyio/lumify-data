7500a0125a6ec82ad0378aaf9527243891ed49f8
Andy Keffalas <akeffala@nearinfinity.com>
2013-08-21 17:06:03 -0400

    Added worker thread for modifying the highlighted text of an artifact upon new entity creation from the details panel.

--- web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntityCreate.java
+++ web/src/main/java/com/altamiracorp/reddawn/web/routes/entity/EntityCreate.java
@@ -1,18 +1,35 @@
 package com.altamiracorp.reddawn.web.routes.entity;
 
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.altamiracorp.reddawn.RedDawnSession;
+import com.altamiracorp.reddawn.entityHighlight.EntityHighlightWorker;
 import com.altamiracorp.reddawn.entityHighlight.TermAndTermMentionOffsetItem;
 import com.altamiracorp.reddawn.model.GraphSession;
 import com.altamiracorp.reddawn.model.RowKeyHelper;
+import com.altamiracorp.reddawn.model.graph.GraphRepository;
 import com.altamiracorp.reddawn.model.graph.GraphVertex;
 import com.altamiracorp.reddawn.model.graph.GraphVertexImpl;
-import com.altamiracorp.reddawn.model.graph.GraphRepository;
 import com.altamiracorp.reddawn.model.ontology.OntologyRepository;
 import com.altamiracorp.reddawn.model.ontology.PropertyName;
 import com.altamiracorp.reddawn.model.ontology.VertexType;
-import com.altamiracorp.reddawn.ucd.artifact.ArtifactRepository;
-import com.altamiracorp.reddawn.ucd.artifact.ArtifactRowKey;
-import com.altamiracorp.reddawn.ucd.term.*;
+import com.altamiracorp.reddawn.ucd.term.Term;
+import com.altamiracorp.reddawn.ucd.term.TermAndTermMention;
+import com.altamiracorp.reddawn.ucd.term.TermMention;
+import com.altamiracorp.reddawn.ucd.term.TermRepository;
+import com.altamiracorp.reddawn.ucd.term.TermRowKey;
 import com.altamiracorp.reddawn.web.Responder;
 import com.altamiracorp.reddawn.web.User;
 import com.altamiracorp.reddawn.web.WebApp;
@@ -21,23 +38,19 @@ import com.altamiracorp.web.AppAware;
 import com.altamiracorp.web.Handler;
 import com.altamiracorp.web.HandlerChain;
 import com.altamiracorp.web.utils.UrlUtils;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
-import java.util.List;
+import com.google.common.util.concurrent.MoreExecutors;
 
 public class EntityCreate implements Handler, AppAware {
     private static final Logger LOGGER = LoggerFactory.getLogger(EntityCreate.class.getName());
     private static final String MODEL_KEY = "manual";
     private WebApp app;
-    private ArtifactRepository artifactRepository = new ArtifactRepository();
     private TermRepository termRepository = new TermRepository();
     private OntologyRepository ontologyRepository = new OntologyRepository();
     private GraphRepository graphRepository = new GraphRepository();
+
+    private final ExecutorService executorService = MoreExecutors.getExitingExecutorService(
+            new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>()),
+            0L, TimeUnit.MILLISECONDS);
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
@@ -50,11 +63,11 @@ public class EntityCreate implements Handler, AppAware {
         long mentionEnd = Long.parseLong(getRequiredParameter(request, "mentionEnd"));
         String sign = getRequiredParameter(request, "sign");
         String conceptId = getRequiredParameter(request, "conceptId");
-        GraphVertex conceptVertex = graphRepository.findVertex(session.getGraphSession(), conceptId);
 
         // optional parameters
         String objectSign = request.getParameter("objectSign");
 
+        GraphVertex conceptVertex = graphRepository.findVertex(session.getGraphSession(), conceptId);
         GraphVertex resolvedVertex = null;
         if (objectSign != null && objectSign.length() > 0) {
             objectSign = UrlUtils.urlDecode(objectSign);
@@ -66,7 +79,8 @@ public class EntityCreate implements Handler, AppAware {
             graphRepository.saveRelationship(session.getGraphSession(), termAndTermMention.getTermMention().getGraphVertexId(), resolvedVertex.getId(), "isA");
         }
 
-        artifactRepository.touchRow(session.getModelSession(), new ArtifactRowKey(artifactKey));
+        // Modify the highlighted artifact text in a background thread
+        executorService.execute(new EntityHighlightWorker(session, artifactKey));
 
         TermAndTermMentionOffsetItem offsetItem = new TermAndTermMentionOffsetItem(termAndTermMention);
         new Responder(response).respondWith(offsetItem.toJson());
@@ -135,6 +149,7 @@ public class EntityCreate implements Handler, AppAware {
         return UrlUtils.urlDecode(parameter);
     }
 
+    @Override
     public void setApp(App app) {
         this.app = (WebApp) app;
     }
