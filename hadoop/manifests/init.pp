cd44a25b404fda1d3dc8809993b7bbafef65ba44
David Singley <dsingley@nearinfinity.com>
2013-08-21 16:49:22 -0400

    configure data directories

--- hadoop/manifests/init.pp
+++ hadoop/manifests/init.pp
@@ -25,6 +25,24 @@ class hadoop {
     require  => Package['hadoop-0.20'],
   }
 
+  file { "/etc/hadoop/conf/hdfs-site.xml":
+    ensure   => file,
+    content  => template("hadoop/hdfs-site.xml.erb"),
+    owner    => "root",
+    group    => "root",
+    force    => true,
+    require  => Package['hadoop-0.20'],
+  }
+
+  file { "/etc/hadoop/conf/mapred-site.xml":
+    ensure   => file,
+    content  => template("hadoop/mapred-site.xml.erb"),
+    owner    => "root",
+    group    => "root",
+    force    => true,
+    require  => Package['hadoop-0.20'],
+  }
+
   file { "hadoop-masters-config":
     path    => "/etc/hadoop/conf/masters",
     ensure  => file,
@@ -37,5 +55,46 @@ class hadoop {
     ensure  => file,
     content => template("hadoop/slaves.erb"),
     require => Package['hadoop-0.20'],
+  }
+
+  file { "/usr/lib/hadoop/.ssh" :
+    ensure  => directory,
+    owner   => 'hdfs',
+    group   => 'hadoop',
+    mode    => 'u=rwx,go=',
+    require => Package['hadoop-0.20'],
+  }
+
+  macro::setup-passwordless-ssh { 'hdfs' :
+    sshdir  => '/usr/lib/hadoop/.ssh',
+    require => File['/usr/lib/hadoop/.ssh'],
+  }
+
+  file { '/data0' :
+    ensure  => directory,
+  }
+
+  file { [ '/data0/hadoop', '/data0/hadoop/tmp' ] :
+    ensure  => directory,
+    owner   => 'hdfs',
+    group   => 'hadoop'
+    mode    => 'u=rwx,g=rx,o=',
+    require => File['/data0'],
+  }
+
+  file { [ '/data0/hdfs', '/data0/hdfs/name', '/data0/hdfs/data' ] :
+    ensure  => directory,
+    owner   => 'hdfs',
+    group   => 'hadoop'
+    mode    => 'u=rwx,g=rx,o=',
+    require => File['/data0'],
+  }
+
+  file { [ '/data0/mapred', '/data0/mapred/local' ] :
+    ensure  => directory,
+    owner   => 'mapred',
+    group   => 'hadoop'
+    mode    => 'u=rwx,g=rx,o=',
+    require => File['/data0'],
   }
 }
