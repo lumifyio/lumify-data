db74a95656563df863fecb99ca3851c11f436930
Ryan Lanman <rmlanman@gmail.com>
2013-08-22 10:10:41 -0400

    Adding zip code extraction

---
+++ bin/300_ZipCodeExtractionMR.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# require: 250_SentenceExtractionMR.sh
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+classpath=$(${DIR}/classpath.sh core)
+if [ $? -ne 0 ]; then
+  echo "${classpath}"
+  exit
+fi
+
+if [ "${VIRTUALIZATION_DISABLED}" = 'true' ]; then
+  ip=$(ifconfig eth0 | awk -F ':| +' '/inet addr/ {print $4}')
+else
+  ip=192.168.33.10
+fi
+
+java \
+-Dfile.encoding=UTF-8 \
+-classpath ${classpath} \
+-Xmx1g \
+-XX:MaxPermSize=512m \
+com.altamiracorp.reddawn.entityExtraction.EntityExtractionMR \
+--zookeeperInstanceName=reddawn \
+--zookeeperServerNames=${ip} \
+--blurControllerLocation=${ip}:40010 \
+--blurPath=hdfs://${ip}/blur \
+--graph.storage.index.search.hostname=${ip} \
+--hadoopUrl=hdfs://${ip}:8020 \
+--username=root \
+--password=password \
+--failOnFirstError \
+--classname=com.altamiracorp.reddawn.entityExtraction.RegexEntityExtractor \
+-DregularExpression="\b\d{5}-\d{4}\b|\b\d{5}\b" \
+-DentityType="Location"
