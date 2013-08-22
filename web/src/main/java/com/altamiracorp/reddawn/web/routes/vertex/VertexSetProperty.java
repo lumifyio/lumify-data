72679b25daf53df61fc4b9d20f2b6a305db72db3
Joe Ferner <joe@fernsroth.com>
2013-08-22 15:15:43 -0400

    return differnt http return code for bad data

--- web/src/main/java/com/altamiracorp/reddawn/web/routes/vertex/VertexSetProperty.java
+++ web/src/main/java/com/altamiracorp/reddawn/web/routes/vertex/VertexSetProperty.java
@@ -11,12 +11,15 @@ import com.altamiracorp.web.AppAware;
 import com.altamiracorp.web.Handler;
 import com.altamiracorp.web.HandlerChain;
 import org.json.JSONArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public class VertexSetProperty implements Handler, AppAware {
+    private static final Logger LOGGER = LoggerFactory.getLogger(VertexSetProperty.class.getName());
     private WebApp app;
     private GraphRepository graphRepository = new GraphRepository();
     private OntologyRepository ontologyRepository = new OntologyRepository();
@@ -29,7 +32,14 @@ public class VertexSetProperty implements Handler, AppAware {
         String valueStr = request.getParameter("value");
 
         Property property = ontologyRepository.getProperty(session.getGraphSession(), propertyName);
-        Object value = property.convertString(valueStr);
+        Object value;
+        try {
+            value = property.convertString(valueStr);
+        } catch (Exception ex) {
+            LOGGER.warn("Validation error propertyName: " + propertyName + ", valueStr: " + valueStr, ex);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+            return;
+        }
 
         graphRepository.setProperty(session.getGraphSession(), graphVertexId, propertyName, value);
 
