705031ade0316160b974c512778b2171d4c42e73
Joe Ferner <joe@fernsroth.com>
2013-08-28 11:33:46 -0400

    plural the concepts

--- web/src/main/java/com/altamiracorp/reddawn/web/routes/ontology/ConceptList.java
+++ web/src/main/java/com/altamiracorp/reddawn/web/routes/ontology/ConceptList.java
@@ -3,12 +3,14 @@ package com.altamiracorp.reddawn.web.routes.ontology;
 import com.altamiracorp.reddawn.RedDawnSession;
 import com.altamiracorp.reddawn.model.ontology.Concept;
 import com.altamiracorp.reddawn.model.ontology.OntologyRepository;
+import com.altamiracorp.reddawn.model.ontology.PropertyName;
 import com.altamiracorp.reddawn.web.Responder;
 import com.altamiracorp.reddawn.web.WebApp;
 import com.altamiracorp.web.App;
 import com.altamiracorp.web.AppAware;
 import com.altamiracorp.web.Handler;
 import com.altamiracorp.web.HandlerChain;
+import org.atteo.evo.inflector.English;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -35,6 +37,11 @@ public class ConceptList implements Handler, AppAware {
 
     private JSONObject buildJsonTree(HttpServletRequest request, RedDawnSession session, Concept concept) throws JSONException {
         JSONObject result = concept.toJson();
+
+        String displayName = result.optString(PropertyName.DISPLAY_NAME.toString());
+        if (displayName != null) {
+            result.put("pluralDisplayName", English.plural(displayName));
+        }
 
         List<Concept> childConcepts = ontologyRepository.getChildConcepts(session.getGraphSession(), concept);
         if (childConcepts.size() > 0) {
