b3c6989f7250ac924f06434fcd3ebac5979246a0
Susan Feng <susan.feng@altamiracorp.com>
2013-09-04 09:54:48 -0400

    added jcrop plugin

--- web/src/main/webapp/js/require.config.js
+++ web/src/main/webapp/js/require.config.js
@@ -5,6 +5,7 @@
 var jQueryPlugins = {
   atmosphere: '../libs/jquery.atmosphere/jquery.atmosphere',
   withinScrollable: 'util/jquery.within-scrollable',
+  jCrop: '../libs/jquery.jcrop/',
   scrollStop: '../libs/jquery-scrollstop/jquery.scrollstop',
   bigText: '../libs/jquery-bigtext/jquery-bigtext'
 };
