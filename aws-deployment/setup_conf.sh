829b5e8729a2956a3b37bc576391634c16730048
David Singley <dsingley@nearinfinity.com>
2013-09-05 12:50:31 -0400

    fix /conf directory setup

--- aws-deployment/setup_conf.sh
+++ aws-deployment/setup_conf.sh
@@ -4,4 +4,4 @@ most_recent_conf_tgz=$(ls conf-*.tgz | tail -1)
 most_recent_conf=$(basename ${most_recent_conf_tgz} .tgz)
 tar xzf ${most_recent_conf_tgz}
 hadoop fs -rmr /conf || true
-hadoop fs -put ${most_recent_conf} /conf
+hadoop fs -put ${most_recent_conf}/conf /conf
