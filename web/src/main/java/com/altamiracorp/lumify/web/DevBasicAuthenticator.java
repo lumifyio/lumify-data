44a709f1ffa8d0a7222c9d8a0f95f1f251c79983
Andy Keffalas <akeffala@nearinfinity.com>
2013-09-11 13:41:50 -0400

    Transitioned remaining handlers to use base handler

--- web/src/main/java/com/altamiracorp/lumify/web/DevBasicAuthenticator.java
+++ web/src/main/java/com/altamiracorp/lumify/web/DevBasicAuthenticator.java
@@ -1,26 +1,24 @@
 package com.altamiracorp.lumify.web;
 
-import com.altamiracorp.lumify.AppSession;
-import com.altamiracorp.lumify.model.user.User;
-import com.altamiracorp.lumify.model.user.UserRepository;
-import com.altamiracorp.web.App;
-import com.altamiracorp.web.AppAware;
-import com.altamiracorp.web.Handler;
-import com.altamiracorp.web.HandlerChain;
-import org.apache.commons.codec.binary.Base64;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
-public class DevBasicAuthenticator implements Handler, AppAware {
+import org.apache.commons.codec.binary.Base64;
+
+import com.altamiracorp.lumify.AppSession;
+import com.altamiracorp.lumify.model.user.User;
+import com.altamiracorp.lumify.model.user.UserRepository;
+import com.altamiracorp.web.HandlerChain;
+
+public class DevBasicAuthenticator extends BaseRequestHandler {
     private static final String HTTP_BASIC_REALM = "lumify";
     private static final String HTTP_AUTHORIZATION_HEADER = "Authorization";
     private static final String HTTP_AUTHENTICATE_HEADER = "WWW-Authenticate";
     private static final int HTTP_NOT_AUTHORIZED_ERROR_CODE = 401;
     private UserRepository userRepository = new UserRepository();
-    private WebApp app;
 
     public static User getUser(HttpServletRequest request) {
         return getUser(request.getSession());
@@ -74,10 +72,5 @@ public class DevBasicAuthenticator implements Handler, AppAware {
 
     private boolean isAuthorizationPresent(HttpServletRequest request) {
         return request.getHeader(HTTP_AUTHORIZATION_HEADER) != null;
-    }
-
-    @Override
-    public void setApp(App app) {
-        this.app = (WebApp) app;
     }
 }
--- web/src/main/java/com/altamiracorp/lumify/web/routes/resource/ResourceGet.java
+++ web/src/main/java/com/altamiracorp/lumify/web/routes/resource/ResourceGet.java
@@ -1,26 +1,21 @@
 package com.altamiracorp.lumify.web.routes.resource;
 
-import com.altamiracorp.lumify.AppSession;
-import com.altamiracorp.lumify.model.resources.Resource;
-import com.altamiracorp.lumify.model.resources.ResourceRepository;
-import com.altamiracorp.lumify.web.WebApp;
-import com.altamiracorp.web.App;
-import com.altamiracorp.web.AppAware;
-import com.altamiracorp.web.Handler;
-import com.altamiracorp.web.HandlerChain;
-import com.altamiracorp.web.utils.UrlUtils;
-
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ResourceGet implements Handler, AppAware {
+import com.altamiracorp.lumify.AppSession;
+import com.altamiracorp.lumify.model.resources.Resource;
+import com.altamiracorp.lumify.model.resources.ResourceRepository;
+import com.altamiracorp.lumify.web.BaseRequestHandler;
+import com.altamiracorp.web.HandlerChain;
+
+public class ResourceGet extends BaseRequestHandler {
     private ResourceRepository resourceRepository = new ResourceRepository();
-    private WebApp app;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
-        String rowKey = getRequiredAttribute(request, "_rowKey");
+        final String rowKey = getAttributeString(request, "_rowKey");
         AppSession session = app.getAppSession(request);
 
         Resource resource = resourceRepository.findByRowKey(session.getModelSession(), rowKey);
@@ -33,18 +28,5 @@ public class ResourceGet implements Handler, AppAware {
         ServletOutputStream out = response.getOutputStream();
         out.write(resource.getContent().getData());
         out.close();
-    }
-
-    public static String getRequiredAttribute(HttpServletRequest request, String parameterName) {
-        String attribute = (String) request.getAttribute(parameterName);
-        if (attribute == null) {
-            throw new RuntimeException("'" + parameterName + "' is required.");
-        }
-        return UrlUtils.urlDecode(attribute);
-    }
-
-    @Override
-    public void setApp(App app) {
-        this.app = (WebApp) app;
     }
 }
