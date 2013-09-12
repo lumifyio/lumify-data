b313e4d9690a1f6d3b98a1a583e3e35bd24bf04f
Andy Keffalas <akeffala@nearinfinity.com>
2013-09-12 17:12:55 -0400

    Modified Responder to be abstract

--- web/src/main/java/com/altamiracorp/lumify/web/Responder.java
+++ web/src/main/java/com/altamiracorp/lumify/web/Responder.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class Responder {
+public abstract class Responder {
     public static void respondWith(final HttpServletResponse response, final JSONObject json) throws IOException {
         response.setContentType("application/json");
         response.getWriter().write(json.toString());
@@ -21,5 +21,9 @@ public class Responder {
     public static void respondWith(final HttpServletResponse response, final String plainText) throws IOException {
         response.setContentType("text/plain");
         response.getWriter().write(plainText);
+    }
+
+    private Responder() {
+
     }
 }
