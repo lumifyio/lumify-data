a3717328464b1004da83456697366b76b7516f9f
Ryan Lanman <rmlanman@gmail.com>
2013-09-17 13:42:34 -0400

    Combine File Input Format update

--- core/src/main/java/com/altamiracorp/lumify/fileImport/WholeFileInputFormat.java
+++ core/src/main/java/com/altamiracorp/lumify/fileImport/WholeFileInputFormat.java
@@ -1,8 +1,9 @@
 package com.altamiracorp.lumify.fileImport;
 
-import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.lib.input.CombineFileInputFormat;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
@@ -13,6 +14,12 @@ import org.apache.hadoop.mapreduce.lib.input.CombineFileSplit;
 import java.io.IOException;
 
 public class WholeFileInputFormat extends CombineFileInputFormat<MapWritable, Text> {
+
+    @Override
+    public boolean isSplitable (JobContext context, Path file) {
+        return false;
+    }
+
     @Override
     public RecordReader<MapWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException {
         return new CombineFileRecordReader<MapWritable, Text>((CombineFileSplit)split, context, WholeFileRecordReader.class);
