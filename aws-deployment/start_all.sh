4904725f74a9e69ff1606d5e8529bac01287996d
David Singley <dsingley@nearinfinity.com>
2013-09-17 16:04:56 -0400

    remove blur script

--- aws-deployment/start_all.sh
+++ aws-deployment/start_all.sh
@@ -5,5 +5,4 @@ hosts_file=$1
 ./start_hadoop.sh ${hosts_file}
 ./start_zookeeper.sh ${hosts_file}
 ./start_accumulo.sh ${hosts_file}
-./start_blur.sh ${hosts_file}
 ./start_elasticsearch.sh ${hosts_file}
