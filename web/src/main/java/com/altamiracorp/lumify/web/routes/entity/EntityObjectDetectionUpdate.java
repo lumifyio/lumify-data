e2a28ba0678b5c1a64a97b9cac51a329041d192e
Susan Feng <susan.feng@altamiracorp.com>
2013-09-27 16:03:43 -0400

    allow user to modify coordinates on an existing detected object

--- web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityObjectDetectionUpdate.java
+++ web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityObjectDetectionUpdate.java
@@ -1,9 +1,11 @@
 package com.altamiracorp.lumify.web.routes.entity;
 
 import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.model.ModelSession;
 import com.altamiracorp.lumify.model.Value;
 import com.altamiracorp.lumify.model.graph.GraphRepository;
 import com.altamiracorp.lumify.model.graph.GraphVertex;
+import com.altamiracorp.lumify.model.ontology.LabelName;
 import com.altamiracorp.lumify.model.ontology.PropertyName;
 import com.altamiracorp.lumify.model.termMention.TermMention;
 import com.altamiracorp.lumify.model.termMention.TermMentionRepository;
@@ -26,17 +28,20 @@ public class EntityObjectDetectionUpdate extends BaseRequestHandler {
     private final ArtifactRepository artifactRepository;
     private final TermMentionRepository termMentionRepository;
     private final SearchProvider searchProvider;
+    private final ModelSession modelSession;
 
     @Inject
     public EntityObjectDetectionUpdate(
             final TermMentionRepository termMentionRepository,
             final ArtifactRepository artifactRepository,
             final GraphRepository graphRepository,
-            final SearchProvider searchProvider) {
+            final SearchProvider searchProvider,
+            final ModelSession modelSession) {
         this.termMentionRepository = termMentionRepository;
         this.artifactRepository = artifactRepository;
         this.graphRepository = graphRepository;
         this.searchProvider = searchProvider;
+        this.modelSession = modelSession;
     }
 
     @Override
@@ -52,26 +57,37 @@ public class EntityObjectDetectionUpdate extends BaseRequestHandler {
         String x1 = Double.toString(coords.getDouble("x1")), x2 = Double.toString(coords.getDouble("x2")),
                 y1 = Double.toString(coords.getDouble("y1")), y2 = Double.toString(coords.getDouble("y2"));
         String detectedObjectRowKey = getRequiredParameter(request, "detectedObjectRowKey");
+        final String boundingBox = "[x1: " + x1 + ", y1: " + y1 +", x2: " + x2 + ", y2: " + y2 + "]";
 
         GraphVertex conceptVertex = graphRepository.findVertex(conceptId, user);
         GraphVertex resolvedVertex = graphRepository.findVertex(resolvedGraphVertexId, user);
+        GraphVertex artifactVertex = graphRepository.findVertexByRowKey(artifactRowKey, user);
 
         // update graph vertex
         objectDetectionHelper.updateGraphVertex(resolvedVertex, conceptId, sign, user);
+        graphRepository.setPropertyEdge(artifactVertex.getId(), resolvedVertex.getId(), LabelName.CONTAINS_IMAGE_OF.toString()
+                , PropertyName.BOUNDING_BOX.toString(), boundingBox, user);
+
+        ArtifactDetectedObjects artifactDetectedObjects = artifactRepository.findByRowKey(artifactRowKey, user).getArtifactDetectedObjects();
+        JSONObject value = Value.toJson(artifactDetectedObjects.get(detectedObjectRowKey));
+        JSONObject info = value.getJSONObject("info");
 
         // update the term mention
         TermMentionRowKey termMentionRowKey = new TermMentionRowKey(artifactRowKey, (long)coords.getDouble("x1"), (long)coords.getDouble("y1"));
-        TermMention termMention = termMentionRepository.findByRowKey(termMentionRowKey.toString(), user);
+        TermMention termMention = null;
+        if (coords.getDouble("x1") != info.getJSONObject("coords").getDouble("x1") ||  coords.getDouble("y1") != info.getJSONObject("coords").getDouble("y1")) {
+            TermMentionRowKey oldTermMentionRowKey = new TermMentionRowKey(artifactRowKey, (long)info.getJSONObject("coords").getDouble("x1"), (long)info.getJSONObject("coords").getDouble("y1"));
+            modelSession.deleteRow(TermMention.TABLE_NAME, oldTermMentionRowKey, user);
+            termMention = new TermMention(termMentionRowKey);
+        } else {
+            termMention = termMentionRepository.findByRowKey(termMentionRowKey.toString(), user);
+        }
         objectDetectionHelper.updateTermMention(termMention, sign, conceptVertex, resolvedVertex, user);
 
         // update the detected object column
-        ArtifactDetectedObjects artifactDetectedObjects = artifactRepository.findByRowKey(artifactRowKey, user).getArtifactDetectedObjects();
-
         DetectedObject detectedObject = objectDetectionHelper.createObjectTag(x1, x2, y1, y2, resolvedVertex, conceptVertex);
         detectedObject.setRowKey(detectedObjectRowKey);
 
-        JSONObject value = Value.toJson(artifactDetectedObjects.get(detectedObjectRowKey));
-        JSONObject info = value.getJSONObject("info");
         detectedObject.setModel(info.get("model").toString());
 
         JSONObject obj = detectedObject.getJson();
