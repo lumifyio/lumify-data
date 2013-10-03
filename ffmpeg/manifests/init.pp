51016d9a8a1dcd72e29b390f2ccd520db1610a53
David Singley <dsingley@nearinfinity.com>
2013-10-03 16:38:21 -0400

    breakout git macros

--- ffmpeg/manifests/init.pp
+++ ffmpeg/manifests/init.pp
@@ -12,16 +12,16 @@ class ffmpeg ($prefix="/usr/local", $tmpdir="/usr/local/src") {
   $srcdir = "${tmpdir}/ffmpeg-source"
   $bindir = "${prefix}/bin"
 
-  macro::git-clone { "ffmpeg-clone":
+  macro::git::clone { "ffmpeg-clone":
     url     => "http://git.videolan.org/git/ffmpeg.git",
     path    => $srcdir,
     timeout => 600,
   }
 
-  macro::git-checkout { 'ffmpeg-checkout':
+  macro::git::checkout { 'ffmpeg-checkout':
     path    => $srcdir,
     branch  => "n2.0",
-    require => Macro::Git-clone["ffmpeg-clone"],
+    require => Macro::Git::Clone["ffmpeg-clone"],
   }
 
   $configure  = "${srcdir}/configure --prefix='${prefix}' --extra-cflags='-I${prefix}/include' --extra-ldflags='-L${prefix}/lib' --bindir='${prefix}/bin' --extra-libs='-ldl' --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libtheora"
@@ -37,7 +37,7 @@ class ffmpeg ($prefix="/usr/local", $tmpdir="/usr/local/src") {
     environment => "PKG_CONFIG_PATH=${prefix}/lib/pkgconfig",
     creates     => "${prefix}/bin/ffmpeg",
     timeout     => 0,
-    require     => Macro::Git-checkout["ffmpeg-checkout"],
+    require     => Macro::Git::Checkout["ffmpeg-checkout"],
   }
 
   exec { 'ffmpeg-qt-fast-start-build' :
