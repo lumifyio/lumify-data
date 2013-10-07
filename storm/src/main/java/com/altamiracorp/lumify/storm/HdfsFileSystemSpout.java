c8359529d2de9a7317f8f194054ad1a4972bda7a
Joe Ferner <joe@fernsroth.com>
2013-10-07 08:47:06 -0400

    delete on done

--- storm/src/main/java/com/altamiracorp/lumify/storm/HdfsFileSystemSpout.java
+++ storm/src/main/java/com/altamiracorp/lumify/storm/HdfsFileSystemSpout.java
@@ -1,11 +1,10 @@
 package com.altamiracorp.lumify.storm;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.io.IOException;
-import java.net.URI;
-import java.util.Map;
-
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.utils.Utils;
+import com.altamiracorp.lumify.config.ConfigurationHelper;
+import com.altamiracorp.lumify.model.AccumuloSession;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -13,12 +12,11 @@ import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import backtype.storm.spout.SpoutOutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.utils.Utils;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
 
-import com.altamiracorp.lumify.config.ConfigurationHelper;
-import com.altamiracorp.lumify.model.AccumuloSession;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class HdfsFileSystemSpout extends BaseFileSystemSpout {
     private static final Logger LOGGER = LoggerFactory.getLogger(HdfsFileSystemSpout.class.getName());
@@ -31,14 +29,14 @@ public class HdfsFileSystemSpout extends BaseFileSystemSpout {
     public void open(Map stormConf, TopologyContext context, SpoutOutputCollector collector) {
         super.open(stormConf, context, collector);
 
-        rootDataPath = (String) stormConf.get(BaseFileSystemSpout.DATADIR_CONFIG_NAME);
-        checkNotNull(rootDataPath, BaseFileSystemSpout.DATADIR_CONFIG_NAME + " is a required configuration parameter");
-        importPath = rootDataPath + "/import";
+        this.rootDataPath = (String) stormConf.get(BaseFileSystemSpout.DATADIR_CONFIG_NAME);
+        checkNotNull(this.rootDataPath, BaseFileSystemSpout.DATADIR_CONFIG_NAME + " is a required configuration parameter");
+        this.importPath = rootDataPath + "/import";
         Configuration conf = ConfigurationHelper.createHadoopConfigurationFromMap(stormConf);
         try {
             String hdfsRootDir = (String) stormConf.get(AccumuloSession.HADOOP_URL);
             hdfsFileSystem = FileSystem.get(new URI(hdfsRootDir), conf, "hadoop");
-            mkdirs(new Path(importPath));
+            mkdirs(new Path(this.importPath));
         } catch (Exception e) {
             collector.reportError(e);
         }
@@ -60,6 +58,16 @@ public class HdfsFileSystemSpout extends BaseFileSystemSpout {
         } catch (IOException e) {
             getCollector().reportError(e);
         }
+    }
+
+    @Override
+    public void safeAck(Object msgId) throws Exception {
+        String path = getPathFromMessageId(msgId);
+        checkNotNull(path, "path was null");
+        if (hdfsFileSystem.exists(new Path(path))) {
+            hdfsFileSystem.delete(new Path(path), false);
+        }
+        super.safeAck(msgId);
     }
 
     // TODO: we could speed this up by caching the list of files to work on instead of reading all of them each time
