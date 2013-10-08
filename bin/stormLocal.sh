264655712e5afd8328b9aac84aa01752e7b6467e
Joe Ferner <joe@fernsroth.com>
2013-10-08 16:04:12 -0400

    storm command

---
+++ bin/stormLocal.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+classpath=$(${DIR}/classpath.sh storm)
+if [ $? -ne 0 ]; then
+  echo "${classpath}"
+  exit
+fi
+
+if [ "$2" != '' ]; then
+  dir=$2
+else
+  dir=${DIR}/../data/import
+fi
+
+java \
+-Dfile.encoding=UTF-8 \
+-classpath ${classpath} \
+com.altamiracorp.lumify.storm.StormRunner \
+--datadir=${dir} \
+--local
