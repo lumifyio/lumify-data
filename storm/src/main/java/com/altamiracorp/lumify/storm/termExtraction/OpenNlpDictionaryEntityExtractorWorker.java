f38eea5c4a4ce72a213c21b16044618413f67197
Andy Keffalas <akeffala@nearinfinity.com>
2013-10-09 17:05:27 -0400

    Removed unused constructor

--- storm/src/main/java/com/altamiracorp/lumify/storm/termExtraction/OpenNlpDictionaryEntityExtractorWorker.java
+++ storm/src/main/java/com/altamiracorp/lumify/storm/termExtraction/OpenNlpDictionaryEntityExtractorWorker.java
@@ -1,30 +1,29 @@
 package com.altamiracorp.lumify.storm.termExtraction;
 
-import com.altamiracorp.lumify.config.ConfigurationHelper;
-import com.altamiracorp.lumify.core.ingest.termExtraction.TermExtractionAdditionalWorkData;
-import com.altamiracorp.lumify.core.ingest.termExtraction.TermExtractionWorker;
-import com.altamiracorp.lumify.core.ingest.termExtraction.TermExtractionResult;
-import com.altamiracorp.lumify.core.user.User;
-import com.altamiracorp.lumify.entityExtraction.OpenNlpDictionaryEntityExtractor;
-import com.google.inject.Inject;
-import org.apache.hadoop.conf.Configuration;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.Map;
 
+import org.apache.hadoop.conf.Configuration;
+
+import com.altamiracorp.lumify.config.ConfigurationHelper;
+import com.altamiracorp.lumify.core.ingest.termExtraction.TermExtractionAdditionalWorkData;
+import com.altamiracorp.lumify.core.ingest.termExtraction.TermExtractionResult;
+import com.altamiracorp.lumify.core.ingest.termExtraction.TermExtractionWorker;
+import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.entityExtraction.OpenNlpDictionaryEntityExtractor;
+import com.google.inject.Inject;
+
 public class OpenNlpDictionaryEntityExtractorWorker extends TermExtractionWorker {
     private OpenNlpDictionaryEntityExtractor openNlpDictionaryEntityExtractor;
-
-    public OpenNlpDictionaryEntityExtractorWorker() throws Exception {
-    }
 
     @Override
     protected TermExtractionResult doWork(InputStream work, TermExtractionAdditionalWorkData termExtractionAdditionalWorkData) throws Exception {
         return openNlpDictionaryEntityExtractor.extract(work);
     }
 
+    @Override
     public void prepare(Map conf, User user) throws InterruptedException, IOException, URISyntaxException {
         Configuration configuration = ConfigurationHelper.createHadoopConfigurationFromMap(conf);
         openNlpDictionaryEntityExtractor.prepare(configuration, user);
