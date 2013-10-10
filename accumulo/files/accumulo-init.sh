9031414ce8ed4952fe2da52ffe6693bb78d62418
David Singley <dsingley@nearinfinity.com>
2013-10-10 17:10:16 -0400

    made to work by Joe

--- accumulo/files/accumulo-init.sh
+++ accumulo/files/accumulo-init.sh
@@ -5,6 +5,9 @@ set password [lindex $argv 1]
 
 spawn /usr/lib/accumulo/bin/accumulo init
 
-expect "Instance name :" { send "$instancename"; sleep 1 }
-expect "Enter initial password for root:" { send "$password"; sleep 1; }
-expect "Confirm initial password for root:" { send "$password"; sleep 1; }
+expect {
+  "Instance name :" { send "$instancename\r"; exp_continue; }
+  "Delete existing entry from zookeeper?" { send "Y\n"; exp_continue; }
+  "Enter initial password" { send "$password\r"; exp_continue; }
+  "Confirm initial password" { send "$password\r"; exp_continue; }
+}
