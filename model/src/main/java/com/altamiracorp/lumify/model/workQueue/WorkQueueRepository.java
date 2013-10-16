8db285e27619e82dc9be669212980fe0f9444d02
Joe Ferner <joe@fernsroth.com>
2013-10-16 14:51:25 -0400

    made queue names constances

--- model/src/main/java/com/altamiracorp/lumify/model/workQueue/WorkQueueRepository.java
+++ model/src/main/java/com/altamiracorp/lumify/model/workQueue/WorkQueueRepository.java
@@ -3,22 +3,26 @@ package com.altamiracorp.lumify.model.workQueue;
 import org.json.JSONObject;
 
 public abstract class WorkQueueRepository {
+    public static final String ARTIFACT_HIGHLIGHT_QUEUE_NAME = "artifactHighlight";
+    public static final String TEXT_QUEUE_NAME = "text";
+    public static final String PROCESSED_VIDEO_QUEUE_NAME = "processedVideo";
+
     public void pushArtifactHighlight(String artifactGraphVertexId) {
         JSONObject artifactHighlightJson = new JSONObject();
         artifactHighlightJson.put("graphVertexId", artifactGraphVertexId);
-        pushOnQueue("artifactHighlight", artifactHighlightJson);
+        pushOnQueue(ARTIFACT_HIGHLIGHT_QUEUE_NAME, artifactHighlightJson);
     }
 
     public void pushText(String artifactGraphVertexId) {
         JSONObject textQueueDataJson = new JSONObject();
         textQueueDataJson.put("graphVertexId", artifactGraphVertexId);
-        pushOnQueue("text", textQueueDataJson);
+        pushOnQueue(TEXT_QUEUE_NAME, textQueueDataJson);
     }
 
     public void pushProcessedVideo(String artifactRowKey) {
         JSONObject json = new JSONObject();
         json.put("artifactRowKey", artifactRowKey);
-        pushOnQueue("processedVideo", json);
+        pushOnQueue(PROCESSED_VIDEO_QUEUE_NAME, json);
     }
 
     protected abstract void pushOnQueue(String queueName, JSONObject json, String... extra);
