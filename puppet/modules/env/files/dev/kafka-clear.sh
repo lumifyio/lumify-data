c5c7f8748ec8bdef2d0d8727cc1088ea03b83921
Susan Feng <susan.feng@altamiracorp.com>
2013-10-17 01:35:53 -0400

    create structuredDataEntity queue

--- puppet/modules/env/files/dev/kafka-clear.sh
+++ puppet/modules/env/files/dev/kafka-clear.sh
@@ -14,6 +14,8 @@ sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/brokers/topics/
 sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/brokers/topics/processedVideo"
 sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/brokers/topics/structuredData/0"
 sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/brokers/topics/structuredData"
+sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/brokers/topics/structuredDataEntity/0"
+sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/brokers/topics/structuredDataEntity"
 sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/brokers/topics"
 sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/consumers/text/192.168.33.10:9092:0"
 sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/consumers/text"
@@ -27,3 +29,4 @@ sudo -u zookeeper /usr/lib/zookeeper/bin/zkCli.sh "delete /kafka/consumers/artif
 /opt/kafka/bin/kafka-create-topic.sh localhost:2181/kafka artifactHighlight
 /opt/kafka/bin/kafka-create-topic.sh localhost:2181/kafka processedVideo
 /opt/kafka/bin/kafka-create-topic.sh localhost:2181/kafka structuredData
+/opt/kafka/bin/kafka-create-topic.sh localhost:2181/kafka structuredDataEntity
