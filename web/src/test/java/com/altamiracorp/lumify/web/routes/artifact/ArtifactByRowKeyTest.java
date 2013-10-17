95c11c54fb4eccce25ca80fe8bf30b1992a10496
Andy Keffalas <akeffala@nearinfinity.com>
2013-10-17 18:31:34 -0400

    Updated unit test to work with recent refactoring

--- web/src/test/java/com/altamiracorp/lumify/web/routes/artifact/ArtifactByRowKeyTest.java
+++ web/src/test/java/com/altamiracorp/lumify/web/routes/artifact/ArtifactByRowKeyTest.java
@@ -1,15 +1,25 @@
 package com.altamiracorp.lumify.web.routes.artifact;
 
-import com.altamiracorp.lumify.core.user.User;
-import com.altamiracorp.lumify.core.model.graph.GraphRepository;
-import com.altamiracorp.lumify.web.routes.RouteTestBase;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import javax.servlet.http.HttpSession;
+import com.altamiracorp.lumify.core.model.artifact.Artifact;
+import com.altamiracorp.lumify.core.model.artifact.ArtifactRowKey;
+import com.altamiracorp.lumify.core.model.graph.GraphRepository;
+import com.altamiracorp.lumify.core.model.graph.GraphVertex;
+import com.altamiracorp.lumify.core.model.ontology.PropertyName;
+import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.web.AuthenticationProvider;
+import com.altamiracorp.lumify.web.routes.RouteTestBase;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ArtifactByRowKeyTest extends RouteTestBase {
@@ -24,6 +34,9 @@ public class ArtifactByRowKeyTest extends RouteTestBase {
     @Mock
     private GraphRepository mockGraphRepository;
 
+    @Mock
+    private GraphVertex mockVertex;
+
     @Override
     @Before
     public void setUp() throws Exception {
@@ -34,22 +47,20 @@ public class ArtifactByRowKeyTest extends RouteTestBase {
 
     @Test
     public void testHandle() throws Exception {
-        throw new RuntimeException("storm refactor - not implemented"); // TODO storm refactor
-//        ArtifactRowKey artifactRowKey = ArtifactRowKey.build("testContents".getBytes());
-//        when(mockRequest.getAttribute("_rowKey")).thenReturn(artifactRowKey.toString());
-//        when(mockRequest.getSession()).thenReturn(mockSession);
-//        when(mockSession.getAttribute(AuthenticationProvider.CURRENT_USER_REQ_ATTR_NAME)).thenReturn(user);
-//
-//        Artifact artifact = new Artifact(artifactRowKey);
-//        artifact.getGenericMetadata()
-//                .setMimeType("text/html");
-//        when(mockArtifactRepository.findByRowKey(artifactRowKey.toString(), user)).thenReturn(artifact);
-//
-//        artifactByRowKey.handle(mockRequest, mockResponse, mockHandlerChain);
-//
-//        JSONObject responseJson = new JSONObject(responseStringWriter.getBuffer().toString());
-//        assertEquals(ArtifactRawByRowKey.getUrl(artifactRowKey), responseJson.getString("rawUrl"));
-//        assertEquals(artifactRowKey.toString(), responseJson.getJSONObject("key").getString("value"));
-//        assertEquals("document", responseJson.getString("type"));
+        ArtifactRowKey artifactRowKey = ArtifactRowKey.build("testContents".getBytes());
+        when(mockRequest.getAttribute("_rowKey")).thenReturn(artifactRowKey.toString());
+        when(mockRequest.getSession()).thenReturn(mockSession);
+        when(mockSession.getAttribute(AuthenticationProvider.CURRENT_USER_REQ_ATTR_NAME)).thenReturn(user);
+
+        Artifact artifact = new Artifact(artifactRowKey);
+        when(mockVertex.getProperty(PropertyName.SUBTYPE)).thenReturn("document");
+        when(mockGraphRepository.findVertexByRowKey(artifactRowKey.toString(), user)).thenReturn(mockVertex);
+        when(mockArtifactRepository.findByRowKey(artifactRowKey.toString(), user)).thenReturn(artifact);
+
+        artifactByRowKey.handle(mockRequest, mockResponse, mockHandlerChain);
+
+        JSONObject responseJson = new JSONObject(responseStringWriter.getBuffer().toString());
+        assertEquals(ArtifactRawByRowKey.getUrl(artifactRowKey), responseJson.getString("rawUrl"));
+        assertEquals(artifactRowKey.toString(), responseJson.getJSONObject("key").getString("value"));
     }
 }
