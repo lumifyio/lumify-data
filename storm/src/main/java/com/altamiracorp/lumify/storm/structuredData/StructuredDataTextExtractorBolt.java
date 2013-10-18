4ee1c257eae43e97637b51ed2dc9c972f1036e9b
Joe Ferner <joe@fernsroth.com>
2013-10-18 16:43:41 -0400

    primary file in sdte

--- storm/src/main/java/com/altamiracorp/lumify/storm/structuredData/StructuredDataTextExtractorBolt.java
+++ storm/src/main/java/com/altamiracorp/lumify/storm/structuredData/StructuredDataTextExtractorBolt.java
@@ -2,9 +2,11 @@ package com.altamiracorp.lumify.storm.structuredData;
 
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
+import com.altamiracorp.lumify.FileImporter;
 import com.altamiracorp.lumify.core.ingest.structuredData.StructuredDataExtractionWorker;
 import com.altamiracorp.lumify.storm.BaseArtifactProcessingBolt;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.ServiceLoader;
@@ -29,5 +31,16 @@ public class StructuredDataTextExtractorBolt extends BaseArtifactProcessingBolt 
     @Override
     protected ServiceLoader getServiceLoader() {
         return ServiceLoader.load(StructuredDataExtractionWorker.class);
+    }
+
+    @Override
+    protected File getPrimaryFileFromArchive(File archiveTempDir) {
+        for (File f : archiveTempDir.listFiles()) {
+            if (f.getName().endsWith(FileImporter.MAPPING_JSON_FILE_NAME_SUFFIX)) {
+                continue;
+            }
+            return f;
+        }
+        throw new RuntimeException("Could not find primary file");
     }
 }
