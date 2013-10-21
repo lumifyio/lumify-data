240143f9462b3061dba29a923342229e75ff8eb1
Rebecca Matous <rmatous@nearinfinity.com>
2013-10-21 14:19:38 -0400

    Added README for testing

---
+++ storm/src/main/java/com/altamiracorp/lumify/storm/video/TranscriptTextWorker.java
@@ -0,0 +1,11 @@
+package com.altamiracorp.lumify.storm.video;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rmatous
+ * Date: 10/17/13
+ * Time: 1:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TranscriptTextWorker {
+}
