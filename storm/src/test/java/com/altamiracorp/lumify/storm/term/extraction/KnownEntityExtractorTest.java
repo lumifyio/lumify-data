bdf316de7975025888d2219a7fa2c80e87a3039b
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-10-22 13:13:30 -0400

    adding basic test for KnownEntityExtractor

---
+++ storm/src/test/java/com/altamiracorp/lumify/storm/term/extraction/KnownEntityExtractorTest.java
@@ -0,0 +1,53 @@
+package com.altamiracorp.lumify.storm.term.extraction;
+
+
+import com.altamiracorp.lumify.core.ingest.term.extraction.TermExtractionResult;
+import com.altamiracorp.lumify.core.user.User;
+import junit.framework.Assert;
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.InputStream;
+
+import static groovy.util.GroovyTestCase.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class KnownEntityExtractorTest {
+    private KnownEntityExtractor extractor;
+    @Mock
+    private User user;
+    private Configuration config;
+    String dictionaryPath;
+
+
+    @Before
+    public void setup() {
+        dictionaryPath = getClass().getResource(".").getPath();
+        extractor = new KnownEntityExtractor();
+        config = new Configuration();
+        config.set("nlpConfPathPrefix", "file://" + dictionaryPath);
+    }
+
+    @Test
+    public void textExtract() throws Exception {
+
+        extractor.prepare(config, user);
+        InputStream in = getClass().getResourceAsStream("bffls.txt");
+        TermExtractionResult result = extractor.extract(in);
+        assertEquals(result.getTermMentions().size(), 3);
+        for (TermExtractionResult.TermMention termMention : result.getTermMentions()) {
+            assertTrue(termMention.isResolved());
+            assertEquals(termMention.getOntologyClassUri(), "person");
+            assertEquals(termMention.getSign(), "Joe Ferner");
+        }
+
+
+    }
+
+
+}
