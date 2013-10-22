27581b0afaa01c68f36583695f752af21d845d87
Andy Keffalas <akeffala@nearinfinity.com>
2013-10-22 14:20:28 -0400

    Genericize methods used to write to queues

--- core/src/main/java/com/altamiracorp/lumify/core/model/workQueue/WorkQueueRepository.java
+++ core/src/main/java/com/altamiracorp/lumify/core/model/workQueue/WorkQueueRepository.java
@@ -1,35 +1,55 @@
 package com.altamiracorp.lumify.core.model.workQueue;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Map;
+
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
 
 public abstract class WorkQueueRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkQueueRepository.class);
+
+    private static final String KEY_ARTIFACT_ROWKEY = "artifactRowKey";
+    private static final String KEY_GRAPH_VERTEX_ID = "graphVertexId";
+
     public static final String ARTIFACT_HIGHLIGHT_QUEUE_NAME = "artifactHighlight";
     public static final String TEXT_QUEUE_NAME = "text";
     public static final String PROCESSED_VIDEO_QUEUE_NAME = "processedVideo";
     public static final String STRUCTURED_DATA_ENTITY_QUEUE_NAME = "structuredDataEntity";
 
-    public void pushArtifactHighlight(String artifactGraphVertexId) {
-        JSONObject artifactHighlightJson = new JSONObject();
-        artifactHighlightJson.put("graphVertexId", artifactGraphVertexId);
-        pushOnQueue(ARTIFACT_HIGHLIGHT_QUEUE_NAME, artifactHighlightJson);
+    public void pushArtifactHighlight(final String artifactGraphVertexId) {
+        checkNotNull(artifactGraphVertexId);
+        writeToQueue(ARTIFACT_HIGHLIGHT_QUEUE_NAME, ImmutableMap.<String, String>of(KEY_GRAPH_VERTEX_ID, artifactGraphVertexId));
     }
 
-    public void pushText(String artifactGraphVertexId) {
-        JSONObject textQueueDataJson = new JSONObject();
-        textQueueDataJson.put("graphVertexId", artifactGraphVertexId);
-        pushOnQueue(TEXT_QUEUE_NAME, textQueueDataJson);
+    public void pushText(final String artifactGraphVertexId) {
+        checkNotNull(artifactGraphVertexId);
+        writeToQueue(TEXT_QUEUE_NAME, ImmutableMap.<String, String>of(KEY_GRAPH_VERTEX_ID, artifactGraphVertexId));
     }
 
-    public void pushProcessedVideo(String artifactRowKey) {
-        JSONObject json = new JSONObject();
-        json.put("artifactRowKey", artifactRowKey);
-        pushOnQueue(PROCESSED_VIDEO_QUEUE_NAME, json);
+    public void pushProcessedVideo(final String artifactRowKey) {
+        checkNotNull(artifactRowKey);
+        writeToQueue(PROCESSED_VIDEO_QUEUE_NAME, ImmutableMap.<String, String>of(KEY_ARTIFACT_ROWKEY, artifactRowKey));
     }
 
-    public void pushStructuredDataEntity (String artifactGraphVertexId) {
-        JSONObject structuredDataEntityJson = new JSONObject();
-        structuredDataEntityJson.put("graphVertexId", artifactGraphVertexId);
-        pushOnQueue(STRUCTURED_DATA_ENTITY_QUEUE_NAME, structuredDataEntityJson);
+    public void pushStructuredDataEntity(final String artifactGraphVertexId) {
+        checkNotNull(artifactGraphVertexId);
+        writeToQueue(STRUCTURED_DATA_ENTITY_QUEUE_NAME, ImmutableMap.<String, String>of(KEY_GRAPH_VERTEX_ID, artifactGraphVertexId));
+    }
+
+    private void writeToQueue(final String queueName, final Map<String, String> content) {
+        final JSONObject data = new JSONObject();
+
+        for(final Map.Entry<String, String> entry : content.entrySet()) {
+            data.put(entry.getKey(), entry.getValue());
+        }
+
+        LOGGER.debug(String.format("Writing data: %s to queue [%s]", data, queueName));
+        pushOnQueue(queueName, data);
     }
 
     protected abstract void pushOnQueue(String queueName, JSONObject json, String... extra);
