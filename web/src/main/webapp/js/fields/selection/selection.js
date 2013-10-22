e9950a4a707a2bcc57795c40a6ab390ff4618088
Jason Harwig <jharwig@nearinfinity.com>
2013-10-22 16:04:57 -0500

    [#58282338] Integrate new FieldSelection component into property editing in detail pane

--- web/src/main/webapp/js/fields/selection/selection.js
+++ web/src/main/webapp/js/fields/selection/selection.js
@@ -20,57 +20,63 @@ define([
 
             this.$node.html(template({placeholder:this.attr.placeholder}));
 
-            this.select('findPropertySelection')
-                .on('click', function(e) { 
-                    var target = $(e.target);
+            if (this.attr.properties.length === 0) {
+                this.select('findPropertySelection')
+                    .attr('placeholder', 'No valid properties')
+                    .attr('disabled', true);
+            } else {
+                this.select('findPropertySelection')
+                    .on('click', function(e) { 
+                        var target = $(e.target);
 
-                    if (!target.val()) {
-                        // Weirdness to force the typeahead to open when no value
-                        target.val(' ').typeahead('lookup').val(''); 
-                    } else {
-                        target.typeahead('lookup').select();
-                    }
-
-                    target.attr('placeholder', 'Type to filter properties');
-                })
-                .on('change blur', function(e) {
-                    var target = $(e.target);
-                    console.log('changing', self.currentProperty);
-                    if (self.currentProperty) {
-                        target.val(self.currentProperty.displayName || self.currentProperty.title);
-                    } else {
-                        target.val('');
-                    }
-                    target.attr('placeholder', self.attr.placeholder);
-                })
-                .typeahead({
-                    minLength: 0,
-                    source: _.map(this.attr.properties, function(p) {
-                        return p.displayName || p.title;
-                    }),
-                    matcher: function(item) {
-                        if (this.query === ' ') return -1;
-                        if (
-                            this.query && 
-                            self.currentProperty &&
-                                (self.currentProperty.displayName === this.query || 
-                                 self.currentProperty.title === this.query)
-                        ) {
-                            return 1;
+                        if (!target.val()) {
+                            // Weirdness to force the typeahead to open when no value
+                            target.val(' ').typeahead('lookup').val(''); 
+                        } else {
+                            target.typeahead('lookup').select();
                         }
-                        return Object.getPrototypeOf(this).matcher.apply(this, arguments);
-                    },
-                    sorter: function(items) {
-                        var sorted = Object.getPrototypeOf(this).sorter.apply(this, arguments),
-                            index;
 
-                        return sorted;
-                    },
-                    updater: function(item) {
-                        self.propertySelected(item);
-                        return item;
-                    }
-                });
+                        target.attr('placeholder', 'Type to filter properties');
+                    })
+                    .on('change blur', function(e) {
+                        var target = $(e.target);
+                        console.log('changing', self.currentProperty);
+                        if (self.currentProperty) {
+                            target.val(self.currentProperty.displayName || self.currentProperty.title);
+                        } else {
+                            target.val('');
+                        }
+                        target.attr('placeholder', self.attr.placeholder);
+                    })
+                    .typeahead({
+                        minLength: 0,
+                        source: _.map(this.attr.properties, function(p) {
+                            return p.displayName || p.title;
+                        }),
+                        matcher: function(item) {
+                            if (this.query === ' ') return -1;
+                            if (
+                                this.query && 
+                                self.currentProperty &&
+                                    (self.currentProperty.displayName === this.query || 
+                                     self.currentProperty.title === this.query)
+                            ) {
+                                return 1;
+                            }
+                            return Object.getPrototypeOf(this).matcher.apply(this, arguments);
+                        },
+                        sorter: function(items) {
+                            var sorted = Object.getPrototypeOf(this).sorter.apply(this, arguments),
+                                index;
+
+                            return sorted;
+                        },
+                        updater: function(item) {
+                            self.propertySelected(item);
+                            return item;
+                        }
+                    });
+            }
         });
 
         this.propertySelected = function(name) {
