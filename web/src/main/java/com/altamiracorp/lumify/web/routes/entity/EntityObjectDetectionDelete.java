18d79ad3c6f4b8b6d0ab3bf75bf973ab50c5b732
Susan Feng <susan.feng@altamiracorp.com>
2013-10-24 17:04:45 -0400

    fixed deleting object detection

--- web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityObjectDetectionDelete.java
+++ web/src/main/java/com/altamiracorp/lumify/web/routes/entity/EntityObjectDetectionDelete.java
@@ -1,72 +1,67 @@
 package com.altamiracorp.lumify.web.routes.entity;
 
-import com.altamiracorp.lumify.core.model.ModelSession;
+import com.altamiracorp.lumify.core.model.graph.GraphRelationship;
 import com.altamiracorp.lumify.core.model.graph.GraphRepository;
-import com.altamiracorp.lumify.core.model.search.SearchProvider;
-import com.altamiracorp.lumify.core.model.artifact.ArtifactRepository;
+import com.altamiracorp.lumify.core.model.graph.GraphVertex;
+import com.altamiracorp.lumify.core.model.ontology.LabelName;
+import com.altamiracorp.lumify.core.model.ontology.PropertyName;
+import com.altamiracorp.lumify.core.user.User;
 import com.altamiracorp.lumify.web.BaseRequestHandler;
 import com.altamiracorp.miniweb.HandlerChain;
 import com.google.inject.Inject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 public class EntityObjectDetectionDelete extends BaseRequestHandler {
     private final GraphRepository graphRepository;
-    private final ArtifactRepository artifactRepository;
-    private final SearchProvider searchProvider;
-    private final ModelSession modelSession;
 
     @Inject
     public EntityObjectDetectionDelete(
-            final ArtifactRepository artifactRepository,
-            final GraphRepository graphRepository,
-            final SearchProvider searchProvider,
-            final ModelSession modelSession) {
-        this.artifactRepository = artifactRepository;
+            final GraphRepository graphRepository) {
         this.graphRepository = graphRepository;
-        this.searchProvider = searchProvider;
-        this.modelSession = modelSession;
     }
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
-        throw new RuntimeException("storm refactor - not implemented"); // TODO storm refactor
+        JSONObject jsonObject = new JSONObject(getRequiredParameter(request, "objectInfo"));
+        User user = getUser(request);
 
-//        JSONObject jsonObject = new JSONObject(getRequiredParameter(request, "objectInfo"));
-//        JSONObject infoJson = jsonObject.getJSONObject("info");
-//        User user = getUser(request);
-//
-//        // Delete just the relationship if vertex has more than one relationship otherwise delete vertex
-//        String graphVertexId = jsonObject.getString("graphVertexId");
-//        JSONObject obj = graphRepository.findVertex(graphVertexId, user).toJson();
-//        Map<GraphRelationship, GraphVertex> relationships = graphRepository.getRelationships(graphVertexId, user);
-//        if (relationships.size() > 1) {
-//            GraphVertex artifactVertex = graphRepository.findVertexByRowKey(jsonObject.getString("artifactRowKey"), user);
-//            String edgeId = artifactVertex.getId() + ">" + graphVertexId + "|" + LabelName.CONTAINS_IMAGE_OF.toString();
-//            obj.put("edgeId", edgeId);
-//            graphRepository.removeRelationship(artifactVertex.getId(), graphVertexId, LabelName.CONTAINS_IMAGE_OF.toString(), user);
-//        } else {
-//            graphRepository.remove(graphVertexId, user);
-//            obj.put("remove", true);
-//        }
-//
-//        // Delete column from Artifact
-//        Artifact artifact = artifactRepository.findByRowKey(jsonObject.getString("artifactRowKey"), user);
-//        Row<ArtifactRowKey> rowKey = artifactRepository.toRow(artifact);
-//        String columnFamily = artifact.getArtifactDetectedObjects().getColumnFamilyName();
-//        String columnQualifier = infoJson.getString("_rowKey");
-//        for (Column column : rowKey.get(columnFamily).getColumns()) {
-//            if (column.getName().equals(columnQualifier)) {
-//                column.setDirty(true);
-//            }
-//        }
-//        modelSession.deleteColumn(rowKey, Artifact.TABLE_NAME, columnFamily, columnQualifier, user);
-//
-//        // Overwrite old ElasticSearch index
-//        Artifact newArtifact = artifactRepository.findByRowKey(jsonObject.getString("artifactRowKey"), user);
-//        searchProvider.add(newArtifact, user);
-//
-//        respondWithJson(response, obj);
+        // Delete just the relationship if vertex has more than one relationship otherwise delete vertex
+        String graphVertexId = jsonObject.getString("graphVertexId");
+        JSONObject obj = graphRepository.findVertex(graphVertexId, user).toJson();
+        Map<GraphRelationship, GraphVertex> relationships = graphRepository.getRelationships(graphVertexId, user);
+        GraphVertex artifactVertex = graphRepository.findVertexByRowKey(jsonObject.getString("artifactRowKey"), user);
+        if (relationships.size() > 1) {
+            String edgeId = artifactVertex.getId() + ">" + graphVertexId + "|" + LabelName.CONTAINS_IMAGE_OF.toString();
+            obj.put("edgeId", edgeId);
+            graphRepository.removeRelationship(artifactVertex.getId(), graphVertexId, LabelName.CONTAINS_IMAGE_OF.toString(), user);
+        } else {
+            graphRepository.remove(graphVertexId, user);
+            obj.put("remove", true);
+        }
+
+        // Delete the appropriate detected object json object from the array and reset the property
+        JSONArray detectedObjects = new JSONArray(artifactVertex.getProperty(PropertyName.DETECTED_OBJECTS).toString());
+        boolean deleted = false;
+        for (int i = 0; i < detectedObjects.length(); i++) {
+            JSONObject detectedObject = detectedObjects.getJSONObject(i);
+            if (detectedObject.get("graphVertexId").equals(jsonObject.get("graphVertexId"))) {
+                detectedObjects.remove(i);
+                deleted = true;
+            }
+        }
+        if (!deleted) {
+            throw new RuntimeException("Tag was not found in the list of detected objects");
+        }
+        artifactVertex.setProperty(PropertyName.DETECTED_OBJECTS, detectedObjects.toString());
+        graphRepository.save(artifactVertex, user);
+
+        //TODO: Overwrite old ElasticSearch index with new info
+
+        respondWithJson(response, obj);
     }
 }
