9f6d965103d9a09079d124ba8a9c1cc09ef45e42
Andy Keffalas <akeffala@nearinfinity.com>
2013-10-30 15:25:47 -0400

    [#59571554] Modified file import route to transfer uploaded file to topology for processing

--- web/src/main/java/com/altamiracorp/lumify/web/routes/artifact/ArtifactImport.java
+++ web/src/main/java/com/altamiracorp/lumify/web/routes/artifact/ArtifactImport.java
@@ -1,25 +1,31 @@
 package com.altamiracorp.lumify.web.routes.artifact;
 
-import com.altamiracorp.lumify.core.user.User;
-import com.altamiracorp.lumify.web.BaseRequestHandler;
-import com.altamiracorp.miniweb.HandlerChain;
-import com.google.inject.Inject;
-import org.apache.commons.io.IOUtils;
-import org.json.JSONObject;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+
+import org.apache.commons.fileupload.FileUploadBase;
+import org.apache.commons.fileupload.ParameterParser;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.altamiracorp.lumify.web.BaseRequestHandler;
+import com.altamiracorp.miniweb.HandlerChain;
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
 
 public class ArtifactImport extends BaseRequestHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArtifactImport.class);
+
+    private static final String PARAMS_FILENAME = "filename";
+    private static final String UNKNOWN_FILENAME = "unknown_filename";
+
     private final FileImporter fileImporter;
+
 
     @Inject
     public ArtifactImport(FileImporter fileImporter) {
@@ -28,42 +34,37 @@ public class ArtifactImport extends BaseRequestHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
-        User user = getUser(request);
-        InputStream fis;
-        if (request.getContentType().equals(MediaType.MULTIPART_FORM_DATA)) {
-            List<Part> files = new ArrayList<Part>(request.getParts());
+        if( ServletFileUpload.isMultipartContent(request) ) {
+            final List<Part> files = Lists.newArrayList(request.getParts());
+
             if (files.size() != 1) {
                 throw new RuntimeException("Wrong number of uploaded files. Expected 1 got " + files.size());
             }
 
-            Part file = files.get(0);
-            fis = file.getInputStream();
+            final Part file = files.get(0);
+            final String fileName = getFilename(file);
+
+            LOGGER.debug("Processing uploaded file: " + fileName);
+            fileImporter.writeFile(file.getInputStream(), fileName);
         } else {
-            fis = request.getInputStream();
+            LOGGER.warn("Could not process request without multipart content");
         }
-        File tempFile = File.createTempFile("fileImport", ".bin");
-        writeToTempFile(fis, tempFile);
-
-        List<FileImporter.Result> results = fileImporter.writePackage(tempFile, "File Upload", user);
-
-        tempFile.delete();
-
-        JSONObject json = new JSONObject();
-        json.put("results", FileImporter.Result.toJson(results));
-
-        respondWithJson(response, json);
     }
 
-    private void writeToTempFile(InputStream in, File tempFile) throws IOException {
-        try {
-            FileOutputStream out = new FileOutputStream(tempFile);
-            try {
-                IOUtils.copy(in, out);
-            } finally {
-                out.close();
+    private static String getFilename(Part part) {
+        String fileName = UNKNOWN_FILENAME;
+
+        final ParameterParser parser = new ParameterParser();
+        parser.setLowerCaseNames(true);
+
+        final Map params = parser.parse(part.getHeader(FileUploadBase.CONTENT_DISPOSITION), ';');
+        if( params.containsKey(PARAMS_FILENAME) ) {
+            final String name = (String) params.get(PARAMS_FILENAME);
+            if( name != null ) {
+                fileName = name.trim();
             }
-        } finally {
-            in.close();
         }
+
+        return fileName;
     }
 }
--- web/src/main/java/com/altamiracorp/lumify/web/routes/artifact/FileImporter.java
+++ web/src/main/java/com/altamiracorp/lumify/web/routes/artifact/FileImporter.java
@@ -1,202 +1,62 @@
 package com.altamiracorp.lumify.web.routes.artifact;
 
-import com.altamiracorp.lumify.core.ingest.video.VideoTranscript;
-import com.altamiracorp.lumify.core.model.graph.GraphVertex;
-import com.altamiracorp.lumify.core.user.User;
-import com.altamiracorp.lumify.core.model.artifact.Artifact;
-import com.altamiracorp.lumify.core.model.artifact.ArtifactRepository;
-import com.google.common.io.Files;
-import com.google.inject.Inject;
-import net.lingala.zip4j.core.ZipFile;
-import org.apache.accumulo.core.client.MutationsRejectedException;
-import org.apache.commons.io.FileUtils;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.TrueFileFilter;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import com.altamiracorp.lumify.core.config.Configuration;
+import com.google.inject.Inject;
 
 public class FileImporter {
-    private static final Logger LOGGER = LoggerFactory.getLogger(FileImporter.class.getName());
-    private final ArtifactRepository artifactRepository;
-    // TODO storm refactor
-    //private final YoutubeccReader youtubeccReader = new YoutubeccReader();
+    private static final String UNKNOWN_DATA_DIR = "/lumify/data/unknown";
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileImporter.class);
+    private final FileSystem hdfsFileSystem;
 
     @Inject
-    public FileImporter(ArtifactRepository artifactRepository) {
-        this.artifactRepository = artifactRepository;
+    public FileImporter(Configuration config) {
+        try {
+            String hdfsRootDir = config.get(Configuration.HADOOP_URL);
+            org.apache.hadoop.conf.Configuration hadoopConfiguration = new org.apache.hadoop.conf.Configuration();
+            hdfsFileSystem = FileSystem.get(new URI(hdfsRootDir), hadoopConfiguration, "hadoop");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public Result writeFile(File file, String source, JSONObject mappingJson, VideoTranscript videoTranscript, User user) throws IOException, MutationsRejectedException {
-        throw new RuntimeException("storm refactor - not implemented"); // TODO storm refactor
-//        if (file.getName().startsWith(".")) {
-//            return null;
-//        }
-//        if (isSupportingFile(file)) {
-//            return null;
-//        }
-//        Artifact artifact = artifactRepository.createArtifactFromInputStream(
-//                file.length(),
-//                new FileInputStream(file),
-//                file.getName(),
-//                file.lastModified(),
-//                user);
-//        artifact.getGenericMetadata().setSubject(artifact.getGenericMetadata().getFileName());
-//        artifact.getGenericMetadata().setSource(source);
-//        if (mappingJson != null) {
-//            artifact.getGenericMetadata().setMappingJson(mappingJson);
-//        }
-//        if (videoTranscript != null) {
-//            artifact.getContent().mergeVideoTranscript(videoTranscript);
-//        }
-//
-//        LOGGER.info("Writing artifact: " + artifact.getGenericMetadata().getFileName() + "." + artifact.getGenericMetadata().getFileExtension() + " (rowId: " + artifact.getRowKey().toString() + ")");
-//        artifactRepository.save(artifact, user);
-//        artifact = artifactRepository.findByRowKey(artifact.getRowKey().toString(), user);
-//        GraphVertex graphVertex = artifactRepository.saveToGraph(artifact, user);
-//        return new Result(file, artifact, graphVertex);
+    public void writeFile(InputStream fileStream, String uploadFileName) throws IOException {
+        checkNotNull(fileStream);
+        checkNotNull(uploadFileName);
+        checkArgument(!uploadFileName.isEmpty());
+
+        final File tempFile = File.createTempFile("fileImport", "bin");
+        LOGGER.debug("Writing stream to temporary file location: " + tempFile.getAbsolutePath());
+        writeToTempFile(fileStream, tempFile);
+
+        hdfsFileSystem.copyFromLocalFile(true, true, new Path(tempFile.getAbsolutePath()), new Path(UNKNOWN_DATA_DIR, uploadFileName));
+        LOGGER.info("Uploaded file written to distributed filesystem");
     }
 
-    public ArrayList<Result> writePackage(File file, String source, User user) throws Exception {
-        ArrayList<Result> results;
-        ZipFile zipped = new ZipFile(file);
-        if (zipped.isValidZipFile()) {
-            File tempDir = Files.createTempDir();
+    private void writeToTempFile(InputStream in, File tempFile) throws IOException {
+        try {
+            FileOutputStream out = new FileOutputStream(tempFile);
             try {
-                LOGGER.info("Extracting: " + file.getAbsoluteFile() + " to " + tempDir.getAbsolutePath());
-                zipped.extractAll(tempDir.getAbsolutePath());
-
-                results = writeDirectory(tempDir, "*", source, user);
+                IOUtils.copy(in, out);
             } finally {
-                FileUtils.deleteDirectory(tempDir);
+                out.close();
             }
-        } else {
-            results = new ArrayList<Result>();
-            Result r = writeFile(file, source, null, null, user);
-            results.add(r);
-        }
-        return results;
-    }
-
-    public ArrayList<Result> writeDirectory(File directory, String pattern, String source, User user) throws Exception {
-        throw new RuntimeException("TODO storm refactor"); // TODO storm refactor
-//        ArrayList<Result> results = new ArrayList<Result>();
-//        IOFileFilter fileFilter = new WildcardFileFilter(pattern);
-//        IOFileFilter directoryFilter = TrueFileFilter.INSTANCE;
-//        Iterator<File> fileIterator = FileUtils.iterateFiles(directory, fileFilter, directoryFilter);
-//
-//        while (fileIterator.hasNext()) {
-//            File f = fileIterator.next();
-//            if (f.isFile() && !isSupportingFile(f)) {
-//                JSONObject mappingJson = readMappingJsonFile(f);
-//                VideoTranscript videoTranscript = readVideoTranscript(f);
-//                Result r = writeFile(f, source, mappingJson, videoTranscript, user);
-//                if (r != null) {
-//                    results.add(r);
-//                }
-//            }
-//        }
-//        return results;
-    }
-
-//    private boolean isSupportingFile(File f) {
-//        if (f.getName().endsWith(FileImporter.MAPPING_JSON_FILE_NAME_SUFFIX)) {
-//            return true;
-//        }
-//        if (f.getName().endsWith(FileImporter.YOUTUBE_CC_FILE_NAME_SUFFIX)) {
-//            return true;
-//        }
-//        if (f.getName().endsWith((FileImporter.SRT_CC_FILE_NAME_SUFFIX))) {
-//            return true;
-//        }
-//        return false;
-//    }
-
-//    private JSONObject readMappingJsonFile(File f) throws JSONException, IOException {
-//        File mappingJsonFile = new File(f.getAbsolutePath() + FileImporter.MAPPING_JSON_FILE_NAME_SUFFIX);
-//        JSONObject mappingJson = null;
-//        if (mappingJsonFile.exists()) {
-//            FileInputStream mappingJsonFileIn = new FileInputStream(mappingJsonFile);
-//            try {
-//                mappingJson = new JSONObject(IOUtils.toString(mappingJsonFileIn));
-//            } finally {
-//                mappingJsonFileIn.close();
-//            }
-//        }
-//        return mappingJson;
-//    }
-
-    private VideoTranscript readVideoTranscript(File f) throws Exception {
-        throw new RuntimeException("storm refactor - not implemented"); // TODO storm refactor
-//        VideoTranscript videoTranscript = null;
-//        File youtubeccFile = new File(f.getAbsolutePath() + FileImporter.YOUTUBE_CC_FILE_NAME_SUFFIX);
-//        if (youtubeccFile.exists()) {
-//            videoTranscript = new VideoTranscript();
-//            VideoTranscript youtubeccTranscript = youtubeccReader.read(youtubeccFile);
-//            videoTranscript.merge(youtubeccTranscript);
-//        }
-//
-//        File srtccFile = new File(f.getAbsolutePath() + FileImporter.SRT_CC_FILE_NAME_SUFFIX);
-//        if ( srtccFile.exists() ) {
-//            videoTranscript = videoTranscript == null ? new VideoTranscript() : videoTranscript;
-//            VideoTranscript youtubeccTranscript = SubRip.read(srtccFile);
-//            videoTranscript.merge(youtubeccTranscript);
-//        }
-//        return videoTranscript;
-    }
-
-    public static class Result {
-        private final String name;
-        private final String artifactRowKey;
-        private final String graphVertexId;
-
-        public Result(File file, Artifact artifact, GraphVertex graphVertex) {
-            this.name = file.getName();
-            this.artifactRowKey = artifact.getRowKey().toString();
-            this.graphVertexId = graphVertex.getId();
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getArtifactRowKey() {
-            return artifactRowKey;
-        }
-
-        public String getGraphVertexId() {
-            return graphVertexId;
-        }
-
-        public JSONObject toJson() {
-            try {
-                JSONObject json = new JSONObject();
-                json.put("name", getName());
-                json.put("artifactRowKey", getArtifactRowKey());
-                json.put("graphVertexId", getGraphVertexId());
-                return json;
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static JSONArray toJson(Collection<Result> results) {
-            JSONArray r = new JSONArray();
-            for (Result result : results) {
-                r.put(result.toJson());
-            }
-            return r;
+        } finally {
+            in.close();
         }
     }
 }
