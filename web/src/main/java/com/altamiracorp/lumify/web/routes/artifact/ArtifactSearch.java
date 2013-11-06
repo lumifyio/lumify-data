9f8d566da150d1d69ba20019ab10566f1ad5a974
Mark Nachazel <mark.nachazel@altamiracorp.com>
2013-11-06 09:26:32 -0500

    fixed merge in artifactsearch

--- web/src/main/java/com/altamiracorp/lumify/web/routes/artifact/ArtifactSearch.java
+++ web/src/main/java/com/altamiracorp/lumify/web/routes/artifact/ArtifactSearch.java
@@ -1,25 +1,22 @@
 package com.altamiracorp.lumify.web.routes.artifact;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.altamiracorp.lumify.model.graph.GraphPagedResults;
+import com.altamiracorp.lumify.core.model.artifact.ArtifactRepository;
+import com.altamiracorp.lumify.core.model.graph.GraphPagedResults;
+import com.altamiracorp.lumify.core.model.graph.GraphVertex;
+import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
+import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.web.BaseRequestHandler;
+import com.altamiracorp.miniweb.HandlerChain;
+import com.google.inject.Inject;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.altamiracorp.lumify.core.user.User;
-import com.altamiracorp.lumify.core.model.graph.GraphVertex;
-import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
-import com.altamiracorp.lumify.core.model.artifact.ArtifactRepository;
-import com.altamiracorp.lumify.web.BaseRequestHandler;
-import com.altamiracorp.miniweb.HandlerChain;
-import com.google.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
 
 public class ArtifactSearch extends BaseRequestHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(ArtifactSearch.class);
@@ -39,8 +36,8 @@ public class ArtifactSearch extends BaseRequestHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
         final String query = getRequiredParameter(request, "q");
         final String filter = getRequiredParameter(request, "filter");
-        final int offset = (int)getOptionalParameterLong(request, "offset", 0);
-        final int size = (int)getOptionalParameterLong(request, "size", 100);
+        final int offset = (int) getOptionalParameterLong(request, "offset", 0);
+        final int size = (int) getOptionalParameterLong(request, "size", 100);
         final String type = getOptionalParameter(request, "subType");
 
         User user = getUser(request);
