d49df0ef1103d7ac632e0e4de1efccee40deea34
Susan Feng <susan.feng@altamiracorp.com>
2013-11-20 15:35:57 -0500

    modified rowkey to contain date and time

--- core/src/main/java/com/altamiracorp/lumify/core/model/audit/AuditRowKey.java
+++ core/src/main/java/com/altamiracorp/lumify/core/model/audit/AuditRowKey.java
@@ -3,12 +3,17 @@ package com.altamiracorp.lumify.core.model.audit;
 import com.altamiracorp.bigtable.model.RowKey;
 import com.altamiracorp.lumify.core.util.RowKeyHelper;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AuditRowKey extends RowKey {
     public AuditRowKey(String rowKey) {
         super(rowKey);
     }
 
     public static AuditRowKey build(String vertexId) {
-        return new AuditRowKey(RowKeyHelper.buildMinor(vertexId, RowKeyHelper.padLong(System.currentTimeMillis())));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date date = new Date();
+        return new AuditRowKey(RowKeyHelper.buildMinor(vertexId, dateFormat.format(date)));
     }
 }
