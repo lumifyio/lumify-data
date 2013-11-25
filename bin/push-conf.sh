2e2596612cd4f410df9a2419f56ac9da37a056c9
David Singley <dsingley@nearinfinity.com>
2013-11-25 16:02:31 -0500

    remove obsolete script

--- bin/push-conf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eu
-
-sudo -u hdfs /usr/lib/hadoop/bin/hadoop fs -ls /conf
-[ $? -eq 0 ] && sudo -u hdfs /usr/lib/hadoop/bin/hadoop fs -rmr /conf
-sudo -u hdfs /usr/lib/hadoop/bin/hadoop fs -copyFromLocal /vagrant/conf /
-sudo -u hdfs /usr/lib/hadoop/bin/hadoop fs -lsr /conf
