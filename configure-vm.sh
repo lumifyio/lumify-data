262d09c01c979acab4ea075ba3073ae1f11ead80
David Singley <dsingley@nearinfinity.com>
2013-11-26 15:07:42 -0500

    -n a few more directories

--- configure-vm.sh
+++ configure-vm.sh
@@ -4,7 +4,7 @@ rpm -q epel-release-6-8 > /dev/null \
   || rpm -ivH http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 yum -y groupinstall development-tools
-yum -y install fedora-packager
+yum -y install yasm fedora-packager
 
 id -u makerpm > /dev/null \
   || useradd makerpm
@@ -15,7 +15,8 @@ id -Gn makerpm | grep -q mock \
 echo 'makerpm
 makerpm' | passwd makerpm 2> /dev/null
 
-su - makerpm -c "mkdir -p /home/makerpm/repo && createrepo /home/makerpm/repo"
+su - makerpm -c "mkdir -p /home/makerpm/repo/RPMS/x86_64 /home/makerpm/repo/{SRPMS,source}"
+su - makerpm -c "createrepo /home/makerpm/repo"
 
 cat <<-EOM > /etc/yum.repos.d/lumify-local.repo
 [lumify-local]
@@ -26,3 +27,4 @@ gpgcheck=0
 EOM
 
 su - makerpm -c rpmdev-setuptree
+su - makerpm -c "mkdir -p /home/makerpm/source"
