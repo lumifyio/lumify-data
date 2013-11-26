0fbb95a810b122a03fcabb193b6f3d28ed543d26
Joe Ferner <joe@fernsroth.com>
2013-11-26 15:59:01 -0500

    ccextractor

--- lumify-rpms/functions.sh
+++ lumify-rpms/functions.sh
@@ -11,3 +11,23 @@ function _clone {
 
   (cd ${SOURCE_DIR}/${name} && git checkout ${treeish})
 }
+
+function _download {
+  local name=$1
+  local url=$2
+  local fname=$3
+
+  if [ ! -d ${SOURCE_DIR}/${name} ]; then
+    #curl "${url}" -s -L --fail -o "${SOURCE_DIR}/${fname}"
+    local extension="${fname##*.}"
+    case $extension in
+    "zip")
+        unzip -od "${SOURCE_DIR}" "${SOURCE_DIR}/${fname}"
+        ;;
+    *)
+        echo "Unhandled extension to extract $extension"
+        exit 1
+        ;;
+    esac
+  fi
+}
