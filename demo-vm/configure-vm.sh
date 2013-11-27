deb016679685d4b0ac02eaccaf61545b0146feff
David Singley <dsingley@nearinfinity.com>
2013-11-27 15:31:50 -0500

    rename .war for deployment

--- demo-vm/configure-vm.sh
+++ demo-vm/configure-vm.sh
@@ -4,4 +4,4 @@ mkdir -p /opt/lumify/config
 cp /vagrant/demo-vm/configuration.properties /opt/lumify/config/configuration.properties
 
 cp /vagrant/deployment/application.xml /opt/jetty/contexts
-cp /vagrant/lumify-public/web/target/application-1.0-SNAPSHOT.war /opt/jetty/webapps
+cp /vagrant/lumify-public/web/target/application-1.0-SNAPSHOT.war /opt/jetty/webapps/application.war
