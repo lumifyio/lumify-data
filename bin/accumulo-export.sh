cec3038dc98e7429270cfa71de03ebdd3896c945
David Singley <dsingley@nearinfinity.com>
2013-11-29 16:57:23 -0500

    sorry

--- bin/accumulo-export.sh
+++ bin/accumulo-export.sh
@@ -1,39 +1,41 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Please specify local output directory'
-    exit 1
+  echo 'Please specify local filesystem path to export tabled to'
+  exit 1
 fi
 
-LOCAL_OUTPUT_DIR=$1
+LOCAL_DIR=$1
+
 
 ACCUMULO_SHELL='sudo -u accumulo /usr/lib/accumulo/bin/accumulo shell -u root -p password'
 
 for table in $($ACCUMULO_SHELL -e 'tables' | grep -v $(date +'%Y')); do
-  echo ${table}
-
   if [[ ${table} == atc_* ]]; then
+    echo "exporting ${table}..."
+
     hadoop fs -mkdir /tmp/exportedTables/${table}
 
     table_cloned=${table}_cloned
 
-    $ACCUMULO_SHELL -e "clonetable ${table} ${table_cloned}"
-    $ACCUMULO_SHELL -e "offline -t ${table_cloned}"
-    $ACCUMULO_SHELL -e "exporttable -t ${table_cloned} /tmp/exportedTables/${table}"
+    $ACCUMULO_SHELL -e "clonetable ${table} ${table_cloned}" 2>/dev/null
+    $ACCUMULO_SHELL -e "offline -t ${table_cloned}" 2>/dev/null
+    $ACCUMULO_SHELL -e "exporttable -t ${table_cloned} /tmp/exportedTables/${table}" 2>/dev/null
 
     while true; do
       echo 'sleeping 5 seconds...'
       sleep 5
-      hadoop fs -ls /tmp/exportedTables/${table}/distcp.txt /tmp/exportedTables/$table/exportMetadata.zip && break
+      hadoop fs -ls /tmp/exportedTables/${table}/distcp.txt \
+                    /tmp/exportedTables/$table/exportMetadata.zip &>/dev/null && break
     done
 	
-    mkdir -p ${LOCAL_OUTPUT_DIR}/${table}
+    mkdir -p ${LOCAL_DIR}/${table}
 
     for src in $(hadoop fs -cat /tmp/exportedTables/${table}/distcp.txt); do
-      hadoop fs -get ${src} ${LOCAL_OUTPUT_DIR}/${table}
+      hadoop fs -get ${src} ${LOCAL_DIR}/${table}
     done
 
-    $ACCUMULO_SHELL -e "deletetable -f ${table_cloned}"
+    $ACCUMULO_SHELL -e "deletetable -f ${table_cloned}" 2>/dev/null
   fi
 done
 
--- bin/accumulo-import.sh
+++ bin/accumulo-import.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Please specify path of tables to import'
-    exit 1
+if [[ ! -d "$1" ]]; then
+  echo 'Please specify local filesystem path to import tables from'
+  exit 1
 fi
 
-ACCUMULO_SHELL='sudo -u accumulo /usr/lib/accumulo/bin/accumulo shell -u root -p password'
 EXPORTED_TABLE_DIR=$1
-for dir in $(find ${EXPORTED_TABLE_DIR}/* -type d | xargs -l1 basename); do
-  echo ${dir}
-  
+
+ACCUMULO_SHELL='sudo -u accumulo /usr/lib/accumulo/bin/accumulo shell -u root -p password'
+
+for dir in $(find ${EXPORTED_TABLE_DIR} -name 'exportMetadata.zip' | xargs -L1 dirname | sed -e 's|.*/||'); do
+  echo "importing ${dir}..."
+
   hadoop fs -mkdir /tmp/importTables/${dir}
-  hadoop fs -put ${EXPORTED_TABLE_DIR}/${dir}/* /tmp/importTables/${dir}/
-  
+  hadoop fs -put ${EXPORTED_TABLE_DIR}/${dir}/* /tmp/importTables/${dir}
+
   $ACCUMULO_SHELL -e "importtable ${dir} /tmp/importTables/${dir}"
 done
 
 hadoop fs -rmr /tmp/importTables
-
