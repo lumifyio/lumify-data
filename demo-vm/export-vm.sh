069aaf64a229403b9a9744169e55a094a124ec8c
David Singley <dsingley@nearinfinity.com>
2013-11-29 14:46:39 -0500

    reaname VM and remove shared folders for export

--- demo-vm/export-vm.sh
+++ demo-vm/export-vm.sh
@@ -8,15 +8,43 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
+function _get_vm_property {
+  local id=$1
+  local key=$2
+
+  VBoxManage showvminfo ${id} --machinereadable | awk -F '=' "/${key}=/ {print \$2}" | sed -e 's/"//g'
+}
+
+function _set_vm_property {
+  local id=$1
+  local key=$2
+  local value=$3
+
+  VBoxManage modifyvm ${id} --${key} ${value}
+}
+
+function _remove_vm_shared_folder {
+  local id=$1
+  local name=$2
+
+  VBoxManage sharedfolder remove ${id} --name ${name}
+}
+
 
 id=$(cat ${DIR}/../.vagrant/machines/demo/virtualbox/id)
 version=$(date +'%Y-%m-%d')
 ova_filename=${DIR}/lumify-${version}.ova
 
-(cd ${DIR}/.. && vagrant halt demo)
+vagrant halt demo
+
+for name in $(_get_vm_property ${id} 'SharedFolderNameMachineMapping[0-9]'); do
+  _remove_vm_shared_folder ${id} ${name}
+done
+
+original_vm_name=$(_get_vm_property ${id} name)
+_set_vm_property ${id} name lumify-demo
 
 rm -f ${ova_filename}
-
 VBoxManage export ${id} \
                   -o ${ova_filename} \
                   --vsys 0 \
@@ -24,3 +52,5 @@ VBoxManage export ${id} \
                   --producturl 'http://lumify.io' \
                   --version ${version} \
                   --eulafile ${DIR}/LICENSE-2.0.html
+
+_set_vm_property ${id} name ${original_vm_name}
