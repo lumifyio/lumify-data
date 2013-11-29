df5c48d926fb4010184ec012f6c434a4e608c764
David Singley <dsingley@nearinfinity.com>
2013-11-29 14:45:39 -0500

    script to cleanup the VM before packaging

---
+++ demo-vm/clean-vm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/opt/stop.sh
+
+sudo rm -f /var/log/accumulo/*
+sudo rm -f /var/log/elasticsearch/*
+sudo find /var/log/hadoop-* -type f -exec rm {} \;
+sudo rm -f /var/log/hue/*
+sudo rm -f /var/log/zookeeper/*
+sudo rm -f /opt/jetty/logs/*
+sudo rm -f /opt/kafka/logs/*
+sudo rm -f /opt/storm/logs/*
+
+sudo find /home -name '.bash_history' -exec rm -f {} \;
