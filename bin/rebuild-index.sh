e7c95aed9392d19e4d6134c36c30664bf5bd01f4
David Singley <dsingley@nearinfinity.com>
2013-12-03 09:52:22 -0500

    new script for Joe's TBD class

---
+++ bin/rebuild-index.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+classpath=$(${DIR}/classpath.sh lumify-public/storm-lumify)
+if [ $? -ne 0 ]; then
+  echo "${classpath}"
+  exit
+fi
+
+cd ${DIR}/../lumify-public
+
+java \
+-Dfile.encoding=UTF-8 \
+-Djava.security.krb5.realm= \
+-Djava.security.krb5.kdc= \
+-classpath ${classpath} \
+-Xmx1024M \
+com.altamiracorp.lumify.storm.searchIndex.searchIndexTool
