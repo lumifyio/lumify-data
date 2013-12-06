e7eb1c99690554ec4a8a31ef8f7ae17f4d1f3338
Joe Ferner <joe@fernsroth.com>
2013-12-06 10:00:29 -0500

    added push and pull repo to rpm project

---
+++ lumify-rpms/push-repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+source ${DIR}/setenv.sh
+
+rm -rf repo/repodata
+createrepo --baseurl=${LUMIFYREPO_URL} repo
+
+s3cmd sync --delete-removed repo/ s3://bits.lumify.io/yum/
