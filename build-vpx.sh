7295d01b861630e6607d54cbf6dbfc6e3dadf78f
Joe Ferner <joe@fernsroth.com>
2013-12-06 21:47:32 -0500

    add test to vpx to see if patch is already applied

--- build-vpx.sh
+++ build-vpx.sh
@@ -11,14 +11,14 @@ DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 source ${DIR}/setenv.sh
 source ${DIR}/functions.sh
 
-
 name="lumify-vpx"
 version="1.2.0"
 release="dist"
 
 _clone ${name} http://git.chromium.org/webm/libvpx.git v${version}
 
-cd ${SOURCE_DIR}/${name}
-git apply ${DIR}/source/vpx.patch
+if ! grep --quiet "\-\-build" ${SOURCE_DIR}/${name}/build/make/configure.sh; then
+    (cd ${SOURCE_DIR}/${name} && git apply ${DIR}/source/vpx.patch)
+fi
 
 _build ${name} ${version} ${release}
