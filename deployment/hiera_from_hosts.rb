89487ce11fdd42f87ca8af1fa112ee19a93e8087
David Singley <dsingley@nearinfinity.com>
2013-12-10 16:39:03 -0500

    more things no one told me about...

--- deployment/hiera_from_hosts.rb
+++ deployment/hiera_from_hosts.rb
@@ -14,22 +14,34 @@ File.read(ARGV[0]).each_line do |line|
   end
 end
 
+def get(cluster, regex, type)
+  array = cluster.select{|k,v| k.match(regex)}.collect{|k,v| v[type]}.flatten
+  array.size == 1 ? array.first : array
+end
+
 hiera = Hash.new
-hiera['proxy_url'] = 'http://10.0.3.10:8080'
+hiera['proxy_url'] = 'http://' + get(cluster, /puppet/, :ip) + ':8080'
 hiera['hadoop_masters'] = cluster['namenode'][:ip].to_a
-hiera['hadoop_slaves'] = cluster.select{|k,v| k.match(/node\d{2}/)}.collect{|k,v| v[:ip]}.flatten
+hiera['hadoop_slaves'] = get(cluster, /node\d{2}/, :ip)
 hiera['accumulo_example_config'] = '3GB/native-standalone'
 hiera['accumulo_masters'] = cluster['accumulomaster'][:name].to_a
-hiera['accumulo_slaves'] = cluster.select{|k,v| k.match(/node\d{2}/)}.collect{|k,v| v[:name]}.flatten
+hiera['accumulo_slaves'] = get(cluster, /node\d{2}/, :name)
+zk_port = 2181
 zk_nodes = Hash.new
 cluster.select{|k,v| k.match(/zk\d/)}.each do |k,v|
   n = k.match(/zk(\d{2})/).captures[0].to_i
-  zk_nodes[n] = "#{v[:ip]}:2181"
+  zk_nodes[n] = "#{v[:ip]}:#{zk_port}"
 end
+hiera['zookeeper_port'] = zk_port
 hiera['zookeeper_nodes'] = zk_nodes
-hiera['namenode_ipaddress'] = cluster['namenode'][:ip].first
+hiera['namenode_ipaddress'] = cluster['namenode'][:ip]
 hiera['namenode_hostname'] = 'namenode'
-hiera['elasticsearch_locations'] = cluster.select{|k,v| k.match(/node\d{2}/)}.collect{|k,v| v[:ip]}.flatten.collect{|ip| "#{ip}:9300"}
+hiera['elasticsearch_locations'] = get(cluster, /node\d{2}/, :ip).collect{|ip| "#{ip}:9300"}
+hiera['kafka_host_ipaddress'] = get(cluster, /kafka\d{2}/, :ip).first
+hiera['storm_nimbus_host'] = get(cluster, /stormmaster/, :ip)
+hiera['storm_nimbus_thrift_port'] = 6627
+hiera['storm_supervisor_slots_ports'] = [6700, 6701, 6702, 6703]
+hiera['storm_ui_port'] = 8081
 hiera['jetty_confidential_port'] = 443
 hiera['jetty_key_store_path'] = '/opt/lumify/config/jetty.jks'
 hiera['jetty_key_store_password'] = 'OBF:1v2j1uum1xtv1zej1zer1xtn1uvk1v1v'
