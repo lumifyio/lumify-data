c666042b6fbfd6e59b724f214eecb7df054f3ca8
Rebecca Matous <rmatous@nearinfinity.com>
2013-12-12 11:49:40 -0500

    fixed stormLocal.sh to work

--- bin/stormLocal.sh
+++ bin/stormLocal.sh
@@ -19,5 +19,5 @@ java \
 -Dfile.encoding=UTF-8 \
 -Djava.library.path=$LD_LIBRARY_PATH \
 -classpath ${classpath} \
-com.altamiracorp.lumify.storm.facebook.StormRunner \
+com.altamiracorp.lumify.facebook.facebook.StormFacebookRunner \
 --local
