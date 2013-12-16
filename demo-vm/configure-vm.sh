2a7feb48d7a1e218da677457c8ade9d99b71a58a
Susan Feng <susan.feng@altamiracorp.com>
2013-12-15 19:56:49 -0500

    fix more paths

--- demo-vm/configure-vm.sh
+++ demo-vm/configure-vm.sh
@@ -29,21 +29,25 @@ mkdir -p /opt/lumify/config
 cp /vagrant/demo-vm/configuration.properties /opt/lumify/config/configuration.properties
 
 # run maven
+echo 'Running maven'
 su - vagrant -c 'cd /vagrant && mvn package install -P storm-jar,web-war -DskipTests' 2>&1 \
   | tee /vagrant/mvn.log \
   | grep '\[INFO\] Building'
 
 # deploy the webapp
-cp /vagrant/deployment/application.xml /opt/jetty/contexts
-cp /vagrant/lumify-public/web/target/application-1.0-SNAPSHOT.war /opt/jetty/webapps/application.war
+echo 'Deploying the webapp'
+cp /vagrant/deployment/lumify.xml /opt/jetty/contexts
+cp /vagrant/lumify-public/lumify-web/target/lumify-web-1.0-SNAPSHOT.war /opt/jetty/webapps/lumify.war
 
 # restore sample data
+echo 'restoring sample data'
 cp /vagrant/bin/accumulo-import.sh /opt/lumify
 cp /vagrant/demo-vm/sample-data.tgz /opt/lumify
 /opt/lumify/format.sh
 /opt/lumify/accumulo-import.sh /opt/lumify/sample-data.tgz
 
 # deploy the open source topology
+echo 'Deploy the open source topology'
 /opt/storm/bin/storm list \
   | grep -q 'lumify\s*ACTIVE' && /opt/storm/bin/storm kill lumify
 /opt/storm/bin/storm jar \
