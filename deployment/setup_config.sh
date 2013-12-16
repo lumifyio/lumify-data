fe4c3cf9aef32b18ef5f254ebe96cb085008ad16
David Singley <dsingley@nearinfinity.com>
2013-12-16 12:56:48 -0500

    correct HDFS config path

--- deployment/setup_config.sh
+++ deployment/setup_config.sh
@@ -3,5 +3,6 @@
 most_recent_config_tgz=$(ls config-*.tgz | tail -1)
 most_recent_config=$(basename ${most_recent_config_tgz} .tgz)
 tar xzf ${most_recent_config_tgz}
-hadoop fs -rmr /lumify/config || true
-hadoop fs -put ${most_recent_config}/config /lumify/config
+hadoop fs -rm -r /lumify/config || true
+hadoop fs -mkdir -p /lumify/config
+hadoop fs -put ${most_recent_config}/config/* /lumify/config
