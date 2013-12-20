9c1923831b1aeb38fb760a8efeedc4f56655e224
Joe Ferner <joe@fernsroth.com>
2013-12-20 16:55:39 -0500

    connect to 10 at a time

--- lumify-enterprise-tools/src/main/java/com/altamiracorp/lumify/tools/JmxClient.java
+++ lumify-enterprise-tools/src/main/java/com/altamiracorp/lumify/tools/JmxClient.java
@@ -11,19 +11,18 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class JmxClient extends CommandLineBase {
     private final Object sysoutLock = new Object();
 
     public static void main(String[] args) throws Exception {
         int res = new JmxClient().run(args);
-        if (res != 0) {
-            System.exit(res);
-        }
+        System.exit(res);
+    }
+
+    public JmxClient() {
+        initFramework = false;
     }
 
     @Override
@@ -47,11 +46,17 @@ public class JmxClient extends CommandLineBase {
         String ipsString = cmd.getOptionValue("ips");
         String[] ips = ipsString.split(",");
 
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ArrayList<Future<JMXConnector>> connections = new ArrayList<Future<JMXConnector>>();
+        ExecutorService executor = Executors.newFixedThreadPool(10);
         for (String ip : ips) {
-            connectAsync(executor, ip);
+            connections.add(connectAsync(executor, ip));
         }
 
+        for (Future<JMXConnector> connection : connections) {
+            connection.get(10, TimeUnit.SECONDS);
+        }
+
+        System.out.println("DONE");
         return 0;
     }
 
