c080408e876fc6bb5a1442e26df7dedfb73991c8
Joe Ferner <joe@fernsroth.com>
2013-12-20 11:31:54 -0500

    clean up code for more readability

--- lumify-enterprise-tools/src/main/java/com/altamiracorp/lumify/tools/WikipediaExtractionImport.java
+++ lumify-enterprise-tools/src/main/java/com/altamiracorp/lumify/tools/WikipediaExtractionImport.java
@@ -13,39 +13,70 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class WikipediaExtractionImport {
-
-    public static String OUTPUT_PATH = "/lumify/data/unknown";
+    private static final String OUTPUT_PATH_KEY = "outputPath";
 
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, NullWritable, NullWritable> {
+        private String outputPath;
+        private FileSystem fs;
 
-        public void map(LongWritable key, Text value, OutputCollector outputCollector, Reporter reporter) throws IOException {
-            Configuration conf = new Configuration();
-            FileSystem fs = FileSystem.get(conf);
-
-            String[] tabSeparatedValues = value.toString().split("\t");
-            if (tabSeparatedValues.length >= 5) {
-                String title = tabSeparatedValues[1];
-                String body = tabSeparatedValues[4];
-
-                reporter.setStatus(title + " (" + body.length() + ")");
-
-                String filename = title.replaceAll("[^A-Za-z0-9]", "_");
-                Path tempPath = new Path("./" + filename);
-                Path ingestPath = new Path(OUTPUT_PATH + "/" + filename);
-
-                OutputStream os = fs.create(tempPath);
-                os.write(body.replaceAll("\\\\n", "\n").getBytes());
-                os.close();
-
-                fs.rename(tempPath, ingestPath);
-            } else {
-                reporter.setStatus("key " + key.toString() + " has < 5 fields");
+        @Override
+        public void configure(JobConf job) {
+            try {
+                super.configure(job);
+                Configuration conf = new Configuration();
+                outputPath = job.get(OUTPUT_PATH_KEY);
+                fs = FileSystem.get(conf);
+            } catch (IOException ex) {
+                throw new RuntimeException("Could not configure", ex);
             }
         }
+
+        public void map(LongWritable key, Text value, OutputCollector outputCollector, Reporter reporter) throws IOException {
+            try {
+                LineData lineData = processLine(value.toString());
+                reporter.setStatus(lineData.title + " (" + lineData.body.length() + ")");
+                writeFile(lineData);
+            } catch (Exception ex) {
+                reporter.setStatus("Could not process key: " + key.toString() + ": " + ex.getMessage());
+            }
+        }
+
+        private void writeFile(LineData lineData) throws IOException {
+            Path tempFileName = new Path(outputPath, lineData.filename + "_COPYING_");
+            Path outputFileName = new Path(outputPath, lineData.filename);
+            OutputStream os = fs.create(tempFileName);
+            try {
+                os.write(lineData.body.getBytes());
+            } finally {
+                os.close();
+            }
+
+            fs.rename(tempFileName, outputFileName);
+        }
+    }
+
+    private static LineData processLine(String line) {
+        String[] tabSeparatedValues = line.split("\t");
+        if (tabSeparatedValues.length < 5) {
+            throw new RuntimeException("line has < 5 fields");
+        }
+
+        LineData lineData = new LineData();
+        lineData.title = tabSeparatedValues[1];
+        lineData.body = tabSeparatedValues[4].replaceAll("\\\\n", "\n");
+        lineData.filename = lineData.title.replaceAll("[^A-Za-z0-9]", "_");
+        return lineData;
+    }
+
+    private static class LineData {
+        public String title;
+        public String body;
+        public String filename;
     }
 
     public static void main(String[] args) throws Exception {
         JobConf conf = new JobConf(WikipediaExtractionImport.class);
+        conf.set(OUTPUT_PATH_KEY, args[1]);
         conf.setJobName("wex-import");
         conf.setMapperClass(Map.class);
         conf.setInputFormat(TextInputFormat.class);
