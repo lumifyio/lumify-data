7523ac94e4ff5c9442ebb217f08e9545d3c6a9e7
Joe Ferner <joe@fernsroth.com>
2013-12-24 09:05:42 -0500

    jvm info

---
+++ lumify-enterprise-tools/src/main/java/com/altamiracorp/lumify/tools/jmxclient/JvmJmxMBeanProcessor.java
@@ -0,0 +1,33 @@
+package com.altamiracorp.lumify.tools.jmxclient;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class JvmJmxMBeanProcessor extends JmxMBeanProcessor {
+    @Override
+    public ProcessResult process(MBeanServerConnection mbeanServerConnection, String source, ObjectName mbeanName) throws Exception {
+        if (!mbeanName.getCanonicalName().startsWith("java.lang")) {
+            return null;
+        }
+
+        if (mbeanName.getCanonicalName().contains("type=Runtime")) {
+            Map<String, Object> attributes = getAttributes(mbeanServerConnection, mbeanName);
+            return new ProcessResult("JVM", "Runtime", source, runtimeToColumns(attributes), false);
+        }
+
+        return null;
+    }
+
+    private List<ProcessResultColumn> runtimeToColumns(Map<String, Object> attributes) {
+        List<ProcessResultColumn> columns = new ArrayList<ProcessResultColumn>();
+        Long uptime = (Long) attributes.get("Uptime");
+        double uptimeSec = uptime.doubleValue() / 1000.0 / 60;
+        columns.add(new ProcessResultColumn("Uptime (min)", uptimeSec, ProcessResultColumnTotal.None));
+        columns.add(new ProcessResultColumn("Start Time", new Date((Long) attributes.get("StartTime")), ProcessResultColumnTotal.None));
+        return columns;
+    }
+}
