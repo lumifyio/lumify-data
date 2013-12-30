e3f4052abf7ee0ae2be443a2eb3d78da4a6b5006
Susan Feng <susan.feng@altamiracorp.com>
2013-12-30 11:04:37 -0500

    brought Server class back

--- bin/900_Server.sh
+++ bin/900_Server.sh
@@ -8,15 +8,23 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
-pushd ${DIR}/../lumify-public/lumify-web > /dev/null
-MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
-mvn \
-	-Dfile.encoding=UTF-8 \
-	-Djava.awt.headless=true \
-	-Djava.security.krb5.realm= \
-	-Djava.security.krb5.kdc= \
-	-Djetty.keystore.path=${DIR}/../config/ssl/lumify.jks \
-	-Djetty.keystore.password=password \
-	$LUMIFY_JETTY_ARGS \
-	jetty:run
-popd > /dev/null
+classpath=$(${DIR}/classpath.sh lumify-public/lumify-web)
+if [ $? -ne 0 ]; then
+    echo "${classpath}"
+exit
+
+fi
+  cd ${DIR}/../lumify-public
+
+  java \
+    -Dfile.encoding=UTF-8 \
+    -Djava.awt.headless=true \
+    -Djava.security.krb5.realm= \
+    -Djava.security.krb5.kdc= \
+    -classpath ${classpath} \
+    -Xmx1024M \
+    com.altamiracorp.lumify.web.Server \
+    --port=8080 \
+    --keyStorePath=${DIR}/../config/ssl/lumify.jks \
+    --keyStorePassword=password
+fi
