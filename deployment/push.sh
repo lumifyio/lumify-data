36030d458a6f252c0533422c4c0f3daebbb7791a
David Singley <dsingley@nearinfinity.com>
2013-12-30 10:28:19 -0500

    also build the tools uberjar

--- deployment/push.sh
+++ deployment/push.sh
@@ -57,7 +57,7 @@ function run_maven {
     echo 'maven ok.' >&2
   else
     echo 'running maven...' >&2
-    local mvn_output="$(cd ..; mvn clean install -P storm-jar,web-war -DskipTests=true)"
+    local mvn_output="$(cd ..; mvn clean install -P tools-jar,storm-jar,web-war -DskipTests=true)"
     local mvn_exit=$?
     if [ ${mvn_exit} -ne 0 ]; then
       echo "${mvn_output}" >&2
