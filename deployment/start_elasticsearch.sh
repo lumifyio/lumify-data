210b80c07b0fd3525a47ed70b33c1bbb605fa801
Joe Ferner <joe@fernsroth.com>
2014-01-02 10:01:53 -0500

    move deployment directory

--- deployment/start_elasticsearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SSH_OPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=QUIET'
-
-hosts_file=$1
-
-for node in $(awk '/node[0-9]+/ {print $1}' ${hosts_file}); do
-  ssh ${SSH_OPTS} ${node} initctl start elasticsearch
-done
