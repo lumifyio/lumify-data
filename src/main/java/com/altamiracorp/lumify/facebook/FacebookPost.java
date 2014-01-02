39d964ba6d0f6dcd6090a59ec913c226992289e2
Joe Ferner <joe@fernsroth.com>
2014-01-02 09:16:49 -0500

    more fields and better inheritance

---
+++ src/main/java/com/altamiracorp/lumify/facebook/FacebookPost.java
@@ -0,0 +1,151 @@
+package com.altamiracorp.lumify.facebook;
+
+import com.altamiracorp.lumify.core.ingest.ArtifactExtractedInfo;
+import com.altamiracorp.lumify.core.model.artifact.Artifact;
+import com.altamiracorp.lumify.core.model.artifact.ArtifactRowKey;
+import com.altamiracorp.lumify.core.model.artifact.ArtifactType;
+import com.altamiracorp.lumify.core.model.audit.AuditAction;
+import com.altamiracorp.lumify.core.model.audit.AuditRepository;
+import com.altamiracorp.lumify.core.model.graph.GraphRepository;
+import com.altamiracorp.lumify.core.model.graph.GraphVertex;
+import com.altamiracorp.lumify.core.model.graph.InMemoryGraphVertex;
+import com.altamiracorp.lumify.core.model.ontology.Concept;
+import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
+import com.altamiracorp.lumify.core.model.ontology.PropertyName;
+import com.altamiracorp.lumify.core.model.ontology.VertexType;
+import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.core.util.HdfsLimitOutputStream;
+import com.thinkaurelius.titan.core.attribute.Geoshape;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+public class FacebookPost {
+    private static final String PROFILE_ID = "profileId";
+    private static final String COORDS = "coords";
+    private static final String TAGGEED_UIDS = "tagged_uids";
+    private static final String TIMESTAMP = "timestamp";
+    private static final String POSTED_RELATIONSHIP = "postPostedByProfile";
+    private static final String MENTIONED_RELATIONSHIP = "postMentionedProfile";
+    private static final String FACEBOOK = "Facebook";
+    private static final String MESSAGE = "message";
+    private static final String AUTHOR_UID = "author_uid";
+    private static final String FACEBOOK_PROFILE = "facebookProfile";
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacebookBolt.class);
+    private static final String PROCESS = FacebookPost.class.getName();
+    private FacebookBolt facebookBolt = new FacebookBolt();
+
+
+    protected ArtifactExtractedInfo processPostArtifact(JSONObject post) throws Exception {
+        //extract knowledge from post
+        String message = post.getString(MESSAGE);
+        Long name_uid = post.getLong(AUTHOR_UID);
+        String author_uid = name_uid.toString();
+        Date time = new Date(post.getLong(TIMESTAMP) * 1000);
+        //use extracted information
+        ArtifactRowKey build = ArtifactRowKey.build(post.toString().getBytes());
+        String rowKey = build.toString();
+
+        HdfsLimitOutputStream textOut = new HdfsLimitOutputStream(facebookBolt.getFileSystem(), Artifact.MAX_SIZE_OF_INLINE_FILE);
+        try {
+            if (message != null) {
+                textOut.write(message.getBytes());
+            }
+        } finally {
+            textOut.close();
+        }
+
+        ArtifactExtractedInfo artifactExtractedInfo = new ArtifactExtractedInfo();
+        if (textOut.hasExceededSizeLimit()) {
+            artifactExtractedInfo.setTextHdfsPath(textOut.getHdfsPath().toString());
+        } else {
+            artifactExtractedInfo.setText(new String (textOut.getSmall()));
+        }
+        artifactExtractedInfo.setSource(FACEBOOK);
+        artifactExtractedInfo.setRaw(post.toString().getBytes());
+        artifactExtractedInfo.setMimeType("text/plain");
+        artifactExtractedInfo.setRowKey(rowKey);
+        artifactExtractedInfo.setArtifactType(ArtifactType.DOCUMENT.toString());
+        artifactExtractedInfo.setAuthor(author_uid);
+        artifactExtractedInfo.setDate(time);
+        artifactExtractedInfo.setProcess(PROCESS);
+        if (message.length() == 0) {
+            artifactExtractedInfo.setTitle("Facebook Image Post");
+        } else if (message.length() > 140) {
+            String shortTitle = message.substring(0, 137) + "...";
+            artifactExtractedInfo.setTitle(shortTitle);
+        } else {
+            artifactExtractedInfo.setTitle(message);
+        }
+
+        //write artifact with extracted info to accumulo and create entity
+        return artifactExtractedInfo;
+    }
+
+    protected GraphVertex processPostVertex (JSONObject post, GraphVertex posting, GraphRepository graphRepository, AuditRepository auditRepository, OntologyRepository ontologyRepository, User user) throws Exception {
+        Long name_uid = post.getLong(AUTHOR_UID);
+        String author_uid = name_uid.toString();
+        LOGGER.info("Saving Facebook post to accumulo and as graph vertex: " + posting.getId());
+        Concept profileConcept = ontologyRepository.getConceptByName(FACEBOOK_PROFILE, user);
+        String profileConceptId = profileConcept.getId();
+        List<String> modifiedProperties = new ArrayList<String>();
+
+        //create entities for each of the ids tagged or author and the relationships
+        GraphVertex authorVertex = graphRepository.findVertexByPropertyAndType(PROFILE_ID, author_uid, VertexType.ENTITY, user);
+        if (authorVertex == null) {
+            authorVertex = new InMemoryGraphVertex();
+            authorVertex.setProperty(PROFILE_ID, author_uid);
+            authorVertex.setProperty(PropertyName.TITLE.toString(), author_uid);
+            authorVertex.setProperty(PropertyName.TYPE.toString(), VertexType.ENTITY.toString());
+            authorVertex.setProperty(PropertyName.SUBTYPE.toString(), profileConceptId);
+            graphRepository.save(authorVertex, user);
+            graphRepository.commit();
+            auditRepository.auditEntity(AuditAction.CREATE.toString(), authorVertex.getId(), posting.getId(), profileConceptId, author_uid, PROCESS, "", user);
+            auditRepository.auditEntityProperties(AuditAction.UPDATE.toString(), authorVertex, PROFILE_ID, PROCESS, "", user);
+            auditRepository.auditEntityProperties(AuditAction.UPDATE.toString(), authorVertex, PropertyName.TYPE.toString(), PROCESS, "", user);
+        }
+        graphRepository.saveRelationship(authorVertex.getId(), posting.getId(), POSTED_RELATIONSHIP, user);
+        String postedRelationshipLabelDisplayName = ontologyRepository.getDisplayNameForLabel(POSTED_RELATIONSHIP, user);
+        String text = posting.getProperty(PropertyName.TITLE).toString();
+        auditRepository.auditRelationships(AuditAction.CREATE.toString(), posting, authorVertex, postedRelationshipLabelDisplayName, PROCESS, "", user);
+        if (post.get(TAGGEED_UIDS) instanceof JSONObject) {
+            Iterator tagged = post.getJSONObject(TAGGEED_UIDS).keys();
+            while (tagged.hasNext()) {
+                String next = tagged.next().toString();
+                GraphVertex taggedVertex = graphRepository.findVertexByPropertyAndType(PROFILE_ID, next, VertexType.ENTITY, user);
+                if (taggedVertex == null) {
+                    taggedVertex = new InMemoryGraphVertex();
+                    taggedVertex.setProperty(PROFILE_ID, next);
+                    taggedVertex.setProperty(PropertyName.TITLE.toString(), next);
+                    taggedVertex.setProperty(PropertyName.TYPE.toString(), VertexType.ENTITY.toString());
+                    taggedVertex.setProperty(PropertyName.SUBTYPE.toString(), profileConceptId);
+                    graphRepository.save(taggedVertex, user);
+                    graphRepository.commit();
+                    auditRepository.auditEntity(AuditAction.CREATE.toString(), taggedVertex.getId(), posting.getId(), profileConceptId, next, PROCESS, "", user);
+                    auditRepository.auditEntityProperties(AuditAction.UPDATE.toString(), taggedVertex, PROFILE_ID, PROCESS, "", user);
+                    auditRepository.auditEntityProperties(AuditAction.UPDATE.toString(), taggedVertex, PropertyName.TYPE.toString(), PROCESS, "", user);
+                }
+                graphRepository.saveRelationship(posting.getId(), taggedVertex.getId(), MENTIONED_RELATIONSHIP, user);
+                String mentionedRelationshipLabelDisplayName = ontologyRepository.getDisplayNameForLabel(MENTIONED_RELATIONSHIP, user);
+                auditRepository.auditRelationships(AuditAction.CREATE.toString(), posting, taggedVertex, mentionedRelationshipLabelDisplayName, PROCESS, "", user);
+            }
+        }
+
+        if (post.has(COORDS) && !post.getJSONObject(COORDS).equals(JSONObject.NULL)) {
+            JSONObject coordinates = post.getJSONObject(COORDS);
+            Geoshape geo = Geoshape.point(coordinates.getDouble("latitude"), coordinates.getDouble("longitude"));
+            posting.setProperty(PropertyName.GEO_LOCATION, geo);
+            modifiedProperties.add(PropertyName.GEO_LOCATION.toString());
+            auditRepository.auditEntityProperties(AuditAction.UPDATE.toString(), posting, PropertyName.GEO_LOCATION.toString(), PROCESS, "", user);
+        }
+
+        graphRepository.save(posting, user);
+
+        return posting;
+    }
+}
---
+++ src/test/java/com.altamiracorp.lumify.facebook/FacebookPostTest.java
@@ -0,0 +1,189 @@
+package com.altamiracorp.lumify.facebook;
+
+import com.altamiracorp.lumify.core.ingest.ArtifactExtractedInfo;
+import com.altamiracorp.lumify.core.model.audit.AuditRepository;
+import com.altamiracorp.lumify.core.model.graph.GraphRepository;
+import com.altamiracorp.lumify.core.model.graph.GraphVertex;
+import com.altamiracorp.lumify.core.model.graph.InMemoryGraphVertex;
+import com.altamiracorp.lumify.core.model.ontology.Concept;
+import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
+import com.altamiracorp.lumify.core.model.ontology.PropertyName;
+import com.altamiracorp.lumify.core.model.ontology.VertexType;
+import com.altamiracorp.lumify.core.model.search.SearchProvider;
+import com.altamiracorp.lumify.core.user.SystemUser;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Matchers.any;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.when;
+import org.mockito.stubbing.Answer;
+import java.io.InputStream;
+import java.util.Iterator;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FacebookPostTest {
+    @Mock
+    private SearchProvider searchProvider;
+    @Mock
+    GraphRepository graphRepository;
+    @Mock
+    AuditRepository auditRepository;
+    @Mock
+    OntologyRepository ontologyRepository;
+    @Mock
+    SystemUser systemUser;
+    @Mock
+    private GraphVertex postVertex;
+    @Mock
+    private GraphVertex authorVertex;
+    @Mock
+    private GraphVertex taggedVertex;
+    @Mock
+    private Iterator keys;
+    @Mock
+    private InMemoryGraphVertex inMemoryGraphVertex;
+    @Mock
+    private Concept facebookConcept;
+    @Mock
+    private Concept emailConcept;
+
+
+    private FacebookPost facebookPost;
+    private JSONObject normalPostObject;
+    private JSONObject emptyPostObject;
+    private JSONObject longPostObject;
+    private ArtifactExtractedInfo returnedExtractedInfo;
+    private GraphVertex returnedVertex;
+
+    @Before
+    public void setup() {
+        setNormalPostObject();
+        setEmptyPostObject();
+        setLongPostObject();
+    }
+
+    @Test
+    public void testProcessNormalArtifact () throws Exception {
+        facebookPost = new FacebookPost();
+        returnedExtractedInfo = facebookPost.processPostArtifact(normalPostObject);
+        assertEquals("One day I will live at the White House", returnedExtractedInfo.getTitle());
+        assertEquals("12345", returnedExtractedInfo.getAuthor());
+        assertEquals("Facebook", returnedExtractedInfo.getSource());
+    }
+
+    @Test
+    public void testProcessEmptyArtifact () throws Exception {
+        facebookPost = new FacebookPost();
+        returnedExtractedInfo = facebookPost.processPostArtifact(emptyPostObject);
+        assertEquals("Facebook Image Post", returnedExtractedInfo.getTitle());
+        assertEquals("12345", returnedExtractedInfo.getAuthor());
+        assertEquals("Facebook", returnedExtractedInfo.getSource());
+    }
+
+    @Test
+    public void testProcessLongArtifact () throws Exception {
+        facebookPost = new FacebookPost();
+        returnedExtractedInfo = facebookPost.processPostArtifact(longPostObject);
+        assertEquals("One day I will live at the White House or maybe just a quite large house that is all white on the outside; as long as it has a nice big y...", returnedExtractedInfo.getTitle());
+        assertEquals("12345", returnedExtractedInfo.getAuthor());
+        assertEquals("Facebook", returnedExtractedInfo.getSource());
+    }
+
+    @Test
+    public void testProcessHdfsArtifact () {
+
+    }
+    @Test
+    public void testProcessPostVertex () throws Exception {
+        facebookPost = new FacebookPost();
+        when(graphRepository.findVertexByPropertyAndType("profileId", "12345", VertexType.ENTITY, systemUser)).thenReturn(authorVertex);
+        when(graphRepository.findVertexByPropertyAndType("profileId", "67890", VertexType.ENTITY, systemUser)).thenReturn(taggedVertex);
+
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                postVertex   = (GraphVertex) invocationOnMock.getArguments()[0];
+                authorVertex = (GraphVertex) invocationOnMock.getArguments()[0];
+                taggedVertex = (GraphVertex) invocationOnMock.getArguments()[0];
+                return null;
+            }
+        }).when(searchProvider).add(any(GraphVertex.class), any(InputStream.class));
+
+        when(ontologyRepository.getConceptByName("facebookProfile", systemUser)).thenReturn(facebookConcept);
+        when(ontologyRepository.getConceptByName("emailAddress", systemUser)).thenReturn(emailConcept);
+        when(facebookConcept.getId()).thenReturn("32");
+        when(emailConcept.getId()).thenReturn("32");
+        when(graphRepository.save(postVertex, systemUser)).thenReturn("");
+        when(graphRepository.save(authorVertex, systemUser)).thenReturn("");
+        when(graphRepository.save(taggedVertex, systemUser)).thenReturn("");
+        when(postVertex.getId()).thenReturn("");
+        when(authorVertex.getId()).thenReturn("");
+        when(taggedVertex.getId()).thenReturn("");
+        when(postVertex.getProperty(PropertyName.TITLE)).thenReturn("");
+        when(keys.next()).thenReturn(taggedVertex);
+
+        returnedVertex = facebookPost.processPostVertex(normalPostObject, postVertex, graphRepository, auditRepository, ontologyRepository, systemUser);
+
+        verify(graphRepository, times(1)).save(any(GraphVertex.class), eq(systemUser));
+        verify(graphRepository, times(2)).saveRelationship(anyString(), anyString(), anyString(), eq(systemUser));
+        verify(graphRepository, times(2)).findVertexByPropertyAndType(eq("profileId"), anyString(), eq(VertexType.ENTITY), eq(systemUser));
+    }
+
+    @Test
+    public void testProcessPostUserVertices () throws Exception {
+        facebookPost = new FacebookPost();
+        when(graphRepository.findVertexByPropertyAndType("profileId", "12345", VertexType.ENTITY, systemUser)).thenReturn(null);
+        when(graphRepository.findVertexByPropertyAndType("profileId", "67890", VertexType.ENTITY, systemUser)).thenReturn(null);
+        when(graphRepository.save(postVertex, systemUser)).thenReturn("");
+        when(graphRepository.save(authorVertex, systemUser)).thenReturn("");
+        when(graphRepository.save(taggedVertex, systemUser)).thenReturn("");
+        when(postVertex.getId()).thenReturn("");
+        when(authorVertex.getId()).thenReturn("");
+        when(taggedVertex.getId()).thenReturn("");
+        when(postVertex.getProperty(PropertyName.TITLE)).thenReturn("");
+        when(keys.next()).thenReturn(taggedVertex);
+        when(ontologyRepository.getConceptByName("facebookProfile", systemUser)).thenReturn(facebookConcept);
+        when(ontologyRepository.getConceptByName("emailAddress", systemUser)).thenReturn(emailConcept);
+        when(facebookConcept.getId()).thenReturn("32");
+        when(emailConcept.getId()).thenReturn("32");
+        returnedVertex = facebookPost.processPostVertex(normalPostObject, postVertex, graphRepository, auditRepository, ontologyRepository, systemUser);
+
+        verify(graphRepository, times(3)).save(any(GraphVertex.class), eq(systemUser));
+        verify(graphRepository, times(2)).saveRelationship(anyString(), anyString(), anyString(), eq(systemUser));
+        verify(graphRepository, times(2)).findVertexByPropertyAndType(eq("profileId"), anyString(), eq(VertexType.ENTITY), eq(systemUser));
+    }
+
+    private void setNormalPostObject() {
+        normalPostObject = new JSONObject();
+        normalPostObject.put("message", "One day I will live at the White House");
+        normalPostObject.put("author_uid", 12345);
+        normalPostObject.put("timestamp", 1388172642);
+        JSONObject tagged = new JSONObject();
+        tagged.put("67890", 67890);
+        normalPostObject.put("tagged_uids", tagged);
+        JSONObject coords = new JSONObject();
+        coords.put("latitude", 38.9876);
+        coords.put("longitude", -77.1234);
+        normalPostObject.put("coords", coords);
+    }
+
+    private void setEmptyPostObject() {
+        emptyPostObject = new JSONObject();
+        emptyPostObject.put("message", "");
+        emptyPostObject.put("author_uid", 12345);
+        emptyPostObject.put("timestamp", 1388172642);
+    }
+
+    private void setLongPostObject() {
+        longPostObject = new JSONObject();
+        longPostObject.put("message", "One day I will live at the White House or maybe just a quite large house that is all white on the outside; as long as it has a nice big yard and pool.");
+        longPostObject.put("author_uid", 12345);
+        longPostObject.put("timestamp", 1388172642);
+    }
+}
