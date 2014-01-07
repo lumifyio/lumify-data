39ff80070151c69db98bd511b3cf254c68edbd7b
David Singley <dsingley@nearinfinity.com>
2014-01-07 10:22:28 -0600

    backup may work with sufficient free disk space

--- deployment/backup.sh
+++ deployment/backup.sh
@@ -6,10 +6,12 @@ HOSTS_FILE=$1
 PREFIX=$(echo $(basename ${HOSTS_FILE}) | sed -e 's/_hosts//')
 DATE_TIME=$(date +'%Y%m%dT%H%M%z')
 
+HDFS_DIRS='/accumulo /lumify'
 ZK_BASEDIR='/var/lib/zookeeper'
 ES_BASEDIR='/var/lib/elasticsearch/data'
 
-HDFS_PREFIX="/backup/${DATE_TIME}"
+HDFS_BACKUP_PREFIX="/backup/${DATE_TIME}"
+HDFS_RESTORE_PREFIX="/restore"
 
 S3_ROOT="s3://${AWS_ACCESS_KEY}:${AWS_SECRET_KEY}@${S3_BUCKET}"
 S3_BASEDIR="${S3_ROOT}/${PREFIX}/${DATE_TIME}"
@@ -45,41 +47,107 @@ function _zk_servers {
   awk '/zk[0-9]+/ {print $1}' ${HOSTS_FILE}
 }
 
+function _hadoop_fs {
+  local host=$1; shift
+  local cmd="hadoop fs $*"
+
+  echo ${cmd}
+  ssh ${SSH_OPTS} ${host} ${cmd}
+}
+
+function _hdfs_mkdir {
+  local host=$1
+  local dir=$2
+
+  _hadoop_fs ${host} -mkdir -p ${dir}
+}
+
+function _hdfs_put {
+  local host=$1
+  local src=$2
+  local dst=$3
+
+  _hadoop_fs ${host} -put ${src} ${dst}
+}
+
+function _hdfs_get {
+  local host=$1
+  local src=$2
+  local dst=$3
+
+  _hadoop_fs ${host} -get ${src} ${dst}
+}
+
 function _zookeeper_backup {
+  local dst
+
   for zk in $(_zk_servers); do
-    echo "${zk}: hdfs put ${ZK_BASEDIR} -> ${HDFS_PREFIX}/${zk}/zookeeper"
+    dst="${HDFS_BACKUP_PREFIX}/${zk}/zookeeper"
+
+    echo -n "${zk}: "
+    _hdfs_mkdir ${zk} ${dst}
+    echo -n "${zk}: "
+    _hdfs_put ${zk} ${ZK_BASEDIR}/* ${dst}
   done
 }
 
 function _zookeeper_restore {
   for zk in $(_zk_servers); do
-    echo "${zk}: hdfs get /restored/${zk}/zookeeper -> ${ZK_BASEDIR}"
+    echo -n "${zk}: "
+    echo _hdfs_get ${zk} ${HDFS_RESTORE_PREFIX}/${zk}/zookeeper/* ${ZK_BASEDIR}
   done
 }
 
 function _elasticsearch_backup {
+  local dst
+
   for node in $(_nodes); do
-    echo "${node}: hdfs put ${ES_BASEDIR} -> ${HDFS_PREFIX}/${node}/elasticsearch"
+    dst="${HDFS_BACKUP_PREFIX}/${node}/elasticsearch"
+
+    echo -n "${node}: "
+    _hdfs_mkdir ${node} ${dst}
+    echo -n "${node}: "
+    _hdfs_put ${node} ${ES_BASEDIR} ${dst}
   done
 }
 
 function _elasticsearch_restore {
   for node in $(_nodes); do
-    echo "${node}: hdfs get /restored/${node}/elasticsearch -> ${ES_BASEDIR}"
+    echo -n "${node}: "
+    echo _hdfs_get ${node} ${HDFS_RESTORE_PREFIX}/${node}/elasticsearch ${ES_BASEDIR}
   done
 }
 
+function _distcp {
+  local src=$1
+  local dst=$2
+  local cmd="hadoop distcp ${src} ${dst}"
+
+  echo ${cmd}
+  ssh ${SSH_OPTS} $(_first_node) ${cmd}
+}
+
 function _hdfs_backup {
-  echo "distcp ${HDFS_PREFIX} -> ${S3_BASEDIR}"
+  _hadoop_fs $(_first_node) -du -s -h ${HDFS_BACKUP_PREFIX}
+  _distcp ${HDFS_BACKUP_PREFIX} ${S3_BASEDIR}
+
+  for dir in ${HDFS_DIRS}; do
+    _hadoop_fs $(_first_node) -du -s -h ${dir}
+    _distcp ${dir} ${S3_BASEDIR}/hdfs${dir}
+  done
 }
 
 function _hdfs_restore {
-  echo "distcp ${RESTORE_SOURCE} -> /restored"
-  echo "hdfs mv /restored -> *"
+  _distcp ${S3_ROOT}${S3_RESTORE_DIR} ${HDFS_RESTORE_PREFIX}
+
+  for dir in ${HDFS_DIRS}; do
+    echo "hdfs mv ${HDFS_RESTORE_PREFIX}/hdfs${dir} -> ${dir}"
+  done
 }
 
 function _backup_list {
-  ssh  ${SSH_OPTS} $(_first_node) hadoop fs -fs ${S3_ROOT} -ls /${PREFIX} | awk "/${PREFIX}/ && ! /_distcp/ {print \$6}"
+  _hadoop_fs $(_first_node) hadoop fs -fs ${S3_ROOT} -ls /${PREFIX} \
+                            | awk "/${PREFIX}/ && ! /_distcp/ {print \$6}"
 }
 
 function _backup_matching_date {
@@ -106,6 +174,7 @@ function _most_recent_backup {
 
 case $(basename $0 .sh) in
   backup)
+    _hadoop_fs $(_first_node) -rm -f -r ${HDFS_BACKUP_PREFIX}
     _zookeeper_backup
     _elasticsearch_backup
     _hdfs_backup
@@ -114,11 +183,12 @@ case $(basename $0 .sh) in
     set +u
     if [ "$2" ]; then
       set -u
-      RESTORE_SOURCE=$(_backup_matching_date $2)
-      set -u
+      S3_RESTORE_DIR=$(_backup_matching_date $2)
     else
-      RESTORE_SOURCE=$(_most_recent_backup)
+      set -u
+      S3_RESTORE_DIR=$(_most_recent_backup)
     fi
+    _hadoop_fs $(_first_node) -rm -f -r ${HDFS_RESTORE_PREFIX}
     _hdfs_restore
     _zookeeper_restore
     _elasticsearch_restore
