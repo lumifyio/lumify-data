700c172e17aa590db41a66bfbd902bc2bf164b2f
Rebecca Matous <rmatous@nearinfinity.com>
2014-01-08 11:57:36 -0500

    Facebook file processing

--- lumify-facebook/src/main/java/com/altamiracorp/lumify/facebook/StormRunner.java
+++ lumify-facebook/src/main/java/com/altamiracorp/lumify/facebook/StormRunner.java
@@ -3,6 +3,8 @@ package com.altamiracorp.lumify.facebook;
 import backtype.storm.Config;
 import backtype.storm.generated.StormTopology;
 import backtype.storm.topology.TopologyBuilder;
+import com.altamiracorp.lumify.storm.BaseFileSystemSpout;
+import com.altamiracorp.lumify.storm.HdfsFileSystemSpout;
 import com.altamiracorp.lumify.storm.StormRunnerBase;
 import org.apache.commons.cli.CommandLine;
 
@@ -11,8 +13,14 @@ public class StormRunner extends StormRunnerBase {
     private Boolean startFileSpout = false;
     private Boolean startStreamingSpout = false;
     private static final String FACEBOOK_FILE_DIR = "facebook.fileProcessDirectory";
-    private static final String FACEBOOK_STREAM_PERMISSIONS = "facebook.permissions";
+    private static final String FACEBOOK_STREAM_TABLES = "facebook.tables";
+    private static final String HDFS_FACEBOOK_SUBDIR_PROPERTY = "facebook.hdfs.queryPath";
+    private static final String HDFS_ROOT_PATH_PROPERTY = "facebook.hdfs.dataRoot";
+    private static final String DEFAULT_HDFS_FACEBOOK_SUBDIR = "rawFacebook";
+    private static final String DEFAULT_HDFS_DATA_ROOT = "/lumify/data";
+    private String hdfsDataRoot;
     private String hdfsFacebookSubdir;
+
 
     public static void main(String[] args) throws Exception {
         int res = new StormRunner().run(args);
@@ -31,21 +39,28 @@ public class StormRunner extends StormRunnerBase {
     @Override
     protected void beforeCreateTopology(CommandLine cmd, Config conf) throws Exception {
         super.beforeCreateTopology(cmd, conf);
-        if (conf.get(FACEBOOK_FILE_DIR) != null) {
-            hdfsFacebookSubdir = (String) conf.get(FACEBOOK_FILE_DIR);
-            startFileSpout = true;
-        }
-        if (conf.get(FACEBOOK_STREAM_PERMISSIONS) != null) {
+        if (conf.get(FACEBOOK_STREAM_TABLES) != null) {
             startStreamingSpout = true;
         }
+        if (conf.get(FACEBOOK_FILE_DIR) != null) {
+            startFileSpout = true;
+            String rootProp = (String) conf.get(HDFS_ROOT_PATH_PROPERTY);
+            String subDirProp = (String) conf.get(HDFS_FACEBOOK_SUBDIR_PROPERTY);
+            hdfsDataRoot = rootProp != null ? rootProp.trim() : DEFAULT_HDFS_DATA_ROOT;
+            hdfsFacebookSubdir = (subDirProp != null && !subDirProp.trim().isEmpty()) ? subDirProp.trim() : DEFAULT_HDFS_FACEBOOK_SUBDIR;
+            if (!hdfsFacebookSubdir.startsWith("/")) {
+                hdfsFacebookSubdir = "/" + hdfsFacebookSubdir;
+            }
+            conf.put(BaseFileSystemSpout.DATADIR_CONFIG_NAME, hdfsDataRoot);
+        }
         if (!(startFileSpout || startStreamingSpout)) {
-            throw new IllegalStateException("Must have at least one Facebook spout");
+            throw new IllegalStateException("Must have at least one Facebook spout when running Facebook Storm Topology");
         }
     }
 
     public StormTopology createTopology(int parallelismHint) {
         TopologyBuilder builder = new TopologyBuilder();
-        if (startFileSpout) {
+         if (startFileSpout) {
             createFacebookFileTopology(builder, parallelismHint);
         }
         if (startStreamingSpout) {
@@ -55,16 +70,18 @@ public class StormRunner extends StormRunnerBase {
     }
 
     private void createFacebookFileTopology(TopologyBuilder builder, int parallelismHint) {
-//        String spoutName = "facebookFileProcessingSpout";
-//        builder.setSpout(spoutName, new FacebookFileProcessingSpout(hdfsFacebookSubdir), 1);
-//        builder.setBolt(spoutName + "-bolt", new FacebookBolt(), parallelismHint)
-//                .shuffleGrouping(spoutName);
+        String spoutName = "facebookFileProcessingSpout";
+        builder.setSpout(spoutName, new HdfsFileSystemSpout(hdfsFacebookSubdir), 1);
+        builder.setBolt(spoutName + "-FileProcessingBolt", new FacebookFileProcessingBolt(), parallelismHint)
+                .shuffleGrouping(spoutName);
+        builder.setBolt(spoutName + "-BaseProcessingBolt", new FacebookBolt(), parallelismHint)
+                .shuffleGrouping(spoutName + "-FileProcessingBolt");
     }
 
     private void createFacebookStreamingTopology(TopologyBuilder builder, int parallelismHint) {
         String spoutName = "facebookStreamingSpout";
         builder.setSpout(spoutName, new FacebookStreamingSpout(), 1);
-        builder.setBolt(spoutName + "-bolt", new FacebookBolt(), parallelismHint)
+        builder.setBolt(spoutName + "-BaseProcessingBolt", new FacebookBolt(), parallelismHint)
                 .shuffleGrouping(spoutName);
     }
 }
