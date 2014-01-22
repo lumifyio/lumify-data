aabdea6a0381710dade0b3e77a99a1797c84c044
Gordon Shankman <gordon.shankman@altamiracorp.com>
2014-01-22 11:39:16 -0500

    Finished tests for CsvDocumentMappingTest.

--- lumify-enterprise-storm/src/test/java/com/altamiracorp/lumify/storm/structuredData/mapping/csv/CsvDocumentMappingTest.java
+++ lumify-enterprise-storm/src/test/java/com/altamiracorp/lumify/storm/structuredData/mapping/csv/CsvDocumentMappingTest.java
@@ -10,9 +10,13 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import com.altamiracorp.lumify.core.ingest.term.extraction.TermExtractionResult;
+import com.altamiracorp.lumify.core.ingest.term.extraction.TermMention;
+import com.altamiracorp.lumify.core.ingest.term.extraction.TermRelationship;
+import com.altamiracorp.lumify.util.LineReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringReader;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,6 +39,14 @@ public class CsvDocumentMappingTest {
     private static final String TEST_SUBJECT = "Test Subject";
     private static final int TEST_SKIP_ROWS = 1;
     private static final String TEST_PROCESS_ID = "testProcess";
+    private static final String LIVES_AT = "livesAt";
+    private static final String KNOWS = "knows";
+    private static final String TERM1_ID = "term1";
+    private static final String TERM2_ID = "term2";
+    private static final String TERM3_ID = "term3";
+    private static final int TERM1_IDX = 0;
+    private static final int TERM2_IDX = 1;
+    private static final int TERM3_IDX = 3;
 
     @Mock
     private Reader mappingReader;
@@ -48,8 +60,6 @@ public class CsvDocumentMappingTest {
     private CsvRelationshipMapping rel1;
     @Mock
     private CsvRelationshipMapping rel2;
-    @Mock
-    private CsvRelationshipMapping rel3;
 
     private List<CsvTermColumnMapping> termList;
     private List<CsvRelationshipMapping> relList;
@@ -59,9 +69,22 @@ public class CsvDocumentMappingTest {
     @Before
     public void setup() {
         termList = Arrays.asList(term1, term2, term3);
-        relList = Arrays.asList(rel1, rel2, rel3);
+        relList = Arrays.asList(rel1, rel2);
 
         instance = new CsvDocumentMapping(TEST_SUBJECT, TEST_SKIP_ROWS, termList, relList);
+
+        when(term1.getColumnIndex()).thenReturn(TERM1_IDX);
+        when(term1.getMapId()).thenReturn(TERM1_ID);
+        when(term2.getColumnIndex()).thenReturn(TERM2_IDX);
+        when(term2.getMapId()).thenReturn(TERM2_ID);
+        when(term3.getColumnIndex()).thenReturn(TERM3_IDX);
+        when(term3.getMapId()).thenReturn(TERM3_ID);
+        when(rel1.getLabel()).thenReturn(LIVES_AT);
+        when(rel1.getSourceTermId()).thenReturn(TERM1_ID);
+        when(rel1.getTargetTermId()).thenReturn(TERM2_ID);
+        when(rel2.getLabel()).thenReturn(KNOWS);
+        when(rel2.getSourceTermId()).thenReturn(TERM3_ID);
+        when(rel2.getTargetTermId()).thenReturn(TERM1_ID);
     }
 
     @Test
@@ -132,6 +155,132 @@ public class CsvDocumentMappingTest {
         TermExtractionResult expected = new TermExtractionResult();
         TermExtractionResult actual = instance.mapDocument(mappingReader, TEST_PROCESS_ID);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testMapDocument_NullColumns() throws Exception {
+        LineReader lineReader = mock(LineReader.class);
+        StringReader strReader = mock(StringReader.class);
+        CsvListReader csvReader = mock(CsvListReader.class);
+        TermMention line1term1 = mock(TermMention.class, "l1t1");
+        TermMention line1term2 = mock(TermMention.class, "l1t2");
+        TermMention line1term3 = mock(TermMention.class, "l1t3");
+        String line1 = "line1";
+        String line2 = "line2";
+        List<String> fields1 = Arrays.asList("foo", "bar", "fizz", "buzz");
+        int lineOffset1 = 30;
+        int lineOffset2 = 60;
+        int line1Term1Offset = lineOffset1;
+        int line1Term2Offset = line1Term1Offset + fields1.get(0).length() + 1;
+        int line1Term3Offset = line1Term2Offset + fields1.get(1).length() + fields1.get(2).length() + 2;
+
+        PowerMockito.whenNew(LineReader.class).withArguments(mappingReader).thenReturn(lineReader);
+        PowerMockito.whenNew(StringReader.class).withArguments(anyString()).thenReturn(strReader);
+        PowerMockito.whenNew(CsvListReader.class).withArguments(strReader, CsvPreference.EXCEL_PREFERENCE).thenReturn(csvReader);
+
+        when(lineReader.readLine()).thenReturn(line1, line2);
+        when(lineReader.getOffset()).thenReturn(lineOffset1, lineOffset2);
+        when(csvReader.read()).thenReturn(fields1, null);
+        when(term1.mapTerm(fields1, line1Term1Offset, TEST_PROCESS_ID)).thenReturn(line1term1);
+        when(term2.mapTerm(fields1, line1Term2Offset, TEST_PROCESS_ID)).thenReturn(line1term2);
+        when(term3.mapTerm(fields1, line1Term3Offset, TEST_PROCESS_ID)).thenReturn(line1term3);
+
+        List<TermMention> expectedMentions = Arrays.asList(line1term1, line1term2, line1term3);
+        List<TermRelationship> expectedRelationships = Arrays.asList(
+                new TermRelationship(line1term1, line1term2, LIVES_AT),
+                new TermRelationship(line1term3, line1term1, KNOWS)
+        );
+        TermExtractionResult expected = new TermExtractionResult();
+        expected.addAllTermMentions(expectedMentions);
+        expected.addAllRelationships(expectedRelationships);
+
+        TermExtractionResult result = instance.mapDocument(mappingReader, TEST_PROCESS_ID);
+
+        assertEquals(expected, result);
+        verify(lineReader).skipLines(TEST_SKIP_ROWS);
+        PowerMockito.verifyNew(LineReader.class).withArguments(mappingReader);
+        PowerMockito.verifyNew(StringReader.class).withArguments(line1);
+        PowerMockito.verifyNew(StringReader.class).withArguments(line2);
+        PowerMockito.verifyNew(CsvListReader.class, times(2)).withArguments(strReader, CsvPreference.EXCEL_PREFERENCE);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testMapDocument() throws Exception {
+        LineReader lineReader = mock(LineReader.class);
+        StringReader strReader = mock(StringReader.class);
+        CsvListReader csvReader = mock(CsvListReader.class);
+        TermMention line1term1 = mock(TermMention.class, "l1t1");
+        TermMention line1term2 = mock(TermMention.class, "l1t2");
+        TermMention line1term3 = mock(TermMention.class, "l1t3");
+        TermMention line2term1 = mock(TermMention.class, "l2t1");
+        TermMention line2term3 = mock(TermMention.class, "l2t3");
+        TermMention line3term1 = mock(TermMention.class, "l3t1");
+        TermMention line3term2 = mock(TermMention.class, "l3t2");
+        TermMention line3term3 = mock(TermMention.class, "l3t3");
+        String line1 = "line1";
+        String line2 = "line2";
+        String line3 = "line3";
+        List<String> fields1 = Arrays.asList("foo", "bar", "fizz", "buzz");
+        List<String> fields2 = Arrays.asList("this", "is", "a", "test");
+        List<String> fields3 = Arrays.asList("the", "pen", "is", "blue");
+        int lineOffset1 = 30;
+        int lineOffset2 = 60;
+        int lineOffset3 = 90;
+        int lineOffset4 = 120;
+        int line1Term1Offset = lineOffset1;
+        int line1Term2Offset = line1Term1Offset + fields1.get(0).length() + 1;
+        int line1Term3Offset = line1Term2Offset + fields1.get(1).length() + fields1.get(2).length() + 2;
+        int line2Term1Offset = lineOffset2;
+        int line2Term2Offset = line2Term1Offset + fields2.get(0).length() + 1;
+        int line2Term3Offset = line2Term2Offset + fields2.get(1).length() + fields2.get(2).length() + 2;
+        int line3Term1Offset = lineOffset3;
+        int line3Term2Offset = line3Term1Offset + fields3.get(0).length() + 1;
+        int line3Term3Offset = line3Term2Offset + fields3.get(1).length() + fields3.get(2).length() + 2;
+
+        PowerMockito.whenNew(LineReader.class).withArguments(mappingReader).thenReturn(lineReader);
+        PowerMockito.whenNew(StringReader.class).withArguments(anyString()).thenReturn(strReader);
+        PowerMockito.whenNew(CsvListReader.class).withArguments(strReader, CsvPreference.EXCEL_PREFERENCE).thenReturn(csvReader);
+
+        when(lineReader.readLine()).thenReturn(line1, line2, line3, null);
+        when(lineReader.getOffset()).thenReturn(lineOffset1, lineOffset2, lineOffset3, lineOffset4);
+        when(csvReader.read()).thenReturn(fields1, fields2, fields3);
+        when(term1.mapTerm(fields1, line1Term1Offset, TEST_PROCESS_ID)).thenReturn(line1term1);
+        when(term2.mapTerm(fields1, line1Term2Offset, TEST_PROCESS_ID)).thenReturn(line1term2);
+        when(term3.mapTerm(fields1, line1Term3Offset, TEST_PROCESS_ID)).thenReturn(line1term3);
+        when(term1.mapTerm(fields2, line2Term1Offset, TEST_PROCESS_ID)).thenReturn(line2term1);
+        when(term2.mapTerm(fields2, line2Term2Offset, TEST_PROCESS_ID)).thenReturn(null);
+        when(term3.mapTerm(fields2, line2Term3Offset, TEST_PROCESS_ID)).thenReturn(line2term3);
+        when(term1.mapTerm(fields3, line3Term1Offset, TEST_PROCESS_ID)).thenReturn(line3term1);
+        when(term2.mapTerm(fields3, line3Term2Offset, TEST_PROCESS_ID)).thenReturn(line3term2);
+        when(term3.mapTerm(fields3, line3Term3Offset, TEST_PROCESS_ID)).thenReturn(line3term3);
+
+        List<TermMention> expectedMentions = Arrays.asList(
+                line1term1, line1term2, line1term3,
+                line2term1, line2term3,
+                line3term1, line3term2, line3term3
+        );
+        List<TermRelationship> expectedRelationships = Arrays.asList(
+                new TermRelationship(line1term1, line1term2, LIVES_AT),
+                new TermRelationship(line1term3, line1term1, KNOWS),
+                new TermRelationship(line2term3, line2term1, KNOWS),
+                new TermRelationship(line3term1, line3term2, LIVES_AT),
+                new TermRelationship(line3term3, line3term1, KNOWS)
+        );
+        TermExtractionResult expected = new TermExtractionResult();
+        expected.addAllTermMentions(expectedMentions);
+        expected.addAllRelationships(expectedRelationships);
+
+        TermExtractionResult result = instance.mapDocument(mappingReader, TEST_PROCESS_ID);
+
+        assertEquals(expected, result);
+        verify(lineReader).skipLines(TEST_SKIP_ROWS);
+        PowerMockito.verifyNew(LineReader.class).withArguments(mappingReader);
+        PowerMockito.verifyNew(StringReader.class).withArguments(line1);
+        PowerMockito.verifyNew(StringReader.class).withArguments(line2);
+        PowerMockito.verifyNew(StringReader.class).withArguments(line3);
+        PowerMockito.verifyNew(CsvListReader.class, times(3)).withArguments(strReader, CsvPreference.EXCEL_PREFERENCE);
     }
 
     @SuppressWarnings("unchecked")
