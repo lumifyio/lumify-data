60617bb8345a9e469b34adea6baf3ee8290190b1
Gordon Shankman <gordon.shankman@altamiracorp.com>
2014-01-22 22:38:25 -0500

    Added tests for GeoPointCsvPropertyColumnMapping.  Added GeoCircleCsvPropertyColumnMapping and tests.

--- lumify-enterprise/lumify-enterprise-storm/src/main/java/com/altamiracorp/lumify/storm/structuredData/mapping/csv/GeoCircleCsvPropertyColumnMapping.java
+++ lumify-enterprise/lumify-enterprise-storm/src/main/java/com/altamiracorp/lumify/storm/structuredData/mapping/csv/GeoCircleCsvPropertyColumnMapping.java
@@ -2,27 +2,29 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *//*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 package com.altamiracorp.lumify.storm.structuredData.mapping.csv;
 
 import static com.google.common.base.Preconditions.*;
 
-import com.altamiracorp.securegraph.type.GeoPoint;
+import com.altamiracorp.securegraph.type.GeoCircle;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import java.util.List;
 
 /**
- * This mapping uses two columns from a CSV to create a GeoPoint from
- * double-valued latitude, longitude and optional altitude properties.
- * If this property is required, the getPropertyValue() method will
- * generate an exception if either the latitude or longitude columns
- * are empty or invalid.  If altitude is configured, it also must
- * be a valid value when required.
+ * This mapping uses three columns from a CSV to create a GeoCircle from
+ * double-valued latitude, longitude and radius properties.  If this property
+ * is required, the getPropertyValue() method will generate an exception
+ * if either the latitude, longitude or radius columns are empty or invalid.
  */
-@JsonTypeName("geopoint")
-public class GeoPointCsvPropertyColumnMapping implements CsvPropertyColumnMapping<GeoPoint> {
+@JsonTypeName("geocircle")
+public class GeoCircleCsvPropertyColumnMapping implements CsvPropertyColumnMapping<GeoCircle> {
     /**
      * The name of the property.
      */
@@ -39,9 +41,9 @@ public class GeoPointCsvPropertyColumnMapping implements CsvPropertyColumnMappin
     private final int longitudeColumnIndex;
 
     /**
-     * The altitude value column index.
+     * The radius value column index.
      */
-    private final Integer altitudeColumnIndex;
+    private final int radiusColumnIndex;
 
     /**
      * Is this property required.
@@ -53,23 +55,23 @@ public class GeoPointCsvPropertyColumnMapping implements CsvPropertyColumnMappin
      * @param name the name of the property
      * @param latIdx the index of the latitude column
      * @param longIdx the index of the longitude column
-     * @param altIdx the index of the optional altitude column
+     * @param radIdx the index of the radius column
      * @param reqd true if this property is required
      */
-    public GeoPointCsvPropertyColumnMapping(@JsonProperty("name") final String name,
+    public GeoCircleCsvPropertyColumnMapping(@JsonProperty("name") final String name,
             @JsonProperty("latitudeColumn") final int latIdx,
             @JsonProperty("longitudeColumn") final int longIdx,
-            @JsonProperty(value="altitudeColumn", required=false) final Integer altIdx,
+            @JsonProperty("radiusColumn") final int radIdx,
             @JsonProperty(value="required", required=false) final Boolean reqd) {
         checkNotNull(name, "name must be provided");
         checkArgument(!name.trim().isEmpty(), "name must be provided");
         checkArgument(latIdx >= 0, "latitudeColumn must be >= 0");
         checkArgument(longIdx >= 0, "longitudeColumn must be >= 0");
-        checkArgument(altIdx == null || altIdx >= 0, "altitudeColumn must be >= 0 if provided");
+        checkArgument(radIdx >= 0, "radiusColumn must be >= 0");
         this.name = name.trim();
         this.latitudeColumnIndex = latIdx;
         this.longitudeColumnIndex = longIdx;
-        this.altitudeColumnIndex = altIdx;
+        this.radiusColumnIndex = radIdx;
         this.required = reqd != null ? reqd : CsvPropertyColumnMapping.DEFAULT_REQUIRED;
     }
 
@@ -88,9 +90,9 @@ public class GeoPointCsvPropertyColumnMapping implements CsvPropertyColumnMappin
         return longitudeColumnIndex;
     }
 
-    @JsonProperty("altitudeColumn")
-    public final Integer getAltitudeColumnIndex() {
-        return altitudeColumnIndex;
+    @JsonProperty("radiusColumn")
+    public final int getRadiusColumnIndex() {
+        return radiusColumnIndex;
     }
 
     @Override
@@ -99,22 +101,22 @@ public class GeoPointCsvPropertyColumnMapping implements CsvPropertyColumnMappin
     }
 
     @Override
-    public GeoPoint getPropertyValue(final List<String> fields) {
-        GeoPoint point;
+    public GeoCircle getPropertyValue(final List<String> fields) {
+        GeoCircle circle;
         try {
             double latitude = Double.parseDouble(fields.get(latitudeColumnIndex));
             double longitude = Double.parseDouble(fields.get(longitudeColumnIndex));
-            Double altitude = altitudeColumnIndex != null ? Double.parseDouble(fields.get(altitudeColumnIndex)) : null;
-            point = new GeoPoint(latitude, longitude, altitude);
+            double radius = Double.parseDouble(fields.get(radiusColumnIndex));
+            circle = new GeoCircle(latitude, longitude, radius);
         } catch (NumberFormatException nfe) {
-            point = null;
+            circle = null;
         } catch (NullPointerException npe) {
-            point = null;
+            circle = null;
         }
-        if (required && point == null) {
-            throw new IllegalArgumentException(String.format("%s [lat: %d, long: %d, alt: %d] is a required property.", name,
-                    latitudeColumnIndex, longitudeColumnIndex, altitudeColumnIndex));
+        if (required && circle == null) {
+            throw new IllegalArgumentException(String.format("%s [lat: %d, long: %d, rad: %d] is a required property.", name,
+                    latitudeColumnIndex, longitudeColumnIndex, radiusColumnIndex));
         }
-        return point;
+        return circle;
     }
 }
