6d12b44203f7855968a56ce1090eb7a88612507e
Gordon Shankman <gordon.shankman@altamiracorp.com>
2014-01-24 13:08:22 -0500

    Removed unnecessary MappingProperties class.  Refactored CsvTextExtractorWorker to more generic DocumentMappingTextExtractorWorker and updated tests.

--- lumify-enterprise-storm/src/main/java/com/altamiracorp/lumify/storm/term/extraction/CsvEntityExtractor.java
+++ lumify-enterprise-storm/src/main/java/com/altamiracorp/lumify/storm/term/extraction/CsvEntityExtractor.java
@@ -3,28 +3,19 @@ package com.altamiracorp.lumify.storm.term.extraction;
 import static com.google.common.base.Preconditions.*;
 
 import com.altamiracorp.lumify.core.ingest.term.extraction.TermExtractionResult;
-import com.altamiracorp.lumify.core.ingest.term.extraction.TermMention;
-import com.altamiracorp.lumify.core.ingest.term.extraction.TermRelationship;
 import com.altamiracorp.lumify.core.model.ontology.PropertyName;
 import com.altamiracorp.lumify.core.user.User;
 import com.altamiracorp.lumify.core.util.LumifyLogger;
 import com.altamiracorp.lumify.core.util.LumifyLoggerFactory;
-import com.altamiracorp.lumify.storm.structuredData.MappingProperties;
 import com.altamiracorp.lumify.storm.structuredData.mapping.DocumentMapping;
 import com.altamiracorp.securegraph.Vertex;
-import com.altamiracorp.securegraph.type.GeoPoint;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.StringReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class CsvEntityExtractor {
     private static final LumifyLogger LOGGER = LumifyLoggerFactory.getLogger(CsvEntityExtractor.class);
@@ -70,114 +61,114 @@ public class CsvEntityExtractor {
         }
         return termExtractionResult;
     }
-
-
-    private void processLine(TermExtractionResult termExtractionResult, int offset, List<String> columns, JSONObject mappingJson) throws ParseException {
-        List<TermMention> termMentions = getTermsWithGraphVertices(offset, columns, mappingJson);
-        termExtractionResult.addAllTermMentions(termMentions);
-        termExtractionResult.addAllRelationships(getRelationships(termMentions, mappingJson));
-    }
-
-    private List<TermRelationship> getRelationships(List<TermMention> termMentions, JSONObject mappingJson) {
-        List<TermRelationship> relationships = new ArrayList<TermRelationship>();
-        JSONArray mappingColumnsJson = (JSONArray) mappingJson.get("columns");
-        for (int columnIndex = 0; columnIndex < mappingColumnsJson.length(); columnIndex++) {
-            JSONObject columnMappingJson = mappingColumnsJson.getJSONObject(columnIndex);
-            JSONArray relationshipsJson = columnMappingJson.optJSONArray("relationships");
-            if (relationshipsJson != null) {
-                for (int relationshipIndex = 0; relationshipIndex < relationshipsJson.length(); relationshipIndex++) {
-                    JSONObject relationshipJson = relationshipsJson.getJSONObject(relationshipIndex);
-                    int targetIndex = relationshipJson.getInt("target");
-                    String label = relationshipJson.getString("label");
-                    TermMention sourceTermMention = termMentions.get(columnIndex);
-                    TermMention destTermMention = termMentions.get(targetIndex);
-                    relationships.add(new TermRelationship(sourceTermMention, destTermMention, label));
-                }
-            }
-        }
-        return relationships;
-    }
-
-    private List<TermMention> getTermsWithGraphVertices(int offset, List<String> columns, JSONObject mappingJson) throws ParseException {
-        List<TermMention> termMentions = new ArrayList<TermMention>();
-        JSONArray mappingColumnsJson = (JSONArray) mappingJson.get(MappingProperties.COLUMNS);
-        String process = getClass().getName();
-        for (int i = 0; i < columns.size(); i++) {
-            JSONObject columnMappingJson = mappingColumnsJson.getJSONObject(i);
-            String sign = columns.get(i);
-            sign = sign == null ? "" : sign;
-            String type = columnMappingJson.getString(MappingProperties.TYPE);
-            if (type.equals(MappingProperties.TERM)) {
-                String ontologyClassUri = columnMappingJson.getString(MappingProperties.CONCEPT_LABEL);
-                boolean useExisting = columnMappingJson.optBoolean(MappingProperties.USE_EXISTING, false);
-
-                // Checking to see if any properties were specified
-                Map<String, Object> properties = null;
-                if (columnMappingJson.has(MappingProperties.PROPERTIES)) {
-                    properties = new HashMap<String, Object>();
-                    JSONArray propertiesMappingJson = columnMappingJson.getJSONArray(MappingProperties.PROPERTIES);
-                    for (int propIndex = 0; propIndex < propertiesMappingJson.length(); propIndex++) {
-                        JSONObject propertyMappingJson = propertiesMappingJson.getJSONObject(propIndex);
-                        int target = propertyMappingJson.getInt(MappingProperties.TARGET);
-                        String name = propertyMappingJson.getString(MappingProperties.NAME);
-
-                        JSONObject targetPropertyMappingJson = mappingColumnsJson.getJSONObject(target);
-                        String columnData = columns.get(target);
-                        Object propertyValue = getPropertyValue(targetPropertyMappingJson, columnData);
-                        properties.put(name, propertyValue);
-                    }
-                }
-
-                termMentions.add(new TermMention.Builder()
-                        .start(offset)
-                        .end(offset + sign.length())
-                        .sign(sign)
-                        .ontologyClassUri(ontologyClassUri)
-                        .resolved(true)
-                        .properties(properties)
-                        .useExisting(useExisting)
-                        .process(process)
-                        .build());
-            } else {
-                termMentions.add(null);
-            }
-            offset = offset + sign.length() + 1;
-        }
-        return termMentions;
-    }
-
-    private Object getPropertyValue(JSONObject propertyMappingJson, String columnData) throws ParseException {
-        String dataType = propertyMappingJson.getString(MappingProperties.DATA_TYPE);
-        if (columnData == null) {
-            return dataType.equals(MappingProperties.DATE) ? new Date() : "";
-        }
-        if (dataType.equals(MappingProperties.DATE)) {
-            return getPropertyValueDate(propertyMappingJson, columnData);
-        } else if (dataType.equals(MappingProperties.GEO_LOCATION)) {
-            String[] latlong = columnData.split(",");
-            return new GeoPoint(Float.valueOf(latlong[0]), Float.valueOf(latlong[1]));
-        } else {
-            return columnData;
-        }
-    }
-
-    private Object getPropertyValueDate(JSONObject propertyMappingJson, String columnData) throws ParseException {
-        String format = propertyMappingJson.getString(MappingProperties.FORMAT);
-        SimpleDateFormat sdf;
-        if (format != null) {
-            sdf = dateFormatCache.get(format);
-            if (sdf == null) {
-                sdf = new SimpleDateFormat(format);
-                dateFormatCache.put(format, sdf);
-            }
-        } else {
-            sdf = dateFormatCache.get("<default>");
-            if (sdf == null) {
-                sdf = new SimpleDateFormat();
-                dateFormatCache.put("<default>", sdf);
-            }
-        }
-
-        return sdf.parse(columnData).getTime();
-    }
+//
+//
+//    private void processLine(TermExtractionResult termExtractionResult, int offset, List<String> columns, JSONObject mappingJson) throws ParseException {
+//        List<TermMention> termMentions = getTermsWithGraphVertices(offset, columns, mappingJson);
+//        termExtractionResult.addAllTermMentions(termMentions);
+//        termExtractionResult.addAllRelationships(getRelationships(termMentions, mappingJson));
+//    }
+//
+//    private List<TermRelationship> getRelationships(List<TermMention> termMentions, JSONObject mappingJson) {
+//        List<TermRelationship> relationships = new ArrayList<TermRelationship>();
+//        JSONArray mappingColumnsJson = (JSONArray) mappingJson.get("columns");
+//        for (int columnIndex = 0; columnIndex < mappingColumnsJson.length(); columnIndex++) {
+//            JSONObject columnMappingJson = mappingColumnsJson.getJSONObject(columnIndex);
+//            JSONArray relationshipsJson = columnMappingJson.optJSONArray("relationships");
+//            if (relationshipsJson != null) {
+//                for (int relationshipIndex = 0; relationshipIndex < relationshipsJson.length(); relationshipIndex++) {
+//                    JSONObject relationshipJson = relationshipsJson.getJSONObject(relationshipIndex);
+//                    int targetIndex = relationshipJson.getInt("target");
+//                    String label = relationshipJson.getString("label");
+//                    TermMention sourceTermMention = termMentions.get(columnIndex);
+//                    TermMention destTermMention = termMentions.get(targetIndex);
+//                    relationships.add(new TermRelationship(sourceTermMention, destTermMention, label));
+//                }
+//            }
+//        }
+//        return relationships;
+//    }
+//
+//    private List<TermMention> getTermsWithGraphVertices(int offset, List<String> columns, JSONObject mappingJson) throws ParseException {
+//        List<TermMention> termMentions = new ArrayList<TermMention>();
+//        JSONArray mappingColumnsJson = (JSONArray) mappingJson.get(MappingProperties.COLUMNS);
+//        String process = getClass().getName();
+//        for (int i = 0; i < columns.size(); i++) {
+//            JSONObject columnMappingJson = mappingColumnsJson.getJSONObject(i);
+//            String sign = columns.get(i);
+//            sign = sign == null ? "" : sign;
+//            String type = columnMappingJson.getString(MappingProperties.TYPE);
+//            if (type.equals(MappingProperties.TERM)) {
+//                String ontologyClassUri = columnMappingJson.getString(MappingProperties.CONCEPT_LABEL);
+//                boolean useExisting = columnMappingJson.optBoolean(MappingProperties.USE_EXISTING, false);
+//
+//                // Checking to see if any properties were specified
+//                Map<String, Object> properties = null;
+//                if (columnMappingJson.has(MappingProperties.PROPERTIES)) {
+//                    properties = new HashMap<String, Object>();
+//                    JSONArray propertiesMappingJson = columnMappingJson.getJSONArray(MappingProperties.PROPERTIES);
+//                    for (int propIndex = 0; propIndex < propertiesMappingJson.length(); propIndex++) {
+//                        JSONObject propertyMappingJson = propertiesMappingJson.getJSONObject(propIndex);
+//                        int target = propertyMappingJson.getInt(MappingProperties.TARGET);
+//                        String name = propertyMappingJson.getString(MappingProperties.NAME);
+//
+//                        JSONObject targetPropertyMappingJson = mappingColumnsJson.getJSONObject(target);
+//                        String columnData = columns.get(target);
+//                        Object propertyValue = getPropertyValue(targetPropertyMappingJson, columnData);
+//                        properties.put(name, propertyValue);
+//                    }
+//                }
+//
+//                termMentions.add(new TermMention.Builder()
+//                        .start(offset)
+//                        .end(offset + sign.length())
+//                        .sign(sign)
+//                        .ontologyClassUri(ontologyClassUri)
+//                        .resolved(true)
+//                        .properties(properties)
+//                        .useExisting(useExisting)
+//                        .process(process)
+//                        .build());
+//            } else {
+//                termMentions.add(null);
+//            }
+//            offset = offset + sign.length() + 1;
+//        }
+//        return termMentions;
+//    }
+//
+//    private Object getPropertyValue(JSONObject propertyMappingJson, String columnData) throws ParseException {
+//        String dataType = propertyMappingJson.getString(MappingProperties.DATA_TYPE);
+//        if (columnData == null) {
+//            return dataType.equals(MappingProperties.DATE) ? new Date() : "";
+//        }
+//        if (dataType.equals(MappingProperties.DATE)) {
+//            return getPropertyValueDate(propertyMappingJson, columnData);
+//        } else if (dataType.equals(MappingProperties.GEO_LOCATION)) {
+//            String[] latlong = columnData.split(",");
+//            return new GeoPoint(Float.valueOf(latlong[0]), Float.valueOf(latlong[1]));
+//        } else {
+//            return columnData;
+//        }
+//    }
+//
+//    private Object getPropertyValueDate(JSONObject propertyMappingJson, String columnData) throws ParseException {
+//        String format = propertyMappingJson.getString(MappingProperties.FORMAT);
+//        SimpleDateFormat sdf;
+//        if (format != null) {
+//            sdf = dateFormatCache.get(format);
+//            if (sdf == null) {
+//                sdf = new SimpleDateFormat(format);
+//                dateFormatCache.put(format, sdf);
+//            }
+//        } else {
+//            sdf = dateFormatCache.get("<default>");
+//            if (sdf == null) {
+//                sdf = new SimpleDateFormat();
+//                dateFormatCache.put("<default>", sdf);
+//            }
+//        }
+//
+//        return sdf.parse(columnData).getTime();
+//    }
 }
