4d3cea2529e1c5a4bf76f84b1fd0aa8d3d009181
Susan Feng <susan.feng@altamiracorp.com>
2014-01-30 11:31:40 -0500

    fix twitter

--- lumify-twitter/src/test/java/com/altamiracorp/lumify/twitter/DefaultLumifyTwitterProcessorTest.java
+++ lumify-twitter/src/test/java/com/altamiracorp/lumify/twitter/DefaultLumifyTwitterProcessorTest.java
@@ -24,6 +24,7 @@ import com.altamiracorp.lumify.core.model.ontology.OntologyRepository;
 import com.altamiracorp.lumify.core.model.termMention.TermMentionRepository;
 import com.altamiracorp.lumify.core.model.workQueue.WorkQueueRepository;
 import com.altamiracorp.lumify.core.user.User;
+import com.altamiracorp.lumify.core.user.UserProvider;
 import com.altamiracorp.lumify.core.util.LumifyLogger;
 import com.altamiracorp.securegraph.Graph;
 import com.altamiracorp.securegraph.Vertex;
@@ -88,6 +89,8 @@ public class DefaultLumifyTwitterProcessorTest {
     @Mock
     private AuditRepository auditRepository;
     @Mock
+    private UserProvider userProvider;
+    @Mock
     private User user;
     @Mock
     private OntologyRepository ontologyRepository;
@@ -143,10 +146,11 @@ public class DefaultLumifyTwitterProcessorTest {
     @Before
     public void setup() throws Exception {
         instance = new DefaultLumifyTwitterProcessor();
+        user = userProvider.getSystemUser();
         instance.setOntologyRepository(ontologyRepository);
         instance.setGraph(graph);
         instance.setAuditRepository(auditRepository);
-        instance.setUser(user);
+        instance.setUser(userProvider);
         instance.setTermMentionRepository(termMentionRepository);
         instance.setWorkQueueRepository(workQueueRepository);
         instance.setUrlStreamCreator(urlStreamCreator);
