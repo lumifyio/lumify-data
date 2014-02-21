4c2dd4980e921f34cf517fb06cd452311939290c
Ryan Lanman <rmlanman@gmail.com>
2014-02-21 18:33:43 -0500

    Adding a RabbitMQ module

---
+++ rabbitmq/manifests/init.pp
@@ -0,0 +1,10 @@
+class rabbitmq {
+	
+  $rabbitMQVersion = "3.2.3"
+
+  package { 'rabbitmq' :
+		ensure   => installed,
+		provider => "rpm",
+		source   => "http://www.rabbitmq.com/releases/rabbitmq-server/v${rabbitMQVersion}/rabbitmq-server-${rabbitMQVersion}-1.noarch.rpm",
+  }
+}
