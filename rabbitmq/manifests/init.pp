93c210ca3cd442b172b6d0b0c0ce8b6e115bc70d
Ryan Lanman <rmlanman@gmail.com>
2014-02-24 12:55:56 -0500

    Adding management ui plugin and fixing package declaration

--- rabbitmq/manifests/init.pp
+++ rabbitmq/manifests/init.pp
@@ -2,10 +2,24 @@ class rabbitmq {
   require erlang
 	
   $rabbitMQVersion = "3.2.3"
+  
+  $plugins = ["rabbitmq_management"]
 
-  package { 'rabbitmq' :
+  package { 'rabbitmq-server' :
 		ensure   => installed,
 		provider => "rpm",
 		source   => "http://www.rabbitmq.com/releases/rabbitmq-server/v${rabbitMQVersion}/rabbitmq-server-${rabbitMQVersion}-1.noarch.rpm",
   }
+  
+  define installPlugin {
+    exec { "${title}" :
+      command     => "/usr/sbin/rabbitmq-plugins enable ${title}",
+      user        => "root",
+      group       => "root",      
+      environment => ["HOME=/root"],
+      require     => Package['rabbitmq-server'],
+    }
+  }
+  
+  installPlugin { "${plugins}" : }
 }
