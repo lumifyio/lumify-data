137d0523a53adf28104b03d5b3b9136a2e532371
David Singley <dsingley@nearinfinity.com>
2014-02-26 16:10:19 -0500

    might work...

--- bin/test-run-integration-tests.bat
+++
@@ -1,3 +0,0 @@
-cd lumify-public/lumify-web-war/src/main/webapp
-call grunt deps
-call grunt mochaSelenium:firefox
---
+++ bin/test-run-integration-tests.sh
@@ -0,0 +1,3 @@
+cd lumify-public/lumify-web-war/src/main/webapp
+grunt deps
+grunt mochaSelenium:firefox
