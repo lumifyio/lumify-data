6f9a676a49547f02f3988033cb73b990f1d23735
David Singley <dsingley@nearinfinity.com>
2014-02-28 14:28:48 -0500

    undo npm config if no proxy

--- bin/test/run.sh
+++ bin/test/run.sh
@@ -1,7 +1,13 @@
 cd lumify-public/lumify-web-war/src/main/webapp
+
 if [ "${PROXY_URL}" ]; then
   npm config set registry http://registry.npmjs.org/
   npm config set proxy ${PROXY_URL}
+else
+  npm config delete registry
+  npm config delete proxy
 fi
+
 npm install
+
 grunt test:functional
