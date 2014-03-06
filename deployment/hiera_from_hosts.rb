654f2bedfe81a975931c4fec2e26049338aa0142
David Singley <dsingley@nearinfinity.com>
2014-03-06 11:21:19 -0500

    more support for ruby 2.0

--- deployment/hiera_from_hosts.rb
+++ deployment/hiera_from_hosts.rb
@@ -43,7 +43,7 @@ hiera['zookeeper_port'] = zk_port
 hiera['zookeeper_nodes'] = zk_nodes
 hiera['namenode_ipaddress'] = cluster['namenode'][:ip]
 hiera['namenode_hostname'] = 'namenode'
-hiera['elasticsearch_locations'] = get(cluster, /node\d{2}/, :ip).collect{|ip| "#{ip}:9300"}
+hiera['elasticsearch_locations'] = Array(get(cluster, /node\d{2}/, :ip)).collect{|ip| "#{ip}:9300"}
 kafka_nodes = Hash.new
 cluster.select{|k,v| k.match(/kafka\d/)}.each do |k,v|
   n = k.match(/kafka(\d{2})/).captures[0].to_i
