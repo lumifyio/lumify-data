ff9f5b23d2a93dff285440f8e296261c888add4f
David Singley <dsingley@nearinfinity.com>
2014-03-10 11:50:50 -0400

    we'll just restart the VM instead

--- bin/test/ingest.sh
+++ bin/test/ingest.sh
@@ -2,28 +2,6 @@
 
 clone_dir=$1
 
-for pid_file in $(ls /vagrant/*.pid); do
-  name=$(basename ${pid_file} .pid)
-  echo "checking for running ${name} processes..."
-  pid=$(pgrep ${name} || true)
-  if [ "${pid}" = "$(cat ${pid_file})" ]; then
-    echo "killing ${pid} and children..."
-    pkill -9 -P ${pid} || true
-  fi
-  rm -f ${pid_file}
-done
-
-sudo yum install -y lsof
-for port in 8080 8443; do
-  echo "checking for running processes listening on port ${port}..."
-  pid=$(lsof -i TCP:${port} -s TCP:LISTEN -t || true)
-  if [ "${pid}" ]; then
-    echo "killing:"
-    lsof -i TCP:${port} -s TCP:LISTEN -P
-    kill -9 ${pid} || true
-  fi
-done
-
 cd ${clone_dir} && mvn compile -DskipTests 2>&1 | tee /vagrant/mvn.out
 
 /opt/lumify/format.sh
