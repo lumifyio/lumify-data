0cda3fc55d505519f23e4276f6a56fe508c91e57
David Singley <dsingley@nearinfinity.com>
2014-03-18 13:16:30 -0400

    fix syslog config

---
+++ puppet/modules/env/manifests/cluster/loggly.pp
@@ -0,0 +1,19 @@
+class env::cluster::loggly {
+  $loggly_token = hiera('loggly_token')
+  $loggly_tag = hiera('loggly_tag')
+  $facility = 'local3' # use '*' to send everything
+
+  file { '/etc/rsyslog.d/22-loggly.conf' :
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('env/cluster/22-loggly.conf.erb'),
+    require => Package['rsyslog'],
+    notify  => Exec['restart_rsyslogd'],
+  }
+
+  exec { 'restart_rsyslogd' :
+    command     => '/sbin/service rsyslog restart',
+    refreshonly => true,
+  }
+}
--- puppet/modules/env/manifests/cluster/syslog.pp
+++ puppet/modules/env/manifests/cluster/syslog.pp
@@ -6,24 +6,6 @@ class syslog::fw {
   }
 }
 
-class env::cluster::syslog::loggly {
-  $loggly_token = hiera('loggly_token')
-  $loggly_tag = hiera('loggly_tag')
-  $facility = 'local3' # use '*' to send everything
-
-  exec { 'configure format' :
-    comand => "/bin/cat '$template LogglyFormat,\"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msgid% [${loggly_token}@41058 tag=\\"${loggly_tag}\\"] %msg%\n\"' >> /etc/rsyslog.conf",
-    unless => "/bin/grep -q '$template LogglyFormat' /etc/rsyslog.conf",
-    require => Package['rsyslog'],
-  }
-
-  exec { 'configure forward' :
-    comand => "/bin/cat '${facility}.* @@logs-01.loggly.com:514;LogglyFormat' >> /etc/rsyslog.conf",
-    unless => "/bin/grep -q '@@loggly.com:514' /etc/rsyslog.conf",
-    require => Package['rsyslog'],
-  }
-}
-
 class env::cluster::syslog inherits env::cluster::base {
   $facility = 'local3'
 
@@ -40,17 +22,25 @@ class env::cluster::syslog inherits env::cluster::base {
     comand => "/bin/sed -i -e 's/#$ModLoad imudp/$ModLoad imudp/' -e 's/#$UDPServerRun 514/$UDPServerRun 514/' /etc/rsyslog.conf",
     unless => "/bin/grep -q '^$ModLoad imudp' /etc/rsyslog.conf",
     require => Package['rsyslog'],
+    notify  => Exec['restart_rsyslogd'],
   }
 
   exec { 'set max message size' :
-    comand => "/bin/cat '$MaxMessageSize 64k' >> /etc/rsyslog.conf",
+    comand => "/bin/echo '$MaxMessageSize 64k' >> /etc/rsyslog.conf",
     unless => "/bin/grep -q '$MaxMessageSize 64k' /etc/rsyslog.conf",
     require => Package['rsyslog'],
+    notify  => Exec['restart_rsyslogd'],
   }
 
   exec { 'configure log file' :
-    comand => "/bin/cat '${facility}.* /var/log/${facility}' >> /etc/rsyslog.conf",
+    comand => "/bin/echo '${facility}.* /var/log/${facility}' >> /etc/rsyslog.conf",
     unless => "/bin/grep -q '${facility}.* /var/log/${facility}' /etc/rsyslog.conf",
     require => Package['rsyslog'],
+    notify  => Exec['restart_rsyslogd'],
+  }
+
+  exec { 'restart_rsyslogd' :
+    command     => '/sbin/service rsyslog restart',
+    refreshonly => true,
   }
 }
