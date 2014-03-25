8af561599435d93aa382e01deb1e0024d28ccdf2
Joe Ferner <joe@fernsroth.com>
2014-03-25 13:48:03 -0400

    sphinx working

--- lumify-enterprise/lumify-analytics/src/main/java/com/altamiracorp/lumify/textExtraction/SphinxAudioTextExtractor.java
+++ lumify-enterprise/lumify-analytics/src/main/java/com/altamiracorp/lumify/textExtraction/SphinxAudioTextExtractor.java
@@ -3,14 +3,17 @@ package com.altamiracorp.lumify.textExtraction;
 import com.altamiracorp.lumify.core.ingest.AdditionalArtifactWorkData;
 import com.altamiracorp.lumify.core.ingest.ArtifactExtractedInfo;
 import com.altamiracorp.lumify.core.ingest.video.VideoTranscript;
+import com.altamiracorp.lumify.core.util.LumifyLogger;
+import com.altamiracorp.lumify.core.util.LumifyLoggerFactory;
 import com.altamiracorp.lumify.core.util.ProcessRunner;
 import com.google.inject.Inject;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SphinxAudioTextExtractor {
+    private static final LumifyLogger LOGGER = LumifyLoggerFactory.getLogger(SphinxAudioTextExtractor.class);
     private ProcessRunner processRunner;
 
     public ArtifactExtractedInfo extractFromAudio(InputStream work, AdditionalArtifactWorkData data) throws IOException, InterruptedException {
@@ -21,6 +24,7 @@ public class SphinxAudioTextExtractor {
 
         ArtifactExtractedInfo extractedInfo = new ArtifactExtractedInfo();
         extractedInfo.setVideoTranscript(transcript);
+        extractedInfo.setVideoDuration(transcript.getDuration());
         return extractedInfo;
     }
 
@@ -42,19 +46,58 @@ public class SphinxAudioTextExtractor {
                     data.getFileName() + ": "
             );
 
-            processRunner.execute(
-                    "ffmpeg",
-                    new String[]{
-                            "-infile", wavFile.getAbsolutePath(),
-                    },
-                    null,
-                    data.getFileName() + ": "
-            );
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            try {
+                processRunner.execute(
+                        "pocketsphinx_continuous",
+                        new String[]{
+                                "-infile", wavFile.getAbsolutePath(),
+                                "-time", "true"
+                        },
+                        out,
+                        data.getFileName() + ": "
+                );
+            } finally {
+                out.close();
+            }
+
+            return parseSphinxOutput(new String(out.toByteArray()));
         } finally {
             wavFile.delete();
         }
+    }
 
-        return null;
+    private VideoTranscript parseSphinxOutput(String output) throws IOException {
+        VideoTranscript transcript = new VideoTranscript();
+        BufferedReader reader = new BufferedReader(new StringReader(output));
+
+        Pattern wordPattern = Pattern.compile("^([^\\s]+) ([0-9\\.]+) ([0-9\\.]+) ([0-9\\.]+)$");
+        String line;
+        StringBuilder sentence = new StringBuilder();
+        double sentenceStartTime = 0.0;
+        while ((line = reader.readLine()) != null) {
+            Matcher m = wordPattern.matcher(line);
+            if (m.matches()) {
+                String word = m.group(1);
+                double startTime = Double.parseDouble(m.group(2));
+                double endTime = Double.parseDouble(m.group(2));
+                double duration = Double.parseDouble(m.group(2));
+                if ("<s>".equals(word)) {
+                    sentence = new StringBuilder();
+                    sentenceStartTime = startTime;
+                } else if ("</s>".equals(word)) {
+                    long s = (long) (sentenceStartTime * 1000);
+                    long e = (long) (endTime * 1000);
+                    transcript.add(new VideoTranscript.Time(s, e), sentence.toString().trim());
+                    sentence = new StringBuilder();
+                } else {
+                    sentence.append(word);
+                    sentence.append(' ');
+                }
+            }
+        }
+
+        return transcript;
     }
 
     @Inject
