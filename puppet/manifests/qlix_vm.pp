80b4bc752403cfcc691510db193fafe48907398e
David Singley <dsingley@nearinfinity.com>
2014-03-27 13:40:47 -0400

    configure mod_ssl for Apache httpd

--- puppet/manifests/qlix_vm.pp
+++ puppet/manifests/qlix_vm.pp
@@ -17,3 +17,19 @@ mysql::db { 'lumify' :
   grant    => ['ALL'],
   require  => Class[mysql::server],
 }
+
+file { '/opt/lumify/config/lumify.cert.pem' :
+  ensure => file,
+  source => 'file:///vagrant/config/ssl/lumify.cert.pem',
+  owner   => 'root',
+  group   => 'root',
+  mode    => 'u=rw,go=r',
+}
+
+file { '/opt/lumify/config/lumify.key.pem' :
+  ensure => file,
+  source => 'file:///vagrant/config/ssl/lumify.key.pem',
+  owner   => 'root',
+  group   => 'root',
+  mode    => 'u=rw,go=r',
+}
