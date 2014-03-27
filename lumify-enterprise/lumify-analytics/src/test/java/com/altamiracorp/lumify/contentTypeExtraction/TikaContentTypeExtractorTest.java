78049fd1e4690375a51805f944cd9cdebc5ad50b
Susan Feng <susan.feng@altamiracorp.com>
2014-03-26 22:08:03 -0400

    fix tika content type extractor test

--- lumify-enterprise/lumify-analytics/src/test/java/com/altamiracorp/lumify/contentTypeExtraction/TikaContentTypeExtractorTest.java
+++ lumify-enterprise/lumify-analytics/src/test/java/com/altamiracorp/lumify/contentTypeExtraction/TikaContentTypeExtractorTest.java
@@ -13,11 +13,11 @@ public class TikaContentTypeExtractorTest {
     public void textExtract() throws Exception {
         TikaContentTypeExtractor contentTypeExtractor = new TikaContentTypeExtractor();
         assertEquals("video/x-m4v", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/H_264.m4v"), "m4v"));
-        assertEquals("video/mpeg", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/MPEG-2.m2v"), "m2v"));
+        assertEquals("video", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/MPEG-2.m2v"), "m2v"));
         assertEquals("video/mp4", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/MPEG-4.mp4"), "mp4"));
         assertEquals("text/plain", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/hello.txt"), "txt"));
         assertEquals("application/pdf", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/hello.pdf"), "pdf"));
         assertEquals("image/jpeg", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/cat.jpg"), "jpg"));
-        assertEquals("", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/test.abc"), "abc"));
+        assertEquals("document", contentTypeExtractor.extract(TikaContentTypeExtractor.class.getResourceAsStream("/test.abc"), "abc"));
     }
 }
