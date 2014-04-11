50805d03da66f9b4e1ac8c84ecb0c0fe92fa32ae
Joe Ferner <joe@fernsroth.com>
2014-04-11 10:40:43 -0400

    add placeholder test

---
+++ lumify-enterprise/lumify-enterprise-storm/src/test/java/com/altamiracorp/lumify/PlaceholderTest.java
@@ -0,0 +1,13 @@
+package com.altamiracorp.lumify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PlaceholderTest {
+    @Test
+    public void testPlaceholder() {
+
+    }
+}
