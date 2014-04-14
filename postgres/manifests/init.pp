f344765d210c441706e793a1e37139518541a1ed
Ryan Lanman <rmlanman@gmail.com>
2014-04-14 12:29:59 -0400

    postgres init and service start

--- postgres/manifests/init.pp
+++ postgres/manifests/init.pp
@@ -1,5 +1,8 @@
 class postgres {
-  package { 'postgres-repo':
+  
+  $serviceName = "postgresql-9.3"
+    
+  package { 'pgdg-centos93-9.3-1':
     source   => 'http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm',
     provider => 'rpm',
     ensure   => present,
@@ -7,17 +10,25 @@ class postgres {
   
   package { 'postgresql93-server':
     ensure  => present,
-    require => Package['postgres-repo'],
+    require => Package['pgdg-centos93-9.3-1'],
   }
   
   package { 'postgresql93':
     ensure  => present,
-    require => Package['postgres-repo'],
+    require => Package['pgdg-centos93-9.3-1'],
   }
   
-  service { 'postgresql-9.3':
+  exec { 'initdb':
+    command => "/sbin/service ${serviceName} initdb",
+    onlyif  => "/usr/bin/test ! -d /var/lib/pgsql/9.3/data",
+    user    => "root",
+    group   => "root",
+    require => Package['postgresql93']
+  }
+  
+  service { $serviceName:
     ensure  => running,
     enable  => true,
-    require => Package['postgresql93'],
+    require => Exec['initdb'],
   }
 }
