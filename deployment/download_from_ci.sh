17ad7df9e40f3ae62eb2fec4a2c2cea6a6d3f902
David Singley <dsingley@nearinfinity.com>
2014-04-17 09:24:04 -0400

    download all our storm libs

--- deployment/download_from_ci.sh
+++ deployment/download_from_ci.sh
@@ -19,6 +19,12 @@ function _download {
   fi
 }
 
+function _download_enterprise_uber_jar {
+  local module_name=$1
+
+  _download lumify/${module_name} "*with-dependencies.jar" ${module_name}.jar
+}
+
 function _md5 {
   local file=$1
 
@@ -29,20 +35,44 @@ function _md5 {
   fi
 }
 
-_download lumify/lumify-account-web       "*.war"                  account.war
-_download lumify/lumify-enterprise-storm  "*with-dependencies.jar" lumify-enterprise-storm.jar
-_download lumify/lumify-enterprise-tools  "*with-dependencies.jar" lumify-enterprise-tools.jar
-_download lumify/lumify-facebook          "*with-dependencies.jar" lumify-facebook.jar
+
+_download_enterprise_uber_jar lumify-ccextractor
+_download_enterprise_uber_jar lumify-clavin
+_download_enterprise_uber_jar lumify-email-extractor
+_download_enterprise_uber_jar lumify-enterprise-storm
+_download_enterprise_uber_jar lumify-enterprise-tools
+_download_enterprise_uber_jar lumify-enterprise.iml
+_download_enterprise_uber_jar lumify-java-code-ingest
+_download_enterprise_uber_jar lumify-jetty-server-enterprise
+_download_enterprise_uber_jar lumify-known-entity-extractor
+_download_enterprise_uber_jar lumify-mapped-ingest
+_download_enterprise_uber_jar lumify-opencv-object-detector
+_download_enterprise_uber_jar lumify-opennlp-dictionary-extractor
+_download_enterprise_uber_jar lumify-opennlp-dictionary-extractor-web
+_download_enterprise_uber_jar lumify-opennlp-me-extractor
+_download_enterprise_uber_jar lumify-phone-number-extractor
+_download_enterprise_uber_jar lumify-sphinx
+_download_enterprise_uber_jar lumify-subrip-parser
+_download_enterprise_uber_jar lumify-subrip-transcript
+_download_enterprise_uber_jar lumify-tesseract
+_download_enterprise_uber_jar lumify-tika-mime-type
+_download_enterprise_uber_jar lumify-tika-text-extractor
+_download_enterprise_uber_jar lumify-youtube-transcript
+_download_enterprise_uber_jar lumify-zipcode-extractor
+
+_download lumify/lumify-import            "*with-dependencies.jar" lumify-import.jar
 _download lumify/lumify-storm             "*with-dependencies.jar" lumify-storm.jar
-_download lumify/lumify-twitter           "*with-dependencies.jar" lumify-twitter.jar
+
+_download lumify/lumify-wikipedia-mr      "*with-dependencies.jar" lumify-wikipedia-mr.jar
 _download lumify/lumify-version-inspector "*with-dependencies.jar" lumify-version.jar
 _download lumify/lumify-web-war           "*.war"                  lumify.war
-_download lumify/lumify-wikipedia-mr      "*with-dependencies.jar" lumify-wikipedia-mr.jar
-_download lumify/lumify-wikipedia-storm   "*with-dependencies.jar" lumify-wikipedia-storm.jar
-_download lumify/lumify-import            "*with-dependencies.jar" lumify-import.jar
-_download bigtable/bigtable-ui-war        "*.war"                  bigtable-ui.war
-_download jmxui/jmx-ui-webapp             "*.war"                  jmx-ui.war
-_download securegraph/securegraph-tools   "*with-dependencies.jar" securegraph-tools.jar
+
+#_download lumify/lumify-twitter           "*with-dependencies.jar" lumify-twitter.jar
+#_download lumify/lumify-facebook          "*with-dependencies.jar" lumify-facebook.jar
+#_download securegraph/securegraph-tools   "*with-dependencies.jar" securegraph-tools.jar
+#_download lumify/lumify-account-web       "*.war"                  account.war
+#_download bigtable/bigtable-ui-war        "*.war"                  bigtable-ui.war
+#_download jmxui/jmx-ui-webapp             "*.war"                  jmx-ui.war
 
 for file in ${FILE_LIST}; do
   _md5 ${file}
