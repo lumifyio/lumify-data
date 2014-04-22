bebd31d0b93b0659818cc5e5ee2345f33ab1502b
Joe Ferner <joe@fernsroth.com>
2014-04-22 09:37:53 -0400

    remove facebook and twitter

--- lumify-facebook/src/main/java/com/altamiracorp/lumify/facebook/FieldNames.java
+++
@@ -1,5 +0,0 @@
-package com.altamiracorp.lumify.facebook;
-
-public class FieldNames {
-    public static final String FILE_NAME = "fileName";
-}
--- lumify-twitter/src/main/java/com/altamiracorp/lumify/twitter/FieldNames.java
+++
@@ -1,5 +0,0 @@
-package com.altamiracorp.lumify.twitter;
-
-public class FieldNames {
-    public static final String FILE_NAME = "fileName";
-}
