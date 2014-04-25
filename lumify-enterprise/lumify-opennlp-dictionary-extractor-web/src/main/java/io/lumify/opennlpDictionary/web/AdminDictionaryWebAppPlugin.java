e19587d8642545bf39c26325ca43d0af25b3882b
Joe Ferner <joe@fernsroth.com>
2014-04-25 12:16:12 -0400

    add role filters to router

--- lumify-enterprise/lumify-opennlp-dictionary-extractor-web/src/main/java/io/lumify/opennlpDictionary/web/AdminDictionaryWebAppPlugin.java
+++ lumify-enterprise/lumify-opennlp-dictionary-extractor-web/src/main/java/io/lumify/opennlpDictionary/web/AdminDictionaryWebAppPlugin.java
@@ -1,10 +1,11 @@
 package io.lumify.opennlpDictionary.web;
 
+import com.altamiracorp.miniweb.Handler;
+import com.altamiracorp.miniweb.StaticResourceHandler;
 import io.lumify.web.AuthenticationProvider;
 import io.lumify.web.WebApp;
 import io.lumify.web.WebAppPlugin;
-import com.altamiracorp.miniweb.Handler;
-import com.altamiracorp.miniweb.StaticResourceHandler;
+import io.lumify.web.roleFilters.AdminRoleFilter;
 
 import javax.servlet.ServletConfig;
 
@@ -12,9 +13,9 @@ public class AdminDictionaryWebAppPlugin implements WebAppPlugin {
     @Override
     public void init(WebApp app, ServletConfig config, Class<? extends Handler> authenticator, AuthenticationProvider authenticatorInstance) {
         app.get("/admin/dictionaryAdmin.html", authenticatorInstance, new StaticResourceHandler(getClass(), "/dictionaryAdmin.html", "text/html"));
-        app.get("/admin/dictionary", authenticator, AdminDictionary.class);
-        app.get("/admin/dictionary/concept", authenticator, AdminDictionaryByConcept.class);
-        app.post("/admin/dictionary", authenticator, AdminDictionaryEntryAdd.class);
-        app.post("/admin/dictionary/delete", authenticator, AdminDictionaryEntryDelete.class);
+        app.get("/admin/dictionary", authenticator, AdminRoleFilter.class, AdminDictionary.class);
+        app.get("/admin/dictionary/concept", authenticator, AdminRoleFilter.class, AdminDictionaryByConcept.class);
+        app.post("/admin/dictionary", authenticator, AdminRoleFilter.class, AdminDictionaryEntryAdd.class);
+        app.post("/admin/dictionary/delete", authenticator, AdminRoleFilter.class, AdminDictionaryEntryDelete.class);
     }
 }
