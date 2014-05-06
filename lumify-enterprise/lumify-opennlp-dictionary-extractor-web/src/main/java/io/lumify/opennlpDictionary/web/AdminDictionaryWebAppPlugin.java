2b1dd71cca8f29532594ad2a90750db6c8f60461
Jeff Kunkle <jeff.kunkle@altamiracorp.com>
2014-05-06 11:44:33 -0400

    More progress

--- lumify-enterprise/lumify-opennlp-dictionary-extractor-web/src/main/java/io/lumify/opennlpDictionary/web/AdminDictionaryWebAppPlugin.java
+++ lumify-enterprise/lumify-opennlp-dictionary-extractor-web/src/main/java/io/lumify/opennlpDictionary/web/AdminDictionaryWebAppPlugin.java
@@ -2,7 +2,7 @@ package io.lumify.opennlpDictionary.web;
 
 import com.altamiracorp.miniweb.Handler;
 import com.altamiracorp.miniweb.StaticResourceHandler;
-import io.lumify.web.AuthenticationProvider;
+import io.lumify.web.AuthenticationHandler;
 import io.lumify.web.WebApp;
 import io.lumify.web.WebAppPlugin;
 import io.lumify.web.privilegeFilters.AdminPrivilegeFilter;
@@ -11,8 +11,8 @@ import javax.servlet.ServletConfig;
 
 public class AdminDictionaryWebAppPlugin implements WebAppPlugin {
     @Override
-    public void init(WebApp app, ServletConfig config, Class<? extends Handler> authenticator, AuthenticationProvider authenticatorInstance) {
-        app.get("/admin/dictionaryAdmin.html", authenticatorInstance, new StaticResourceHandler(getClass(), "/dictionaryAdmin.html", "text/html"));
+    public void init(WebApp app, ServletConfig config, Class<? extends Handler> authenticator, AuthenticationHandler authenticationHandler) {
+        app.get("/admin/dictionaryAdmin.html", authenticationHandler, new StaticResourceHandler(getClass(), "/dictionaryAdmin.html", "text/html"));
         app.get("/admin/dictionary", authenticator, AdminPrivilegeFilter.class, AdminDictionary.class);
         app.get("/admin/dictionary/concept", authenticator, AdminPrivilegeFilter.class, AdminDictionaryByConcept.class);
         app.post("/admin/dictionary", authenticator, AdminPrivilegeFilter.class, AdminDictionaryEntryAdd.class);
