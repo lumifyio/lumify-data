85440f892a94985f593983369c15d714bed88668
Joe Ferner <joe@fernsroth.com>
2014-05-06 11:23:25 -0400

    update ontology

--- lumify-twitter/lumify-twitter-storm/src/main/java/io/lumify/twitter/TwitterOntology.java
+++ lumify-twitter/lumify-twitter-storm/src/main/java/io/lumify/twitter/TwitterOntology.java
@@ -4,9 +4,14 @@ import io.lumify.core.model.properties.types.TextLumifyProperty;
 
 public class TwitterOntology {
     public static final String EDGE_LABEL_TWEETED = "http://lumify.io/twitter#tweeted";
+    public static final String EDGE_LABEL_MENTIONED = "http://lumify.io/twitter#mentioned";
+    public static final String EDGE_LABEL_REFERENCED_URL = "http://lumify.io/twitter#refUrl";
+    public static final String EDGE_LABEL_TAGGED = "http://lumify.io/twitter#tagged";
 
-    public static final String USER_CONCEPT_TYPE = "http://lumify.io/twitter#user";
-    public static final String TWEET_CONCEPT_TYPE = "http://lumify.io/twitter#tweet";
+    public static final String CONCEPT_TYPE_USER = "http://lumify.io/twitter#user";
+    public static final String CONCEPT_TYPE_TWEET = "http://lumify.io/twitter#tweet";
+    public static final String CONCEPT_TYPE_HASHTAG = "http://lumify.io/twitter#hashtag";
+    public static final String CONCEPT_TYPE_URL = "http://lumify.io/twitter#url";
 
     public static final TextLumifyProperty PROFILE_IMAGE_URL = TextLumifyProperty.all("http://lumify.io/twitter#profileImageUrl");
 }
