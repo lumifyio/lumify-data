20073e4080cce4af95d4fb8dbcd8fb3cddbf35ba
Jeff Kunkle <jeff.kunkle@altamiracorp.com>
2014-05-07 09:48:30 -0400

    Complete the Twitter OAuth signin

--- lumify-enterprise/lumify-opennlp-dictionary-extractor-web/src/main/java/io/lumify/opennlpDictionary/web/AdminDictionaryWebAppPlugin.java
+++ lumify-enterprise/lumify-opennlp-dictionary-extractor-web/src/main/java/io/lumify/opennlpDictionary/web/AdminDictionaryWebAppPlugin.java
@@ -2,7 +2,6 @@ package io.lumify.opennlpDictionary.web;
 
 import com.altamiracorp.miniweb.Handler;
 import com.altamiracorp.miniweb.StaticResourceHandler;
-import io.lumify.web.AuthenticationHandler;
 import io.lumify.web.WebApp;
 import io.lumify.web.WebAppPlugin;
 import io.lumify.web.privilegeFilters.AdminPrivilegeFilter;
@@ -11,11 +10,13 @@ import javax.servlet.ServletConfig;
 
 public class AdminDictionaryWebAppPlugin implements WebAppPlugin {
     @Override
-    public void init(WebApp app, ServletConfig config, Class<? extends Handler> authenticator, AuthenticationHandler authenticationHandler) {
+    public void init(WebApp app, ServletConfig config, Handler authenticationHandler) {
+        Class<? extends Handler> authenticationHandlerClass = authenticationHandler.getClass();
+
         app.get("/admin/dictionaryAdmin.html", authenticationHandler, new StaticResourceHandler(getClass(), "/dictionaryAdmin.html", "text/html"));
-        app.get("/admin/dictionary", authenticator, AdminPrivilegeFilter.class, AdminDictionary.class);
-        app.get("/admin/dictionary/concept", authenticator, AdminPrivilegeFilter.class, AdminDictionaryByConcept.class);
-        app.post("/admin/dictionary", authenticator, AdminPrivilegeFilter.class, AdminDictionaryEntryAdd.class);
-        app.post("/admin/dictionary/delete", authenticator, AdminPrivilegeFilter.class, AdminDictionaryEntryDelete.class);
+        app.get("/admin/dictionary", authenticationHandlerClass, AdminPrivilegeFilter.class, AdminDictionary.class);
+        app.get("/admin/dictionary/concept", authenticationHandlerClass, AdminPrivilegeFilter.class, AdminDictionaryByConcept.class);
+        app.post("/admin/dictionary", authenticationHandlerClass, AdminPrivilegeFilter.class, AdminDictionaryEntryAdd.class);
+        app.post("/admin/dictionary/delete", authenticationHandlerClass, AdminPrivilegeFilter.class, AdminDictionaryEntryDelete.class);
     }
 }
