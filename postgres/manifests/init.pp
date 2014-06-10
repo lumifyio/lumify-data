3b2a910000ecb1e913f0df0fc8f9fc7d06ae3253
Ryan Lanman <rmlanman@gmail.com>
2014-06-10 16:21:15 -0400

    Updating postgres configs to include user and database creation

--- postgres/manifests/init.pp
+++ postgres/manifests/init.pp
@@ -1,6 +1,13 @@
 class postgres {
   
   $serviceName = "postgresql-9.3"
+  $connect_network = hiera("postgres_connect_network")
+  $listen_addresses = hiera_array("postgres_listen_addresses")
+  $listen_port = hiera("postgres_listen_port",5432)
+  $max_connections = hiera("postgres_max_connections",100)
+  $database_name = hiera("postgres_db_name")
+  $default_db_user = hiera("postgres_db_user")
+  $default_db_pw = hiera("postgres_db_pw")
     
   package { 'pgdg-centos93-9.3-1':
     source   => 'http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm',
@@ -20,15 +27,50 @@ class postgres {
   
   exec { 'initdb':
     command => "/sbin/service ${serviceName} initdb",
-    onlyif  => "/usr/bin/test ! -d /var/lib/pgsql/9.3/data",
+    onlyif  => "/usr/bin/test ! -f /var/lib/pgsql/9.3/data/PG_VERSION",
     user    => "root",
     group   => "root",
     require => Package['postgresql93']
+  }
+  
+  file { '/var/lib/pgsql/9.3/data/pg_hba.conf' :
+    ensure  => file,
+    content => template('postgres/pg_hba.conf.erb'),
+    require => Exec['initdb'],
+    before  => Service[$serviceName],
+  }
+  
+  file { '/var/lib/pgsql/9.3/data/postgresql.conf' :
+    ensure  => file,
+    content => template('postgres/postgresql.conf.erb'),
+    require => Exec['initdb'],
+    before  => Service[$serviceName],
   }
   
   service { $serviceName:
     ensure  => running,
     enable  => true,
     require => Exec['initdb'],
+  }
+  
+  exec { "createdb" :
+    command => "/usr/bin/psql -c \"create database ${database_name}\"",
+    unless  => "/usr/bin/psql -c \"select datname from pg_catalog.pg_database where datname = '${database_name}'\" | grep -q ${database_name}",
+    user    => 'postgres',
+    require => Service[$serviceName]
+  }  
+  
+  exec { "createuser" :
+    command => "/usr/bin/psql -c \"create user ${default_db_user} with password '${default_db_pw}'\"",
+    unless  => "/usr/bin/psql -c \"select usename from pg_catalog.pg_user where usename = '${default_db_user}'\" | grep -q ${default_db_user}",
+    user => 'postgres',
+    require => Service[$serviceName]
+  }
+  
+  
+  exec { "grantuser" :
+    command => "/usr/bin/psql -c \"grant all privileges on database ${database_name} to ${default_db_user}\"",
+    user => 'postgres',
+    require => [Exec["createdb"], Exec["createuser"]]
   }
 }
