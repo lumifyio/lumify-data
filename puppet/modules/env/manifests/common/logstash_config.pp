b8b2de1c2fac94855faed7bca3e8bdcba220ab41
Susan Feng <susan.feng@altamiracorp.com>
2014-07-03 16:07:41 -0400

    check for logstash_server

--- puppet/modules/env/manifests/common/logstash_config.pp
+++ puppet/modules/env/manifests/common/logstash_config.pp
@@ -1,8 +1,10 @@
 class env::common::logstash_config {
-  $logstash_server = hiera('logstash_server')
+  $logstash_server = hiera('logstash_server', '')
 
-  file { '/opt/logstash/logstash.config' :
-    ensure  => file,
-    content => template('env/common/logstash.config.erb'),
+  if $logstash_server != '' {
+    file { '/opt/logstash/logstash.config' :
+      ensure  => file,
+      content => template('env/common/logstash.config.erb'),
+    }
   }
 }
