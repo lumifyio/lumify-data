2bd6684a46bcb4d9e76d0c8f00490920c5566ed5
David Singley <dsingley@nearinfinity.com>
2014-07-13 13:17:28 -0400

    improve logging, add index document count/size

--- lumify-enterprise/lumify-elasticsearch-benchmark/src/main/java/io/lumify/elasticsearchBenchmark/ElasticsearchBenchmark.java
+++ lumify-enterprise/lumify-elasticsearch-benchmark/src/main/java/io/lumify/elasticsearchBenchmark/ElasticsearchBenchmark.java
@@ -5,6 +5,9 @@ import com.beust.jcommander.Parameter;
 import org.elasticsearch.action.admin.cluster.stats.ClusterStatsNodes;
 import org.elasticsearch.action.admin.cluster.stats.ClusterStatsRequest;
 import org.elasticsearch.action.admin.cluster.stats.ClusterStatsResponse;
+import org.elasticsearch.action.admin.indices.stats.IndexStats;
+import org.elasticsearch.action.admin.indices.stats.IndicesStatsRequest;
+import org.elasticsearch.action.admin.indices.stats.IndicesStatsResponse;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
@@ -32,44 +35,49 @@ public class ElasticsearchBenchmark {
     private static final String ELEMENT_TYPE = "benchmarkItem";
     private static final Random RANDOM = new Random(1000);
 
-    @Parameter(names = "--count", description = "Number of items to insert")
+    @Parameter(names = "--count", description = "total number of documents to insert")
     private int count = 10000;
 
     private int currentCount = 0;
 
-    @Parameter(names = "--bulkcount", description = "Number of items to insert in bulk batches. Use 1 to not use bulk api.")
+    @Parameter(names = "--bulkcount", description = "number of documents to insert in bulk batches, set to -1 to not use bulk api")
     private int bulkCount = -1;
 
-    @Parameter(names = "--bulksize", description = "Size of the bulk insert.")
+    @Parameter(names = "--bulksize", description = "target size of the bulk insert (bytes), set to -1 to not use bulk api")
     private int bulkSize = -1;
 
-    @Parameter(names = "--hostname", description = "hostname of elasticsearch node.")
+    @Parameter(names = "--hostname", description = "(comma separated) hostname(s) of elasticsearch node(s)")
     private String hostname;
 
-    @Parameter(names = "--clustername", description = "clustername of elasticsearch cluster.")
+    @Parameter(names = "--clustername", description = "name of the elasticsearch cluster")
     private String clusterName;
 
-    @Parameter(names = "--indexname", description = "Name of the index to test with.")
+    @Parameter(names = "--indexname", description = "name of the index to test with")
     private String indexName = "benchmark";
 
-    @Parameter(names = "--port", description = "port of elasticsearch node.")
+    @Parameter(names = "--port", description = "port of the elasticsearch server")
     private int port = 9300;
 
-    @Parameter(names = "--usenodeapi", description = "Use the node API and join the elasticsearch cluster.")
+    @Parameter(names = "--usenodeapi", description = "optionally use the node API and join the elasticsearch cluster")
     private boolean nodeApi = false;
 
-    @Parameter(names = "--help", help = true, description = "Prints help")
+    @Parameter(names = "--help", help = true, description = "display this help")
     private boolean help;
 
-    @Parameter(names = "--storesourcedata", description = "Store data in the _source field.")
+    @Parameter(names = "--storesourcedata", description = "store data in the _source field")
     private boolean storeSourceData = false;
 
-    @Parameter(names = "--documentsize", description = "The max size of the document in bytes.")
-    private int documentSize = 10000;
+    @Parameter(names = "--maxdocumentsize", description = "maximum size of the test documents (bytes)")
+    private int maxDocumentSize = 10000;
+
+    @Parameter(names = "--minimumdocumentqueuedepth", description = "how many documents to have ready to index")
+    private int minimumDocumentQueueDepth = 1000;
+
+    @Parameter(names = "--documentqueuecheckinterval", description = "how long to wait before checking if we need to create more documents (ms)")
+    private int documentQueueCheckInterval = 10;
 
     private ArrayList<String> words;
     private long numberOfBytesInserted;
-
     private final Queue<String> documentTexts = new LinkedList<String>();
 
     public static void main(String[] args) throws Exception {
@@ -116,14 +124,14 @@ public class ElasticsearchBenchmark {
         long startTime = System.currentTimeMillis();
         double rate;
         while (currentCount < count) {
-            if (currentCount >= nextReportingCount) {
+            if (currentCount > 0 && currentCount >= nextReportingCount) {
                 long duration = System.currentTimeMillis() - lastReportingTime;
                 if (duration == 0) {
                     rate = 0.0;
                 } else {
                     rate = ((double) (currentCount - lastReportingCount)) / ((double) duration) * 1000;
                 }
-                LOGGER.debug(String.format("inserting %d/%d (%.2f docs/s)", currentCount, count, rate));
+                LOGGER.debug(String.format("inserted %d/%d (%.2f docs/s)", currentCount, count, rate));
                 lastReportingCount = currentCount;
                 nextReportingCount += 1000;
                 lastReportingTime = System.currentTimeMillis();
@@ -133,7 +141,7 @@ public class ElasticsearchBenchmark {
         long endTime = System.currentTimeMillis();
         rate = ((double) count) / ((double) (endTime - startTime)) * 1000;
         double avgBytesPerDocument = ((double) numberOfBytesInserted) / ((double) count);
-        LOGGER.info("Complete");
+        LOGGER.info("benchmark complete");
         LOGGER.info(String.format("             documents: %,d", count));
         LOGGER.info(String.format("  documents per second: %,.2f", rate));
         LOGGER.info(String.format("avg bytes per document: %,.2f", avgBytesPerDocument));
@@ -144,35 +152,46 @@ public class ElasticsearchBenchmark {
     }
 
     private String formatResults(String[] args, Client client, double rate, double avgBytesPerDocument) throws ExecutionException, InterruptedException {
-        List<Object> settings = new ArrayList<Object>();
-        settings.add(count);
-        settings.add(bulkCount);
-        settings.add(bulkSize);
-        settings.add(nodeApi);
-        settings.add(storeSourceData);
-        settings.add(documentSize);
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("count", count);
+        map.put("bulkCount", bulkCount);
+        map.put("bulkSize", bulkSize);
+        map.put("nodeApi", nodeApi);
+        map.put("storeSourceData", storeSourceData);
+        map.put("maxDocumentSize", maxDocumentSize);
+        map.put("minimumDocumentQueueDepth", minimumDocumentQueueDepth);
+        map.put("documentQueueCheckInterval", documentQueueCheckInterval);
 
-        ClusterStatsRequest req = new ClusterStatsRequest();
-        ClusterStatsResponse clusterStatsResponse = client.admin().cluster().clusterStats(req).get();
-        ClusterStatsNodes.Counts counts = clusterStatsResponse.getNodesStats().getCounts();
+        ClusterStatsResponse clusterStatsResponse = client.admin().cluster().clusterStats(new ClusterStatsRequest()).get();
+        ClusterStatsNodes.Counts nodeCounts = clusterStatsResponse.getNodesStats().getCounts();
 
-        // TODO: add more cluster info
-        List<Object> clusterInfo = new ArrayList<Object>();
-        clusterInfo.add(counts.getMasterOnly());
-        clusterInfo.add(counts.getDataOnly());
-        clusterInfo.add(counts.getMasterData());
-        clusterInfo.add(counts.getClient());
+        IndicesStatsResponse indicesStatsResponse = client.admin().indices().stats(new IndicesStatsRequest()).get();
+        IndexStats indexStats = indicesStatsResponse.getIndex(indexName);
 
-        return String.format("RESULTS %s, %s, \"%.2f\", \"%.2f\", \"%s\"", toCsvString(settings), toCsvString(clusterInfo), rate, avgBytesPerDocument, toArgsString(args));
+        map.put("cluster master-only nodes", nodeCounts.getMasterOnly());
+        map.put("cluster data-only nodes", nodeCounts.getDataOnly());
+        map.put("cluster master/data nodes", nodeCounts.getMasterData());
+        map.put("cluster client nodes", nodeCounts.getClient());
+        map.put("index document count", indexStats.getPrimaries().getDocs().getCount());
+        map.put("index size (bytes)", indexStats.getPrimaries().getStore().getSizeInBytes());
+
+        map.put("index rate", rate);
+        map.put("avg bytes/doc", avgBytesPerDocument);
+
+        map.put("command line args", toArgsString(args));
+
+        return toCsvString(map);
     }
 
-    private String toCsvString(List<Object> list) {
+    private String toCsvString(Map<String, Object> map) {
+        List<Object> values = new ArrayList<Object>();
+        values.addAll(map.values());
         StringBuilder sb = new StringBuilder("\"");
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < values.size(); i++) {
             if (i != 0) {
                 sb.append("\", \"");
             }
-            sb.append(list.get(i));
+            sb.append(values.get(i));
         }
         sb.append("\"");
         return sb.toString();
@@ -190,21 +209,28 @@ public class ElasticsearchBenchmark {
     }
 
     private void startCreateDocumentTextsThread() {
+        if (bulkCount != -1 && bulkCount != Integer.MAX_VALUE && bulkCount > minimumDocumentQueueDepth) {
+            LOGGER.warn("bulkCount ({}) is larger than minimumDocumentQueueDepth!", bulkCount);
+        }
+        if (bulkSize != -1 && bulkSize > (maxDocumentSize / 2) * minimumDocumentQueueDepth) {
+            LOGGER.warn("bulkSize ({}) is larger than maxDocumentSize/2 * minimumDocumentQueueDepth!", bulkSize);
+        }
+
         Thread createIndexRequestsThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     while (true) {
-                        if (documentTexts.size() < 2000) {
+                        if (documentTexts.size() < minimumDocumentQueueDepth) {
                             synchronized (documentTexts) {
                                 documentTexts.add(getRandomText());
                             }
                         } else {
-                            Thread.sleep(2);
+                            Thread.sleep(documentQueueCheckInterval);
                         }
                     }
                 } catch (Exception ex) {
-                    LOGGER.error("oops", ex);
+                    LOGGER.error("oops!", ex);
                 }
             }
         });
@@ -213,7 +239,7 @@ public class ElasticsearchBenchmark {
     }
 
     private void readWordList() throws IOException {
-        LOGGER.info("Loading word list");
+        LOGGER.info("loading word list...");
         words = new ArrayList<String>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/words.txt")));
         try {
@@ -233,26 +259,32 @@ public class ElasticsearchBenchmark {
     private void insertDocuments(Client client) throws IOException, InterruptedException {
         if (bulkSize == -1 && bulkCount == -1) {
             String text = getNextDocumentText();
+            LOGGER.trace("inserting a single document, {} bytes...", text.length());
             client.index(createIndexRequest(text)).actionGet();
             numberOfBytesInserted += text.length();
             currentCount++;
         } else {
             long initialSize = numberOfBytesInserted;
             BulkRequest bulkRequest = new BulkRequest();
+            int requestCount = 0;
+            long requestSize = 0;
             for (int i = 0; (i == 0) || ((i < bulkCount) && ((numberOfBytesInserted - initialSize) < bulkSize)); i++) {
                 String text = getNextDocumentText();
                 bulkRequest.add(createIndexRequest(text));
                 numberOfBytesInserted += text.length();
                 currentCount++;
+                requestCount++;
+                requestSize += text.length();
             }
+            LOGGER.trace("bulk inserting {} documents, {} bytes...", requestCount, requestSize);
             client.bulk(bulkRequest).actionGet();
         }
     }
 
     private String getNextDocumentText() throws InterruptedException {
         while (documentTexts.size() == 0) {
-            LOGGER.error("creating index requests is too slow.");
-            Thread.sleep(10);
+            LOGGER.error("document queue underflow!");
+            Thread.sleep(100);
         }
         synchronized (documentTexts) {
             return documentTexts.remove();
@@ -276,7 +308,7 @@ public class ElasticsearchBenchmark {
 
     private String getRandomText() {
         StringBuilder value = new StringBuilder();
-        int valueLength = RANDOM.nextInt(documentSize);
+        int valueLength = RANDOM.nextInt(maxDocumentSize);
         while (value.length() < valueLength) {
             if (value.length() > 0) {
                 value.append(' ');
@@ -287,9 +319,9 @@ public class ElasticsearchBenchmark {
     }
 
     private void ensureIndexIsCreated(Client client) throws IOException {
-        LOGGER.info("checking index " + indexName);
+        LOGGER.info("checking for index {}...", indexName);
         if (!client.admin().indices().prepareExists(indexName).execute().actionGet().isExists()) {
-            LOGGER.info("creating index " + indexName);
+            LOGGER.info("creating index {}...", indexName);
             XContentBuilder builder = XContentFactory.jsonBuilder()
                     .startObject()
                     .startObject(ELEMENT_TYPE)
@@ -297,27 +329,32 @@ public class ElasticsearchBenchmark {
                     .endObject()
                     .endObject();
             client.admin().indices().prepareCreate(indexName).addMapping(ELEMENT_TYPE, builder).execute().actionGet();
+            LOGGER.info("index {} created", indexName);
         } else {
-            LOGGER.info("index " + indexName + " already exists");
+            LOGGER.info("index {} already exists", indexName);
         }
     }
 
     private Client createClient(Settings settings) {
         Client client;
+        long startTime = System.currentTimeMillis();
         if (nodeApi) {
-            LOGGER.info("Connecting to elasticsearch via node API");
+            LOGGER.info("connecting to elasticsearch via node API...");
             Node node = NodeBuilder.nodeBuilder()
                     .settings(settings)
                     .client(true)
                     .node();
             client = node.client();
         } else {
-            LOGGER.info("Connecting to elasticsearch via transport client " + hostname + ":" + port);
+            LOGGER.info("connecting to elasticsearch via transport client...");
             client = new TransportClient(settings);
             for (String host : hostname.split(",")) {
+                LOGGER.info("adding address {}:{}...", host, port);
                 ((TransportClient) client).addTransportAddress(new InetSocketTransportAddress(host, port));
             }
         }
+        long stopTime = System.currentTimeMillis();
+        LOGGER.info("connected in {} ms", stopTime - startTime);
         return client;
     }
 }
