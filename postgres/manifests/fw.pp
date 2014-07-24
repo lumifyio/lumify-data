243319314bdf7b8fb9392f3296e19b03f007a1f1
Ryan Lanman <rmlanman@gmail.com>
2014-07-24 09:52:15 -0400

    Adding parameter for source network to firewall rules

--- postgres/manifests/fw.pp
+++ postgres/manifests/fw.pp
@@ -1,11 +1,12 @@
 class postgres::fw::db (
-  $srcnet = "0.0.0.0/0" ){
-    
+  $srcnet = "0.0.0.0/0"
+){
+
   firewall { '520 allow postgres standard' :
     port   => [5432],
     proto  => tcp,
     action => accept,
     source => "${srcnet}"
   }
-  
+
 }
--- zookeeper/manifests/fw.pp
+++ zookeeper/manifests/fw.pp
@@ -1,7 +1,10 @@
-class zookeeper::fw::node {
+class zookeeper::fw::node (
+  $srcnet = "0.0.0.0/0"
+){
   firewall { '260 allow zookeeper' :
     port   => [2181, 2888, 3888],
     proto  => tcp,
     action => accept,
+    source => "${srcnet}",
   }
 }
