712ce8db7503865974f2bf19cae5ffedb69ea7c0
David Singley <dsingley@nearinfinity.com>
2014-08-01 13:20:45 -0400

    update demo vm build

--- bin/mvn-war.sh
+++ bin/mvn-war.sh
@@ -15,11 +15,11 @@ DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
   mvn package -P web-war -pl lumify-public/web/war -am -DskipTests -Dsource.skip=true
   set +x
 
-  if [ -f lumify-public/lumify-web-war/target/lumify-web-war-*.war ]; then
+  if [ -f lumify-public/web/war/target/lumify-web-war-*.war ]; then
     webapps_dir=$(find /opt -maxdepth 2 -type d -name webapps)
     set -x
     sudo rm -rf ${webapps_dir}/lumify*
-    sudo cp lumify-public/lumify-web-war/target/lumify-web-war-*.war ${webapps_dir}/lumify.war
+    sudo cp lumify-public/web/war/target/lumify-web-war-*.war ${webapps_dir}/lumify.war
     set +x
   fi
 )
