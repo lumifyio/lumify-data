86e0fab1d66b3b1f6e05a353a58c42528831ba8e
Joe Ferner <joe@fernsroth.com>
2014-08-07 14:37:57 -0400

    add clone or pull sh

---
+++ bin/clone-or-pull-public.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+
+cd ${DIR}/..
+
+if [ -d "lumify-public" ]; then
+    cd lumify-public
+    git pull
+else
+    git clone git@github.com:lumifyio/lumify.git lumify-public
+fi
