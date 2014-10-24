ecdedf8944a34372e3bbbccd2c7ad7a901316c89
Ryan Lanman <rmlanman@gmail.com>
2014-10-24 15:15:35 -0400

    Getting RabbitMQ configs ready for upgrade

--- puppet/modules/role/manifests/rabbitmq/node.pp
+++ puppet/modules/role/manifests/rabbitmq/node.pp
@@ -2,7 +2,8 @@ class role::rabbitmq::node {
   $rabbitmq_nodes = hiera_array('rabbitmq_nodes', [])
   $rabbitmq_erlang_cookie = hiera('rabbitmq_erlang_cookie', '')
   class { '::rabbitmq' :
-    cluster_nodes => $rabbitmq_nodes,
-    erlang_cookie => $rabbitmq_erlang_cookie,
+    remove_default_user => false,
+    cluster_nodes       => $rabbitmq_nodes,
+    erlang_cookie       => $rabbitmq_erlang_cookie,
   }
 }
--- puppet/puppet-modules/rabbitmq/manifests/init.pp
+++ puppet/puppet-modules/rabbitmq/manifests/init.pp
@@ -1,12 +1,16 @@
 class rabbitmq (
   $cluster_nodes = [],
+  $remove_default_user = true,
   $erlang_cookie = '' # http://www.rabbitmq.com/clustering.html#setup
 ){
   require erlang
   include macro
-	
+
   $rabbitMQVersion = "3.2.3"
-  
+  $mgmt_user = hiera('rabbitmq_mgmt_user','guest')
+  $mgmt_user_pw = hiera('rabbitmq_mgmt_user_pw','guest')
+  $mgmt_user_tags = join(hiera_array('rabbitmq_mgmt_user_tags',['administrator'])," ")
+
   $plugins = ["rabbitmq_management"]
 
   macro::download { "http://www.rabbitmq.com/releases/rabbitmq-server/v${rabbitMQVersion}/rabbitmq-server-${rabbitMQVersion}-1.noarch.rpm" :
@@ -41,22 +45,53 @@ class rabbitmq (
     enable  => true,
     require => Exec[$plugins],
   }
-  
+
+  exec { "create_mgmt_user" :
+    command => "/usr/sbin/rabbitmqctl add_user ${mgmt_user} ${mgmt_user_pw}",
+    unless  => "/usr/sbin/rabbitmqctl list_users | grep -q ${mgmt_user}",
+    user    => 'root',
+    require => Service['rabbitmq-server'],
+  }
+
+  exec { "set_mgmt_user_perms" :
+    command     => "/usr/sbin/rabbitmqctl set_permissions ${mgmt_user} \".*\" \".*\" \".*\"",
+    refreshonly => true,
+    user        => 'root',
+    subscribe   => Exec['create_mgmt_user'],
+  }
+  exec { "set_mgmt_user_tag" :
+    command     => "/usr/sbin/rabbitmqctl set_user_tags ${mgmt_user} ${mgmt_user_tags}",
+    refreshonly => true,
+    user        => 'root',
+    subscribe   => Exec['create_mgmt_user'],
+  }
+
+  if $remove_default_user {
+    exec { "remove_default_user" :
+      command => "/usr/sbin/rabbitmqctl delete_user guest",
+      onlyif  => "/usr/sbin/rabbitmqctl list_users | grep -q guest",
+      user    => 'root',
+      require => Service['rabbitmq-server'],
+    }
+  }
+
   define enablePlugin {
     exec { "${title}" :
       command     => "/usr/sbin/rabbitmq-plugins enable ${title}",
-      onlyif      => "/usr/bin/test `/usr/sbin/rabbitmq-plugins list -E ${title} | wc -l` -eq 0",
+      onlyif      => "/usr/sbin/rabbitmq-plugins list -E ${title} | grep -q ${title}",
       user        => "root",
-      group       => "root",      
+      group       => "root",
       environment => ["HOME=/root"],
       require     => Package['rabbitmq-server'],
     }
   }
-  
+
   enablePlugin { "${plugins}" : }
 
   define policy (
     $vhost = '%2f', # /
+    $user = 'guest',
+    $password = 'guest',
     $pattern,
     $apply_to = 'all',
     $definition,
@@ -66,14 +101,16 @@ class rabbitmq (
     $json = "{\"pattern\":\"${pattern}\", \"apply-to\":\"${apply_to}\", \"definition\":${definition}, \"priority\":${priority}}"
 
     exec { "add-policy-${name}" :
-      command => "/usr/bin/curl -u guest:guest -H 'content-type:application/json' -X PUT ${url} -d '${json}'",
-      unless  => "/usr/bin/curl -s -u guest:guest ${url} -w '%{http_code}\n' -o /dev/null | /bin/grep -q 200",
+      command => "/usr/bin/curl -u ${user}:${password} -H 'content-type:application/json' -X PUT ${url} -d '${json}'",
+      unless  => "/usr/bin/curl -s -u ${user}:${password} ${url} -w '%{http_code}\n' -o /dev/null | /bin/grep -q 200",
       require => Service['rabbitmq-server'],
     }
   }
 
   policy { "ha" :
-    pattern => '.*',
+    pattern    => '.*',
+    user       => $mgmt_user,
+    password   => $mgmt_user_pw,
     definition => '{"ha-mode":"all", "ha-sync-mode":"automatic"}',
   }
 }
