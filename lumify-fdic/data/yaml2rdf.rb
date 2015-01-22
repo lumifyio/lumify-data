1ec322013761f7d1cdb9da169033886aff31fe97
David Singley <dsingley@nearinfinity.com>
2015-01-22 16:03:04 -0500

    slow progress...

--- lumify-fdic/data/yaml2rdf.rb
+++ lumify-fdic/data/yaml2rdf.rb
@@ -20,9 +20,79 @@ rescue
   string
 end
 
+def to_id(name)
+  name.gsub('.', '').gsub(/[^a-z0-9]+/i, '_').strip
+end
+
+def rp_id(yaml_filename)
+  'RP_' + File.basename(yaml_filename, '.pdf.yaml')
+end
+
+def fi_id(name)
+  'FI_' + to_id(name)
+end
+
+def image_id(filename)
+  'FILE_' + filename
+end
+
+def person_id(name)
+  'PERSON_' + to_id(name)
+end
+
+def poa_id(name, financial_institution)
+  'POA_' + to_id(name) + '_' + to_id(financial_institution)
+end
+
+def parse_fi_name(name)
+  quotes = %{"'“”}
+  match_data = name.match(/^\s*(.+?)\s*(?:\([#{quotes}]?([^#{quotes}]*?)[#{quotes}]?\))?\s*$/)
+  return match_data[1], match_data[2]
+rescue
+  log "exception parsing financial institution name: #{name}"
+  name
+end
+
 yaml = YAML.load(File.read(yaml_filename))
 yaml[:pdf_filename] = pdf_filename
-yaml[:resolution_plan][:id] = 'RP_' + File.basename(yaml_filename, '.pdf.yaml')
+yaml[:resolution_plan][:id] = rp_id(yaml_filename)
+
+material_entitiy_hash = Hash.new
+yaml[:resolution_plan][:material_entities].each do |yaml_me|
+  if yaml_me.kind_of? String
+    name, aka = parse_fi_name(yaml_me)
+    properties = Hash.new
+  else
+    name = yaml_me.keys.first
+    properties = yaml_me[name]
+    name, aka = parse_fi_name(name)
+    properties[:resolution_plan_id] = rp_id(yaml_filename)
+  end
+  properties[:id] = fi_id(name)
+  if aka && properties[:alias] == nil
+    properties[:alias] = aka
+  end
+  material_entitiy_hash[name] = properties
+end
+yaml[:resolution_plan][:material_entities] = material_entitiy_hash
+
+financial_market_utility_hash = Hash.new
+yaml[:resolution_plan][:financial_market_utilities].each do |yaml_fmu|
+  if yaml_fmu.kind_of? String
+    name, aka = parse_fi_name(yaml_fmu)
+    properties = Hash.new
+  else
+    name = yaml_fmu.keys.first
+    properties = yaml_fmu[name]
+    name, aka = parse_fi_name(name)
+  end
+  properties[:id] = fi_id(name)
+  if aka && properties[:alias] == nil
+    properties[:alias] = aka
+  end
+  financial_market_utility_hash[name] = properties
+end
+yaml[:resolution_plan][:financial_market_utilities] = financial_market_utility_hash
 
 template_filename = File.join(File.dirname(__FILE__), File.basename(__FILE__, '.rb')) + '.erb'
 template = File.read(template_filename)
