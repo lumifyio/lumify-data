01aebad0af55ef329c275920049778df22b20ff5
Joe Ferner <joe@fernsroth.com>
2015-01-22 16:54:05 -0500

    print people at end

--- lumify-fdic/data/yaml2rdf.rb
+++ lumify-fdic/data/yaml2rdf.rb
@@ -45,9 +45,10 @@ def poa_id(name, financial_institution)
 end
 
 def parse_fi_name(name)
-  quotes = %{"'“”}
-  match_data = name.match(/^\s*(.+?)\s*(?:\([#{quotes}]?([^#{quotes}]*?)[#{quotes}]?\))?\s*$/)
-  return match_data[1], match_data[2]
+  match_data = name.match(/^\s*(.+?)\s*(?:\((.*)\))?\s*$/)
+  fi_name = match_data[1].strip
+  fi_alias = match_data[2] ? match_data[2].gsub(/^[^[0-9A-Za-z]]/, '').gsub(/[^[0-9A-Za-z]]$/, '') : nil
+  return fi_name, fi_alias
 rescue
   log "exception parsing financial institution name: #{name}"
   name
@@ -57,7 +58,9 @@ yaml = YAML.load(File.read(yaml_filename))
 yaml[:pdf_filename] = pdf_filename
 yaml[:resolution_plan][:id] = rp_id(yaml_filename)
 
-material_entitiy_hash = Hash.new
+principal_officers_hash = Hash.new
+
+material_entity_hash = Hash.new
 yaml[:resolution_plan][:material_entities].each do |yaml_me|
   if yaml_me.kind_of? String
     name, aka = parse_fi_name(yaml_me)
@@ -72,9 +75,28 @@ yaml[:resolution_plan][:material_entities].each do |yaml_me|
   if aka && properties[:alias] == nil
     properties[:alias] = aka
   end
-  material_entitiy_hash[name] = properties
+  material_entity_hash[name] = properties
+
+  if properties[:principal_officers]
+    properties[:principal_officers].each do |po|
+      po_name = po.keys.first
+      po_properties = po[po_name]
+      if principal_officers_hash[po_name] == nil
+        principal_officers_hash[po_name] = Hash.new
+      end
+      if principal_officers_hash[po_name][:orgs] == nil
+        principal_officers_hash[po_name][:orgs] = Hash.new
+      end
+      po_properties[:id] = poa_id(po_name, name)
+      principal_officers_hash[po_name][:orgs][name] = po_properties
+
+      if po_properties[:image].kind_of? String
+        principal_officers_hash[po_name][:image] = po_properties[:image]
+      end
+    end
+  end
 end
-yaml[:resolution_plan][:material_entities] = material_entitiy_hash
+yaml[:resolution_plan][:material_entities] = material_entity_hash
 
 financial_market_utility_hash = Hash.new
 yaml[:resolution_plan][:financial_market_utilities].each do |yaml_fmu|
@@ -93,6 +115,8 @@ yaml[:resolution_plan][:financial_market_utilities].each do |yaml_fmu|
   financial_market_utility_hash[name] = properties
 end
 yaml[:resolution_plan][:financial_market_utilities] = financial_market_utility_hash
+
+yaml[:resolution_plan][:principal_officers] = principal_officers_hash
 
 template_filename = File.join(File.dirname(__FILE__), File.basename(__FILE__, '.rb')) + '.erb'
 template = File.read(template_filename)
