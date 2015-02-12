1963b1e6cc3232a504865beaf69dc1b3e5f390a4
David Singley <dsingley@nearinfinity.com>
2015-02-12 13:17:31 -0500

    include top level dir in .zip files

--- lumify-sinaloa/build.sh
+++ lumify-sinaloa/build.sh
@@ -5,17 +5,17 @@ BUILD_DIR=${DIR}/build
 
 function create_zip {
     local name=$1
-    local zip_file_name=${BUILD_DIR}/sinaloa-${name}.zip
+    local zip_file_name=${BUILD_DIR}/${name}.zip
     echo "Creating build/$(basename ${zip_file_name})"
     (
-        cd ${DIR}/${name}
-        zip -rq ${zip_file_name} .
+        cd ${DIR}
+        zip -rq ${zip_file_name} ${name}
     )
 }
 
 rm -rf ${BUILD_DIR}
 mkdir -p ${BUILD_DIR}
-create_zip ontology
-create_zip data-rdf
-create_zip data
+create_zip sinaloa-ontology
+create_zip sinaloa-data-rdf
+create_zip sinaloa-data
 echo "DONE"
