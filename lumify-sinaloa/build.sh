0f87923bcdf586133c3d5f768c306c00344fde9f
Joe Ferner <joe@fernsroth.com>
2015-02-12 10:04:04 -0500

    remove old build data

--- lumify-sinaloa/build.sh
+++ lumify-sinaloa/build.sh
@@ -7,13 +7,13 @@ function create_zip {
     NAME=$1
     ZIP_FILE_NAME=${BUILD_DIR}/sinaloa-${NAME}.zip
     echo "Creating build/$(basename ${ZIP_FILE_NAME})"
-    rm -f ${ZIP_FILE_NAME}
     (
         cd ${DIR}/${NAME}
         zip -rq ${ZIP_FILE_NAME} .
     )
 }
 
+rm -rf ${BUILD_DIR}/*
 mkdir -p ${BUILD_DIR}
 create_zip ontology
 create_zip data-rdf
