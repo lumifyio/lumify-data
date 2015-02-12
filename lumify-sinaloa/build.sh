661e2981659b7f20403827db1732dd9502f87cc6
Joe Ferner <joe@fernsroth.com>
2015-02-12 10:03:11 -0500

    add build script

---
+++ lumify-sinaloa/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+DIR=$(cd $(dirname "$0") && pwd)
+BUILD_DIR=${DIR}/build
+
+function create_zip {
+    NAME=$1
+    ZIP_FILE_NAME=${BUILD_DIR}/sinaloa-${NAME}.zip
+    echo "Creating build/$(basename ${ZIP_FILE_NAME})"
+    rm -f ${ZIP_FILE_NAME}
+    (
+        cd ${DIR}/${NAME}
+        zip -rq ${ZIP_FILE_NAME} .
+    )
+}
+
+mkdir -p ${BUILD_DIR}
+create_zip ontology
+create_zip data-rdf
+echo "DONE"
